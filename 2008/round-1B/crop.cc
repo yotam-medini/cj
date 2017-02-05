a19e28e802c0310e2dd8a05214bbaaa39bb4a78b
Yotam Medini <yotam.medini@gmail.com>
2017-02-06 01:09:24 +0200

    crop passed large

--- 2008/round-1B/crop.cc
+++ 2008/round-1B/crop.cc
@@ -6,7 +6,7 @@
 #include <string>
 // #include <set>
 // #include <map>
-// #include <vector>
+#include <vector>
 #include <utility>
 
 #include <cstdlib>
@@ -17,16 +17,26 @@ using namespace std;
 typedef mpz_class mpzc_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
+typedef vector<unsigned> vu_t;
 typedef vector<ul_t> vul_t;
 typedef vector<ull_t> vull_t;
 
 static unsigned dbg_flags;
+
+struct XYXY_M3
+{
+    XYXY_M3(unsigned vxi=0, unsigned vyi=0, unsigned vx1i=0, unsigned vy1i=0):
+         xi(vxi), yi(vyi), x1i(vx1i), y1i(vy1i) {}
+    unsigned xi, yi, x1i, y1i;
+};
+typedef vector<XYXY_M3> v_xyxy_m3_t;
 
 class Crop
 {
  public:
     Crop(istream& fi);
     void solve();
+    void solve_naive();
     void print_solution(ostream&) const;
  private:
     unsigned n;
@@ -41,6 +51,68 @@ Crop::Crop(istream& fi) :
 }
 
 void Crop::solve()
+{
+    ull_t n_xy[3][3];
+    ull_t n_xyxy[3][3][3][3];
+    v_xyxy_m3_t xy_comps[3][3];
+
+    for (unsigned xi = 0; xi != 3; ++xi)
+    {
+        for (unsigned yi = 0; yi != 3; ++yi)
+        {
+            n_xy[xi][yi] = 0;
+            for (unsigned x1i = 0; x1i != 3; ++x1i)
+            {
+                for (unsigned y1i = 0; y1i != 3; ++y1i)
+                {
+                    n_xyxy[xi][yi][x1i][y1i] = 0;
+                    if ((xi < x1i) || ((xi == x1i) && (yi <= y1i)))
+                    {
+                        unsigned xcmp = (6 - (xi + x1i)) % 3;
+                        unsigned ycmp = (6 - (yi + y1i)) % 3;
+                        XYXY_M3 xyxy_m3(xi, yi, x1i, y1i);
+                        xy_comps[xcmp][ycmp].push_back(xyxy_m3);
+                    }
+                }
+            }
+        }
+    }
+
+
+    ull_t x = x0;
+    ull_t y = y0;
+    for (ull_t t = 0; t != n; ++t)
+    {
+        unsigned xm3 = x % 3;
+        unsigned ym3 = y % 3;
+        unsigned n_xy_old = n_xy[xm3][ym3];
+
+        const v_xyxy_m3_t& v = xy_comps[xm3][ym3];
+        for (v_xyxy_m3_t::const_iterator i = v.begin(), e = v.end();
+             i != e; ++i)
+        {
+            const XYXY_M3 &xyxy = *i;
+            unsigned n2 = n_xyxy[xyxy.xi][xyxy.yi][xyxy.x1i][xyxy.y1i];
+            solution += n2;
+        }
+
+        for (unsigned xi = 0; xi != 3; ++xi)
+        {
+            for (unsigned yi = 0; yi != 3; ++yi)
+            {
+                unsigned n_peer = n_xy[xi][yi];
+                unsigned nn = n_xyxy[xi][yi][xm3][ym3] + n_peer;
+                n_xyxy[xi][yi][xm3][ym3] = nn;
+                n_xyxy[xm3][ym3][xi][yi] = nn;
+            }
+        }
+        n_xy[xm3][ym3] = n_xy_old + 1;
+        x = (A*x +B) % M;
+        y = (C*y +D) % M;
+    }
+}
+
+void Crop::solve_naive()
 {
     vul_t x(vul_t::size_type(n), 0);
     vul_t y(vul_t::size_type(n), 0);
@@ -70,11 +142,12 @@ if (dbg_flags & 0x1) { cerr << "[" << i << "]=(" << xnext <<", "<<ynext<<")\n";}
             {
                 if ( (((xij + x[k]) % 3) == 0) && (((yij + y[k]) % 3) == 0) )
                 {
+if (dbg_flags & 0x1) { cerr << "[" << i<<", "<< j<<", " << k << "]\n"; }
                     ++solution;
                 }
-            }    
-        }    
-    }    
+            }
+        }
+    }
 
 }
 
@@ -101,6 +174,7 @@ int main(int argc, char ** argv)
     }
 
     if (argc > 3) { dbg_flags = strtoul(argv[3], 0, 0); }
+    bool naive = (argc > 4) && !strcmp(argv[4], "-naive");
     // bool large = (argc > 4) && !strcmp(argv[4], "-large");
 
     unsigned n_cases;
@@ -111,14 +185,21 @@ int main(int argc, char ** argv)
     {
         Crop problem(*pfi);
         // cerr << "Case ci="<<ci << " (ci+1)="<<ci+1 << "\n";
-        problem.solve();
+        if (naive)
+        {
+            problem.solve_naive();
+        }
+        else
+        {
+            problem.solve();
+        }
         fout << "Case #"<< ci+1 << ":";
         problem.print_solution(fout);
         fout << "\n";
         fout.flush();
-        
+
     }
-    
+
     if (pfi != &cin) { delete pfi; }
     if (pfo != &cout) { delete pfo; }
     return 0;
