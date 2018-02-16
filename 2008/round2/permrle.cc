30587a22d536d5f2e04246eea9c66237b002d6bb
Yotam Medini <yotam.medini@gmail.com>
2018-02-16 21:20:18 +0200

    permRLE non-naive greedy does not work

--- 2008/round2/permrle.cc
+++ 2008/round2/permrle.cc
@@ -23,6 +23,20 @@ typedef vector<unsigned> vu_t;
 
 static unsigned dbg_flags;
 
+#if 0
+static void ov(ostream &fo, const vu_t &v)
+{
+    fo << "[";
+    const char *del = "";
+    for (auto i = v.begin(), e = v.end(); i != e; ++i)
+    {
+        fo << del << *i;
+        del = ", ";
+    }
+    fo << "]\n";
+}
+#endif
+
 static void iota(vu_t& v, unsigned n, unsigned val=0)
 {
     v.clear();
@@ -73,7 +87,7 @@ class PermRLE
     void solve();
     void print_solution(ostream&) const;
  private:
-    unsigned compress_size(const char *ccs) const;
+    unsigned compress_size(const char *ccs, unsigned limit) const;
     void sperm(const vu_t &perm);
     void perm_sub_mirror(vu_t &perm, unsigned b, unsigned e) const;
     void perm_shift(vu_t &perm, unsigned b, unsigned e, unsigned t) const;
@@ -101,7 +115,7 @@ void PermRLE::solve_naive()
     const unsigned slen = s.size();
     vu_t perm;
     permutation_first(perm, k);
-    unsigned best = compress_size(cs_alt);
+    unsigned best = compress_size(cs_alt, slen);
     while (permutation_next(perm))
     {
         for (unsigned ci = 0; ci < slen; )
@@ -112,7 +126,7 @@ void PermRLE::solve_naive()
                 cs_alt[ci0 + perm[pi]] = cs[ci];
             }
         }
-        unsigned csize = compress_size(cs_alt);
+        unsigned csize = compress_size(cs_alt, best);
         if (best > csize)
         {
             best = csize;
@@ -125,7 +139,7 @@ void PermRLE::solve()
 {
     vu_t perm0, perm;
     permutation_first(perm0, k);
-    unsigned best = compress_size(cs_alt);
+    unsigned best = compress_size(cs_alt, slen);
     if (dbg_flags & 0x1) { cerr << "Initial best=" << best << "\n"; }
     bool reducing = true;
     while (reducing)
@@ -134,26 +148,58 @@ void PermRLE::solve()
         // sub-mirrors
         for (unsigned b = 0; b < k; ++b)
         {
-            for (unsigned e = (b + 2) % k; e != b; e = (e + 1) % k)
+            unsigned b1 = (b + 1) % k;
+            for (unsigned e = (b + 2) % k; e != b1; e = (e + 1) % k)
             {
                 perm = perm0;
                 perm_sub_mirror(perm, b, e);
                 sperm(perm);
-                unsigned csize = compress_size(cs_alt);
+                unsigned csize = compress_size(cs_alt, best);
                 if (best > csize)
                 {
                     reducing = true;
                     best = csize;
                     swap(cs, cs_alt);
                     if (dbg_flags & 0x1) {
+                        // ov(cerr, perm);
                         cerr << "mirror-reducing: best=" << best << "\n"; }
                 }
             }
         }
 
+        // 3-cycles
+        for (unsigned i = 0; i < k; ++i)
+        {
+            for (unsigned j = 0; j < k; ++j)
+            {
+                for (unsigned y = (i != j ? 0 : k); y < k; ++y)
+                {
+                    if ((i != y) && (j != y))
+                    {
+                        perm = perm0;
+                        perm[i] = j;
+                        perm[j] = y;
+                        perm[y] = i;
+                        unsigned csize = compress_size(cs_alt, best);
+                        if (best > csize)
+                        {
+                            reducing = true;
+                            best = csize;
+                            swap(cs, cs_alt);
+                            if (dbg_flags & 0x1) {
+                                // ov(cerr, perm);
+                                cerr << "3cycle-reducing: best=" << best <<"\n";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // swap segments
         for (unsigned b = 0; b < k; ++b)
         {
-            for (unsigned e = (b + 1) % k; e != b; e = (e + 1) % k)
+            for (unsigned e = (b + 1) % k; e != b; e = (e + 1) % (k + 1))
             {
                 for (unsigned t = (b + 1) % k; t != b; t = (t + 1) % k)
                 {
@@ -170,13 +216,14 @@ void PermRLE::solve()
                             perm_sub_mirror(perm, t, (t + e + (k - b)) % k);
                         }
                         sperm(perm);
-                        unsigned csize = compress_size(cs_alt);
+                        unsigned csize = compress_size(cs_alt, best);
                         if (best > csize)
                         {
                             reducing = true;
                             best = csize;
                             swap(cs, cs_alt);
                             if (dbg_flags & 0x1) {
+                                // ov(cerr, perm);
                                 cerr << "shift-reducing: best=" << best << "\n";
                             }
                         }
@@ -193,12 +240,12 @@ void PermRLE::print_solution(ostream &fo) const
     fo << " " << solution;
 }
 
-unsigned PermRLE::compress_size(const char *ccs) const
+unsigned PermRLE::compress_size(const char *ccs, unsigned limit) const
 {
     char clast = ' ';
     unsigned ret = 0;
     char c;
-    for (const char *pc = ccs; (c = *pc); ++pc)
+    for (const char *pc = ccs; (c = *pc) && (ret < limit); ++pc)
     {
         if (clast != c)
         {
@@ -239,6 +286,7 @@ void PermRLE::perm_sub_mirror(vu_t &perm, unsigned b, unsigned e) const
 void PermRLE:: perm_shift(vu_t &perm, unsigned b, unsigned e, unsigned t) const
 {
     unsigned sz = perm.size();
+    e = e % sz;
     for (unsigned i = b, j = t; i != e; i = (i + 1) % sz, j = (j + 1) % sz)
     {
         swap(perm[i], perm[j]);
