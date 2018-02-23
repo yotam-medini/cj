21350a9d3b3ecab5ca7c7bd375c3b47ab3311dd4
Yotam Medini <yotam.medini@gmail.com>
2018-02-23 17:28:11 +0200

    crazyrows: small progress in non-naive

--- 2009/round2/crazyrows.cc
+++ 2009/round2/crazyrows.cc
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-// #include <set>
+#include <set>
 #include <map>
 #include <queue>
 #include <vector>
@@ -19,9 +19,13 @@ using namespace std;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 // typedef vector<ul_t> vul_t;
+typedef vector<bool> vb_t;
 typedef vector<unsigned> vu_t;
 typedef vector<int> vi_t;
 typedef vector<string> vs_t;
+
+typedef set<int> seti_t;
+typedef map<int, seti_t> i2seti_t;
 
 static unsigned dbg_flags;
 
@@ -47,9 +51,13 @@ class CrazyRows
     void print_solution(ostream&) const;
  private:
     bool is_ll(const vi_t &vi) const; // low-left triangle matrix
+    void displaced_add(int pi);
     unsigned n;
     vs_t srows;
-    vi_t max1pos;
+    vi_t vrows;
+    vi_t perm;
+    vi_t invperm;
+    i2seti_t displaced;
     graph_t graph;
     qvi_t qvi;    
     unsigned solution;
@@ -59,7 +67,7 @@ CrazyRows::CrazyRows(istream& fi) : n(0), solution(~0)
 {
     fi >> n;
     srows.reserve(n);
-    max1pos.reserve(n);
+    vrows.reserve(n);
     for (unsigned i = 0; i < n; ++i)
     {
         string s;
@@ -71,14 +79,14 @@ CrazyRows::CrazyRows(istream& fi) : n(0), solution(~0)
         {
             --pos;
         }
-        max1pos.push_back(pos);
+        vrows.push_back(pos);
     }
 }
 
 void CrazyRows::solve_naive()
 {
-    graph.insert(graph_t::value_type(max1pos, Node(0, GRAY)));
-    qvi.push(max1pos);
+    graph.insert(graph_t::value_type(vrows, Node(0, GRAY)));
+    qvi.push(vrows);
     bool found = false;
     while (!found)
     {
@@ -95,13 +103,17 @@ void CrazyRows::solve_naive()
             unsigned dnext = node.d + 1;
             for (unsigned i = 0, j = 1; j < n; i = j++)
             {
-                vi_t vnext(qtop_mtx);
-                swap(vnext[i], vnext[j]);
-                w = graph.find(vnext);
-                if (w == graph.end())
+                if (qtop_mtx[i] > qtop_mtx[j])
                 {
-                    graph.insert(graph_t::value_type(vnext, Node(dnext, GRAY)));
-                    qvi.push(vnext);
+                    vi_t vnext(qtop_mtx);
+                    swap(vnext[i], vnext[j]);
+                    w = graph.find(vnext);
+                    if (w == graph.end())
+                    {
+                        graph.insert(graph_t::value_type(vnext, 
+                            Node(dnext, GRAY)));
+                        qvi.push(vnext);
+                    }
                 }
             }
         }
@@ -110,10 +122,102 @@ void CrazyRows::solve_naive()
     }
 }
 
+// Algorithm:
+//   Push heavy rows down starting from the heaviest, most lower,
+//   Pushed down rows considered final setting (!?)
+//   Keep list/set of displaced rows.
+//   With the displaced rows again start with the heavier lower.
+//   Given displaced, Consider empty places, but if not
+//   do push down, respect 'final setting'.
+void CrazyRows::solve()
+{
+    // vi_t v(vrows);
+    vb_t finals(vb_t::size_type(n), false);
+    // vb_t displaced(vb_t::size_type(n), false);
+
+    seti_t vacant;
+    perm.reserve(n);
+    invperm.reserve(n);
+    for (int i = 0; i < int(n); ++i) 
+    { 
+        perm.push_back(i);
+        invperm.push_back(i);
+    }
+    // unsigned perm_size = 0;
+    for (int k = n - 1; k >= 0; --k)
+    {
+        const int vk = vrows[perm[k]];
+        if (vk > k)
+        {
+            // How many v[k] above
+            vi_t f;
+            for (int i = k; i >= 0; --i)
+            {
+                if (vrows[perm[i]] == vk)
+                {
+                    f.push_back(i);
+                }
+            }
+            int n_push = f.size();
+            auto fiter = f.begin();
+            for (int pi = vk; pi < vk + n_push; ++pi, ++fiter)
+            {
+                int i = *fiter;
+                vacant.insert(i);
+                perm[i] = pi;
+                invperm[pi] = i;
+                perm[pi] = -1;
+                invperm[i] = -1;
+                displaced_add(pi);
+                finals[pi] = true;
+            }
+        }
+    }    
+    i2seti_t old_displaced;
+    swap(old_displaced, displaced);
+    while (!old_displaced.empty())
+    {
+        for (auto &keyval: old_displaced)
+        {
+            int kv = keyval.first;
+            int ti = kv;
+            const seti_t &pis = keyval.second;
+            for (auto i = pis.rbegin(), e = pis.rend(); i != e; ++i)
+            {
+                int pi = *i;
+                for (; invperm[ti] >= 0 && vrows[invperm[ti]] >= kv; ++ti);
+                if (invperm[ti] >= 0)
+                {
+                    displaced_add(invperm[ti]);
+                }
+                perm[pi] = ti;
+                invperm[ti] = pi;
+            }
+        }
+        swap(old_displaced, displaced);
+    }
+}
+
+void CrazyRows::displaced_add(int pi)
+{
+    int vpi = vrows[perm[pi]];
+    auto w = displaced.find(vpi);
+    if (w == displaced.end())
+    {
+        i2seti_t::value_type piv(vpi, seti_t({pi}));
+        displaced.insert(piv);
+    }
+    else
+    {
+         (*w).second.insert(pi);
+    }
+}
+
+#if 0
 void CrazyRows::solve()
 {
     unsigned n_swaps = 0;
-    vi_t v(max1pos);
+    vi_t v(vrows);
     for (int k = n - 1; k >= 0; --k)
     {
         if (v[k] > k)
@@ -141,7 +245,7 @@ void CrazyRows::solve()
     }
     solution = n_swaps;
 }
-
+#endif
 bool CrazyRows::is_ll(const vi_t &vi) const
 {
     bool ret = true;
