be5b933b2420f1bbec1ebec76c9d4ac3a96f91a4
Yotam Medini <yotam.medini@gmail.com>
2018-04-21 22:27:41 +0300

    smooth just naive and tiny case,

--- 2010/round-1A/smooth.cc
+++ 2010/round-1A/smooth.cc
@@ -32,10 +32,8 @@ class Smooth
     void solve();
     void print_solution(ostream&) const;
  private:
-    u_t sub_solve_naive(const vi_t &sub, u_t depth);
     u_t sub_solve_2(int v0, int v1);
     u_t sub_solve_3(int v0, int v1, int v2);
-    u_t sub_solve_naive_insertion(const vi_t &cur, u_t ci, u_t depth);
     bool is_smooth(const vi_t &sub) const;
     u_t D, I, N;
     int M;
@@ -55,7 +53,6 @@ Smooth::Smooth(istream& fi) : solution(0)
 
 void Smooth::solve_naive()
 {
-    // solution = sub_solve_naive(a, 0);
     solution = is_smooth(a) ? 0 : u_t(-1);
     if (solution != 0)
     {
@@ -144,8 +141,7 @@ u_t Smooth::sub_solve_3(int v0, int v1, int v2)
             {
                 subret = abs(c - v0) + sub_solve_2(v0, v1) + sub12;
                 minby(ret, subret);
-                subret = abs(c - v1) + 
-                    sub_solve_2(v0, c) + sub_solve_2(c, v2);
+                subret = abs(c - v1) + sub_solve_2(v0, c) + sub_solve_2(c, v2);
                 minby(ret, subret);
                 subret = abs(c - v2) + sub01 + sub_solve_2(v1, c);
                 minby(ret, subret);
@@ -167,105 +163,6 @@ u_t Smooth::sub_solve_3(int v0, int v1, int v2)
         }
     }
     return ret;
-}
-
-u_t Smooth::sub_solve_naive(const vi_t &cur, u_t depth)
-{
-    u_t ret = 0;
-    if (dbg_flags & 0x1) {
-       for (unsigned i = 0; i < depth; ++i) { cerr << ' '; }
-       for (int v: cur) { cerr << ' ' << v; } cerr << '\n'; }
-    if (!is_smooth(cur))
-    {
-        ret = u_t(-1); // infinity
-        const u_t n = cur.size();
-        vi_t sub;
-        for (unsigned i = 0; i < n; ++i)
-        {
-            const vi_t::const_iterator curi = cur.begin() + i;
-            const int vcuri = *curi;
-            bool jump_pre = (i > 0) && (abs(vcuri - cur[i - 1]) > M);
-            bool jump_aft = (i + 1 < n) && (abs(cur[i + 1] - vcuri) > M);
-            if (jump_pre || jump_aft)
-            {
-                // Try deletion
-                sub.clear();
-                sub.insert(sub.end(), cur.begin(), curi);
-                sub.insert(sub.end(), curi + 1, cur.end());
-                u_t subret = sub_solve_naive(sub, depth + 1);
-                if (ret > subret + D)
-                {
-                    ret = subret + D;
-                }
-
-                if ((i > 0) && ((i + 1) < n) && (jump_pre || (i + 1 == n)))
-                {
-                    subret = sub_solve_naive_insertion(cur, i, depth);
-                    if (ret > subret)
-                    {
-                        ret = subret;
-                    }
-                }
-
-                // Try change
-                int nbr_pre = (i == 0 ? vcuri : cur[i - 1]);
-                int nbr_aft = (i + 1 < n ? cur[i + 1] : vcuri);
-                int newval;
-                if (i == 0)
-                {
-                    newval = nbr_aft - (vcuri < nbr_aft ?  M : -M);
-                }
-                else if (i  == n - 1)
-                {
-                    newval = nbr_pre + (nbr_pre < vcuri ? M : -M);
-                }
-                else
-                {
-                    // int cdelta = nbr_aft - nbr_pre;
-                    // newval = nbr_pre + ((cdelta/M)/2)*M;
-                    newval = (nbr_pre + nbr_aft)/2;
-                }
-                if (newval != vcuri)
-                {
-                    sub = cur;
-                    sub[i] = newval;
-                    subret = sub_solve_naive(sub, depth + 1);
-                    ul_t cost = abs(newval - vcuri);
-                    if (ret > subret + cost)
-                    {
-                        ret = subret + cost;
-                    }
-                }
-            }
-        }
-    }
-    return ret;
-}
-
-u_t Smooth::sub_solve_naive_insertion(const vi_t &cur, u_t ci, u_t depth)
-{
-    u_t ins = 0;
-    vi_t::const_iterator curi = cur.begin() + ci;
-    int vpre = cur[ci - 1];
-    int vcur = cur[ci];
-    int vper = cur[ci + 1];
-    vi_t sub;
-    sub.insert(sub.end(), cur.begin(), curi);
-    for (int step = (vpre < vcur ? M : -M), v = vpre + step;
-         abs(vcur - v) >= M; v += step, ++ins)
-    {
-        sub.push_back(v);
-    }
-    sub.push_back(vcur);
-    for (int step = (vcur < vper ? M : -M), v = vpre + step;
-         abs(vper - v) >= M; v += step, ++ins)
-    {
-        sub.push_back(v);
-    }
-    sub.insert(sub.end(), curi + 1, cur.end());
-    u_t cost = I * ins;
-    u_t subet = sub_solve_naive(sub, depth + 1);
-    return subet + cost;
 }
 
 void Smooth::print_solution(ostream &fo) const
