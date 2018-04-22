bcb12b5040c1de962f4d4656c650c59f39e5423a
Yotam Medini <yotam.medini@gmail.com>
2018-04-23 01:29:39 +0300

    bitparty: passed small, failed big

--- 2018/round-1A/bitparty.cc
+++ 2018/round-1A/bitparty.cc
@@ -1,24 +1,21 @@
 // CodeJam
 // Author:  Yotam Medini  yotam.medini@gmail.com -- Created: 2013/April/20
 
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <string>
-#include <set>
-// #include <map>
-#include <vector>
 #include <utility>
+#include <vector>
+#include <numeric>
 
 #include <cstdlib>
-// #include <gmpxx.h>
 
 using namespace std;
 
-// typedef mpz_class mpzc_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<ull_t> vull_t;
-typedef set<unsigned> setu_t;
 
 static unsigned dbg_flags;
 
@@ -27,12 +24,12 @@ class Cachier
  public:
     Cachier(ul_t _m=0, ul_t _s=0, ul_t _p=0) : 
         m(_m), s(_s), p(_p), nb_assigned(0) {}
-    ul_t nb_time(unsigned nb) const 
+    ull_t nb_time(unsigned nb) const 
     {
         ul_t ret = s*nb + p;
         return ret;
     }
-    ul_t time() const { return nb_time(nb_assigned); }
+    ull_t time() const { return nb_time(nb_assigned); }
     ull_t m, s, p;
     ull_t nb_assigned;
     vull_t best_tail;
@@ -48,15 +45,9 @@ class BitParty
     void print_solution(ostream&) const;
  private:
     ull_t time_subsplit(unsigned ci, ull_t nbit, ull_t nr);
-    void reduce();
-    unsigned best_deal(ull_t nb) const;
-    bool is_better(const Cachier &can, const Cachier &than, ull_t nb) const;
-    bool pair_reduce(unsigned ci_slow, unsigned ci);
     ull_t r, b, c;
     vcachier_t cachiers;
     ull_t solution;
-    setu_t c_assigned;
-    setu_t c_unassigned;
 };
 
 BitParty::BitParty(istream& fi) : solution(ull_t(-1))
@@ -117,185 +108,49 @@ ull_t BitParty::time_subsplit(unsigned ci, ull_t nbit, ull_t nr)
 
 void BitParty::solve()
 {
-    // solve_naive();
-    for (unsigned ci = 0; ci < c; ++ci)
+    ull_t time_low = 0;
+    ull_t time_high = 0;
+    for (const Cachier &cachier: cachiers)
     {
-        c_unassigned.insert(ci);
-    }
-
-    ull_t nb_pending = b;
-    while ((nb_pending > 0) && (c_assigned.size() < r))
-    {
-        unsigned ci = best_deal(nb_pending);
-        Cachier &cachier = cachiers[ci];
-        unsigned nb = min(nb_pending, cachier.m);
-        cachier.nb_assigned = nb;
-        nb_pending -= nb;
-        c_assigned.insert(setu_t::value_type(ci));
-        c_unassigned.erase(setu_t::value_type(ci));
-    }
-
-    while ((nb_pending > 0) && !c_unassigned.empty())
-    {
-        // Give up min-nb cachier
-        unsigned ci_min = *(c_assigned.begin());
-        for (auto cii = c_assigned.begin(), cie = c_assigned.end();
-            cii != cie; ++cii)
+        ull_t t = cachier.nb_time(b);
+        if (time_high < t)
         {
-            unsigned ci = *cii;
-            if (cachiers[ci_min].nb_assigned >  cachiers[ci].nb_assigned)
+            time_high = t;
+        }
+    }
+    if (dbg_flags & 0x1) { cerr << "time_high="<< time_high << "\n"; }
+    vull_t bits_possible = vull_t(vull_t::size_type(c), vull_t::value_type(0));
+    while (time_low < time_high)
+    {
+        ull_t tmid = (time_low + time_high)/2;
+        for (unsigned ci = 0; ci < c; ++ci)
+        {
+            const Cachier &cachier = cachiers[ci];
+            // t = cachier.s * bits + p
+            // bits = (t - p) / s
+            ull_t bt = 0;
+            if (cachier.p < tmid)
             {
-                ci_min = ci;
+                bt = (tmid - cachier.p) / cachier.s;
+                if (bt > cachier.m)
+                {
+                    bt = cachier.m;
+                }
             }
+            bits_possible[ci] = bt;
         }
-        nb_pending += cachiers[ci_min].nb_assigned;
-        cachiers[ci_min].nb_assigned = 0;
-        c_assigned.erase(setu_t::value_type(ci_min));
-        unsigned ci = best_deal(nb_pending);
-        unsigned nb = min(nb_pending, cachiers[ci].m);
-        cachiers[ci].nb_assigned = nb;
-        nb_pending -= nb;
-        c_assigned.insert(setu_t::value_type(ci));
-        c_unassigned.erase(setu_t::value_type(ci));
-    }
-    if (nb_pending > 0)
-    {
-        cerr << "Failure\n";  exit(1);
-    }
-    reduce();
-    solution = 0;
-    for (auto cii = c_assigned.begin(), cie = c_assigned.end();
-        cii != cie; ++cii)
-    {
-        unsigned ci = *cii;
-        unsigned t = cachiers[ci].time();
-        if (solution < t)
+        sort(bits_possible.begin(), bits_possible.end());
+        ull_t bmax = accumulate(bits_possible.end() - r, bits_possible.end(), 0);
+        if (b <= bmax)
         {
-            solution = t;
-        }
-    }
-}
-
-unsigned BitParty::best_deal(ull_t nb) const
-{
-    unsigned ci_best = *(c_unassigned.begin());
-    for (auto cii = c_unassigned.begin(), cie = c_unassigned.end(); cii != cie;
-        ++cii)
-    {
-        unsigned ci = *cii;
-        if (is_better(cachiers[ci], cachiers[ci_best], nb))
-        {
-            ci_best = ci;
-        }
-    }
-    return ci_best;
-}
-
-bool BitParty::is_better(const Cachier &can, const Cachier &than, ull_t nb)
-    const
-{
-    bool ret = false;
-    nb = min(nb, max(can.m, than.m));
-    if ((nb <= can.m) && (nb <= than.m))
-    {
-        ull_t tcan = can.nb_time(nb);
-        ull_t tthan = than.nb_time(nb);
-        ret = (tcan < tthan);
-    }
-    else
-    {
-        if (nb > can.m)
-        {
-            ull_t tcan = can.nb_time(can.m) + than.nb_time(nb - can.m);
-            ull_t tthan = than.nb_time(nb);
-            ret = (tcan < tthan);
+            time_high = tmid;
         }
         else
         {
-            ull_t tcan = can.nb_time(nb);
-            ull_t tthan = than.nb_time(than.m) + can.nb_time(nb - than.m);
-            ret = (tcan < tthan);
+            time_low = tmid + 1;
         }
     }
-      
-    return ret;
-}
-
-void BitParty::reduce()
-{
-    bool reducing = true;
-    while (reducing)
-    {
-        reducing = false;
-        unsigned ci_slowest = *c_assigned.begin();
-        ull_t t_slowest = cachiers[ci_slowest].time();
-        for (auto cii = c_assigned.begin(), cie = c_assigned.end();
-            cii != cie; ++cii)
-        {
-            unsigned ci = *cii;
-            ull_t t = cachiers[ci].time();
-            if (t_slowest < t)
-            {
-                t_slowest = t;
-                ci_slowest = ci;
-            }
-        }
-
-        //for (auto cii = c_assigned.begin(), cie = c_assigned.end();
-        //    (cii != cie) && !reducing; ++cii)
-        for (unsigned ci = 0; ci < c; ++ci)
-        {
-             // unsigned ci = *cii;
-             reducing = (ci != ci_slowest) && 
-                 ((c_assigned.size() < r) || (cachiers[ci].nb_assigned > 0)) &&
-                 pair_reduce(ci_slowest, ci);
-        }
-    }
-}
-
-bool BitParty::pair_reduce(unsigned ci_slow, unsigned ci)
-{
-    bool ret = false;
-    Cachier &cslow = cachiers[ci_slow];
-    Cachier &cother = cachiers[ci];
-    ull_t nb = cslow.nb_assigned + cother.nb_assigned;
-    ull_t tcurr = cslow.time();
-    
-    // p1 + s1 x = p2 + s2 (n - x)
-    // x = (p2 - p1 + s2n)/(s1 + s2)
-    ull_t x = (cother.p + cother.s * nb - cslow.p) / (cslow.s + cother.s);
-    ull_t x1 = x + 1;
-    if (max(cslow.nb_time(x), cother.nb_time(nb - x)) >
-        max(cslow.nb_time(x1), cother.nb_time(nb - x1)))
-    {
-        x = x1;
-    }
-    if (x < cslow.m)
-    {
-         ull_t xother = nb - x;
-         if (xother > cother.m)
-         {
-              xother = cother.m;
-              x = nb - xother;
-         }
-         if (x < cslow.m)
-         {
-             ull_t t = max(cslow.nb_time(x), cother.nb_time(xother));
-             ret = (t < tcurr);
-             if (ret)
-             {
-                 
-                 cslow.nb_assigned = x;
-                 if (cother.nb_assigned == 0)
-                 {
-                     c_assigned.insert(setu_t::value_type(ci));
-                     c_unassigned.erase(setu_t::value_type(ci));
-                 }
-                 cother.nb_assigned = nb - x;
-             }
-         }
-    }
-    return ret;
+    solution = time_low;
 }
 
 void BitParty::print_solution(ostream &fo) const
