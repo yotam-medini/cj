e0122301177cf54e3fca2b6e24ddc0eae97770c4
Yotam Medini <yotam.medini@gmail.com>
2019-02-22 00:35:31 +0200

    loadtest passed small case

---
+++ 2010/round-1C/loadtest.cc
@@ -0,0 +1,135 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com -- Created: 2013/April/20
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <cstdlib>
+
+using namespace std;
+
+// typedef mpz_class mpzc_t;
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+
+static unsigned dbg_flags;
+
+class LoadTest
+{
+ public:
+    LoadTest(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    ull_t l, p, c;
+    ull_t solution;
+};
+
+LoadTest::LoadTest(istream& fi) : solution(0)
+{
+    fi >> l >> p >> c;
+}
+
+void LoadTest::solve_naive()
+{
+    ull_t n = 0;
+    for (ull_t lcn = l * c; lcn < p; lcn *= c, ++n) {}
+    while (n)
+    {
+        ++solution;
+        n /= 2;
+    }
+}
+
+void LoadTest::solve()
+{
+    solve_naive();
+}
+
+void LoadTest::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (LoadTest::*psolve)() =
+        (naive ? &LoadTest::solve_naive : &LoadTest::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        LoadTest loadTest(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (loadTest.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        loadTest.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
