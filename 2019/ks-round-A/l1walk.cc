50abd52d55ab3428e08963d84a84b6e9844b3855
Yotam Medini <yotam.medini@n2ws.com>
2019-06-26 17:19:13 +0300

    l1walk: separate get_quad_l1segments()

--- 2019/ks-round-A/l1walk.cc
+++ 2019/ks-round-A/l1walk.cc
@@ -1,13 +1,129 @@
 #include <iostream>
 #include <cstdlib>
+#include <array>
 
 using namespace std;
 
 typedef unsigned u_t;
 
+class L1Segment
+{
+ public:
+     L1Segment(int vxb=0, int vyb=0, int vxe=0, int vye=0) :
+         xb(vxb),
+         yb(vyb),
+         xe(vxe),
+         ye(vye),
+	 xstep(vxb < vxe ? 1 : -1),
+	 ystep(vyb < vye ? 1 : -1)
+     {}
+     bool empty() const { return xb == xe; }
+     int xb, yb, xe, ye;
+     int xstep, ystep;
+};
+
+typedef array<L1Segment, 4> QL1Segment_t;
+
+void get_quad_l1segments(QL1Segment_t &ql1seg, int r, int c, int x, int y, int d)
+{
+    int cm1 = c - 1;
+    int rm1 = r - 1;
+    for (int q = 0; q < 4; ++q)
+    {
+        int xb = 0, xe = 0, yb = 0, ye = 0;
+        bool has = false;
+        switch (q)
+        {
+         case 0:
+            has = ((cm1 - x) + (rm1 - y) >= d);
+            if (has)
+            {
+                xb = x + d; yb = y; xe = x; ye = y + d;
+                if (xb > cm1)
+                {
+                    yb += (xb - cm1);
+                    xb = cm1;
+                }
+                if (ye > r)
+                {
+                    xe += (ye - r);
+                    ye = r;
+                }
+            }
+            break;
+         case 1:
+            has = (x + (rm1 - y) >= d);
+            if (has)
+            {
+                xb = x; yb = y + d; xe = x - d; ye = y;
+                if (yb > rm1)
+                {
+                    xb -= (yb - rm1);
+                    yb = rm1;
+                }
+		if (xe < -1)
+		{
+		    ye -= xe + 1;
+		    xe = -1;
+		}
+            }
+            break;
+         case 2:
+            has = ((x + y) >= d);
+            if (has)
+            {
+                xb = x - d; yb = y; xe = x; ye = y - d;
+                if (xb < 0)
+                {
+                    yb += xb;
+                    xb = 0;
+                }
+                if (ye < -1)
+                {
+                    xe += ye + 1;
+                    ye = -1;
+                }
+            }
+            break;
+         case 3:
+            has = (((cm1 - x) + y) >= d);
+            if (has)
+            {
+                xb = x; yb = y - d; xe = x + d; ye = y;
+		if (yb < 0)
+		{
+		    xb -= yb;
+		    yb = 0;
+		}
+		if (xe > c)
+		{
+		    ye -= (c - xe);
+		    xe = c;
+		}
+            }
+            break;
+        }
+	ql1seg[q] = L1Segment(xb, yb, xe, ye);
+    }
+}
+
 void l1_walk(int r, int c, int x, int y, int d)
 {
-    // static const int steps[4][2] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    QL1Segment_t qseg;
+    get_quad_l1segments(qseg, r, c, x, y, d);
+    for (int q = 0; q < 4; ++q)
+    {
+        const L1Segment &seg = qseg[q];
+	cout << "Q[" << q << "}:" << (seg.empty() ? " Empty" : "") << "\n";
+	for (int wx = seg.xb, wy = seg.yb; wx != seg.xe; wx += seg.xstep, wy += seg.ystep)
+	{
+	    cout << "  (" << wx << ", " << wy << ")\n";
+	}
+    }
+}
+
+void l1_walk_old(int r, int c, int x, int y, int d)
+{
     int cm1 = c - 1;
     int rm1 = r - 1;
     for (int q = 0; q < 4; ++q)
@@ -77,7 +193,7 @@ void l1_walk(int r, int c, int x, int y, int d)
 		    xb -= yb;
 		    yb = 0;
 		}
-		if (xe >= c)
+		if (xe > c)
 		{
 		    ye -= (c - xe);
 		    xe = c;
