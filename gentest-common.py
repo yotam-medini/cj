969d740e2b3197bed22f693ed6b23c33bb53a89f
Yotam Medini <yotam.medini@gmail.com>
2019-06-29 00:18:09 +0300

    gentest-common.py support large case - no diff

--- gentest-common.py
+++ gentest-common.py
@@ -14,26 +14,35 @@ def syscmd(cmd):
     rc = os.system(cmd)
     return rc
 
+def check_rc(rc):
+    if rc != 0:
+        ew('Failed running %s' % progname)
+        sys.exit(rc | (rc >> 8))
+    
 def rundiff(fn_in):
     fn_out_naive = '%s-auto-naive.out' % progname
     fn_out = '%s-auto.out' % progname
     rc1 = syscmd('./bin/%s -naive %s %s' % (progname, fn_in, fn_out_naive))
     rc2 = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
-    if (rc1 | rc2) != 0:
-        ew('Failed running %s' % progname)
-        sys.exit(rc1 | rc2)
+    check_rc(rc1 | rc2)
     rcdiff = syscmd('diff %s %s' % (fn_out_naive, fn_out))
     if rcdiff != 0:
         ew('Inconsistent')
         sys.exit(1)
 
 if __name__ == '__main__':
+    large = False
     fn_in = '%s-auto.in' % progname
-    N = int(sys.argv[1])
-    for n in range(N):
-        ew('Tested %d/%d' % (n, N))
+    T = int(sys.argv[1])
+    for t in range(T):
+        ew('Tested %d/%d' % (t, T))
         f = open(fn_in, 'w')
         f.write('1\n')
         f.close()
-        rundiff(fn_in)
+        if large:
+            fn_out = '%s-auto.out' % progname
+            rc = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+            check_rc(rc)
+        else:
+            rundiff(fn_in)
     sys.exit(0)
