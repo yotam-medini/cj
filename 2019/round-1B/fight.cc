b9580e466634d0244cea179e5fd6b351ef15755f
Yotam Medini <yotam.medini@gmail.com>
2019-07-13 08:45:58 +0300

    fight has NextBound +gentest but still buggy

--- 2019/round-1B/fight.cc
+++ 2019/round-1B/fight.cc
@@ -18,10 +18,156 @@ typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<int> vi_t;
+typedef vector<vi_t> vvi_t;
+typedef vector<ull_t> vull_t;
 typedef array<int, 2> i2_t;
-
+// typedef map<int, u_t> i2u_t;
 
 static unsigned dbg_flags;
+
+class NextBound
+{
+ public:
+    NextBound(const vi_t &values);
+    int prev_bound(int before) const 
+    {
+        return prev_bound_value(before, get(before));
+    }
+    int next_bound(int after) const
+    {
+        return next_bound_value(after, get(after));
+    }
+    int prev_bound_value(int before, int v) const;
+    int next_bound_value(int after, int v) const;
+    int get(int i) const { return binmax[0][i]; }
+    int prev_bound_naive(int before) const
+    {
+        return prev_bound_value_naive(before, get(before));
+    }
+    int next_bound_naive(int after) const
+    {
+        return next_bound_value_naive(after, get(after));
+    }
+    int prev_bound_value_naive(int before, int v) const;
+    int next_bound_value_naive(int after, int v) const;
+ private:
+    void fill_bin(vvi_t &bmax);
+    int bin_prev_bound_value(const vvi_t &bmax, int before, int v) const;
+    vvi_t binmax;
+    vvi_t rbinmax;
+};
+
+NextBound::NextBound(const vi_t &values)
+{
+    u_t sz = values.size();
+    int log2 = 0;
+    while ((1ul << log2) < sz)
+    {
+        ++log2;
+    }
+    binmax.reserve(log2);
+    binmax.push_back(values);
+    fill_bin(binmax);
+    rbinmax.reserve(log2);
+    vi_t rvalues(values.rbegin(), values.rend());
+    rbinmax.push_back(rvalues);
+    fill_bin(rbinmax);
+}
+
+void NextBound::fill_bin(vvi_t &bmax)
+{
+    while (bmax.back().size() > 1)
+    {
+        bmax.push_back(vi_t());
+        vi_t &next = bmax.back();
+        const vi_t &prev = bmax[bmax.size() - 2];
+        u_t prev_sz = prev.size();
+        next.reserve(prev_sz / 2);
+        for (u_t i = 0, j = 0; j + 1 < prev_sz; ++i, j += 2)
+        {
+            next.push_back(max(prev[j], prev[j + 1]));
+        }
+    }    
+}
+
+int NextBound::prev_bound_value(int before, int v) const
+{
+    return bin_prev_bound_value(binmax, before, v);
+}
+
+int NextBound::next_bound_value(int after, int v) const
+{
+    u_t sz = rbinmax[0].size();
+    int bi = bin_prev_bound_value(rbinmax, sz - after - 1, v);
+    int ret = sz - bi - 1;
+    return ret;
+}
+
+int NextBound::bin_prev_bound_value(const vvi_t &bmax, int before, int v) const
+{
+    int ret = -1;
+    u_t p2b = u_t(-1), bib = u_t(-1);
+    for (int p2 = int(bmax.size()) - 1, bi = 0; p2 >= 0; --p2)
+    {
+        if ((bi + 1)*(1u << p2) <= unsigned(before))
+        {
+            if (bmax[p2][bi] > v)
+            {
+                p2b = p2;
+                bib = bi;
+            }
+            bi = 2*bi + 2;
+        }
+        else
+        {
+            bi = 2*bi;
+        }
+    }
+    if (p2b != u_t(-1))
+    {
+        while (p2b > 0)
+        {
+            --p2b;
+            bib = 2*bib + 1;
+            if (bmax[p2b][bib] <= v)
+            {
+                --bib;
+            }
+            
+        }
+        ret = bib;
+    }
+    return ret;
+}
+
+int NextBound::prev_bound_value_naive(int before, int v) const
+{
+    int ret = -1;
+    for (int i = before - 1; i >= 0; --i)
+    {
+        if (v < get(i))
+        {
+            ret = i;
+            i = 0;
+        }
+    }
+    return ret;
+}
+
+int NextBound::next_bound_value_naive(int after, int v) const
+{
+    int ret = -1;
+    int sz = binmax[0].size();
+    for (int i = after + 1; i < sz; ++i)
+    {
+        if (v < get(i))
+        {
+            ret = i;
+            i = sz;
+        }
+    }
+    return ret;
+}
 
 class Fight
 {
@@ -31,6 +177,7 @@ class Fight
     void solve();
     void print_solution(ostream&) const;
  private:
+    void solve_naive_better();
     bool is_fair(const i2_t &v) const;
     ul_t n;
     int k;
@@ -55,7 +202,7 @@ Fight::Fight(istream& fi) : solution(0)
 
 void Fight::solve_naive()
 {
-    unsigned nfair = 0;
+    ull_t nfair = 0;
     vi_t::const_iterator cbegin = cd[0].begin();
     vi_t::const_iterator dbegin = cd[1].begin();
     for (u_t l = 0; l < n; ++l)
@@ -88,9 +235,9 @@ static void maximize(int &m, int x)
     }
 }
 
-void Fight::solve()
+void Fight::solve_naive_better()
 {
-    unsigned nfair = 0;
+    ull_t nfair = 0;
     for (u_t b = 0; b < n; ++b)
     {
         i2_t cdmax({cd[0][b], cd[1][b]});
@@ -98,11 +245,78 @@ void Fight::solve()
         {
             maximize(cdmax[0], cd[0][e]);
             maximize(cdmax[1], cd[1][e]);
-            if (is_fair(cdmax))
+            // if (is_fair(cdmax))
+            if (abs(cdmax[0] - cdmax[1]) <= k)
             {
                 ++nfair;            
             }
         }
+    }
+    solution = nfair;
+}
+
+void Fight::solve()
+{
+    ull_t nfair = 0;
+    NextBound nbcd[2] = {NextBound(cd[0]), NextBound(cd[1])};
+    vull_t nfair_till;
+    nfair_till.reserve(n);
+    for (u_t i = 0; i < n; ++i)
+    {
+        ull_t nf = 0;
+        i2_t cdi({cd[0][i], cd[1][i]});
+        u_t ismall = (cdi[0] < cdi[1] ? 0 : 1);
+        u_t ilarge = 1 - ismall;
+        int delta = cdi[ilarge] - cdi[ismall];
+        int iprev_small = nbcd[ismall].prev_bound(i);
+        int iprev_large = nbcd[ilarge].prev_bound(i);
+        if (delta <= k)
+        {
+            ++nf;
+            if (iprev_small < iprev_large)
+            {
+                int fair_max = cdi[ismall] + k;
+                int i_unfair = nbcd[ilarge].prev_bound_value(
+                    iprev_large + 1, fair_max);
+                nf += (i - max(iprev_small, i_unfair) - 1);
+                if (iprev_small >= 0)
+                {
+                    nf += nfair_till[iprev_small];
+                }
+            }
+            else
+            {
+                int fair_small = cdi[ilarge] - k;
+                int i_unfair = nbcd[ismall].prev_bound_value(i, fair_small);
+                nf += (i - max(iprev_large, i_unfair) - 1);
+                if (iprev_large >= 0)
+                {
+                    nf += nfair_till[iprev_large];
+                }
+            }
+        }
+        else
+        {
+            if (iprev_small < iprev_large)
+            {
+                if (iprev_small >= 0)
+                {
+                   nf += nfair_till[iprev_small];
+                }
+            }
+            else
+            {
+                int fair_small = cdi[ilarge] - k;
+                int i_unfair = nbcd[ilarge].prev_bound_value(i,  fair_small);
+                // ?? nf += (i - max(iprev_large, i_unfair) - 1);
+                if (iprev_large >= 0)
+                {
+                    nf += nfair_till[iprev_large];
+                }
+            }            
+        }
+        nfair_till.push_back(nf);
+        nfair += nf;
     }
     solution = nfair;
 }
