f08bf59f13fe30f93c96ebec57933b4203eaa7bb
Yotam Medini <yotam.medini@gmail.com>
2019-10-03 00:44:02 +0300

    gentest-cherries for large input

--- 2019/ks-round-E/gentest-cherries.py
+++ 2019/ks-round-E/gentest-cherries.py
@@ -36,28 +36,47 @@ if __name__ == '__main__':
     ai = 1
     T = int(sys.argv[ai]); ai += 1
     n = int(sys.argv[ai]); ai += 1
-    all_edges = []
-    for c0 in range(1, n):
-        for c1 in range(c0 + 1, n + 1):
-            all_edges.append((c0, c1))
+    mdef = int(sys.argv[ai]) if (ai < len(sys.argv)) else None
+    if n <= 7:
+        all_edges = []
+        for c0 in range(1, n):
+            for c1 in range(c0 + 1, n + 1):
+                all_edges.append((c0, c1))
     for t in range(T):
         ew('Tested %d/%d' % (t, T))
         f = open(fn_in, 'w')
         f.write('1\n')
-        m = randint(0, (n*(n-1))/2)
+        mmax = min((n*(n-1))/2, 100000)
+        m = randint(0, mmax) if mdef is None else mdef
         f.write('%d %d\n' % (n, m))
-        vedges = all_edges[:]
-        for i in range(m):
-            ei = randint(0, len(vedges) - 1)
-            e = vedges[ei]
-            f.write('%d %d\n' % (e[0], e[1]))
-            vedges[ei] = vedges[-1]
-            vedges.pop()
-        f.close()
         large = n > 7
         if large:
+            pass
+            vedges = set()
+            for ei in range(m):
+                if (ei & (ei - 1)) == 0:
+                    ew('edges: %d/%d' % (ei, m))
+                added = False
+                while not added:
+                    c1 = randint(1, n)
+                    c2 = randint(1, n)
+                    c1c2 = (c1, c2)
+                    if (c1 < c2) and c1c2 not in vedges:
+                        f.write('%d %d\n' % c1c2)
+                        vedges.add(c1c2)
+                        added = True
+        else:
+            vedges = all_edges[:]
+            for i in range(m):
+                ei = randint(0, len(vedges) - 1)
+                e = vedges[ei]
+                f.write('%d %d\n' % (e[0], e[1]))
+                vedges[ei] = vedges[-1]
+                vedges.pop()
+        f.close()
+        if large:
             fn_out = '%s-auto.out' % progname
-            rc = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+            rc = syscmd('timeout 1 ./bin/%s %s %s' % (progname, fn_in, fn_out))
             check_rc(rc)
         else:
             rundiff(fn_in)
