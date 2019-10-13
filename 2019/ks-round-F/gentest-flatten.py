4741f48d0b07e5f958c8f4b832d5ca78556a565e
Yotam Medini <yotam.medini@gmail.com>
2019-10-13 17:05:11 +0300

    flatten: still buggy in case a[0]==a[1]

---
+++ 2019/ks-round-F/gentest-flatten.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+from random import randint
+import sys
+
+progname = 'flatten'
+
+def ew(msg):
+    sys.stderr.write('%s\n' % msg)
+
+def syscmd(cmd):
+    ew(cmd)
+    rc = os.system(cmd)
+    return rc
+
+def check_rc(rc):
+    if rc != 0:
+        ew('Failed running %s' % progname)
+        sys.exit(rc | (rc >> 8))
+    
+def rundiff(fn_in):
+    fn_out_naive = '%s-auto-naive.out' % progname
+    fn_out = '%s-auto.out' % progname
+    rc1 = syscmd('./bin/%s -naive %s %s' % (progname, fn_in, fn_out_naive))
+    rc2 = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+    check_rc(rc1 | rc2)
+    rcdiff = syscmd('diff %s %s' % (fn_out_naive, fn_out))
+    if rcdiff != 0:
+        ew('Inconsistent')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    large = False
+    fn_in = '%s-auto.in' % progname
+    ai = 1
+    T = int(sys.argv[ai]); ai += 1
+    n = int(sys.argv[ai]); ai += 1
+    hmin = int(sys.argv[ai]); ai += 1
+    hmax = int(sys.argv[ai]); ai += 1
+    for t in range(T):
+        ew('Tested %d/%d' % (t, T))
+        f = open(fn_in, 'w')
+        k = randint(0, n - 1)
+        f.write('1\n%d %d\n' % (n, k))
+        heights = set()
+        sep = ''
+        for i in range(n):
+            h = randint(hmin, hmax)
+            f.write('%s%d' % (sep, h))
+            sep = ' '
+            heights.add(h)
+        f.write('\n')
+        f.close()
+        n_comb = len(heights) ** n
+        large = n_comb > 0x1000
+        if large:
+            fn_out = '%s-auto.out' % progname
+            rc = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+            check_rc(rc)
+        else:
+            rundiff(fn_in)
+    sys.exit(0)
