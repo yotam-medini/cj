8f29b5f64b3c21e3e08fdd0bc53829b999c8a4be
Yotam Medini <yotam.medini@gmail.com>
2019-10-16 21:00:53 +0300

    teachme passed Test set 2

---
+++ 2019/ks-round-F/gentest-teachme.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+from random import randint
+import sys
+
+progname = 'teachme'
+
+def ew(msg):
+    sys.stderr.write('%s\n' % msg)
+
+def syscmd(cmd):
+    ew(cmd)
+    rc = os.system(cmd)
+    return rc
+
+def check_rc(rc):
+    if rc != 0:
+        ew('Failed running %s' % progname)
+        sys.exit(rc | (rc >> 8))
+    
+def rundiff(fn_in):
+    fn_out_naive = '%s-auto-naive.out' % progname
+    fn_out = '%s-auto.out' % progname
+    rc1 = syscmd('./bin/%s -naive %s %s' % (progname, fn_in, fn_out_naive))
+    rc2 = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+    check_rc(rc1 | rc2)
+    rcdiff = syscmd('diff %s %s' % (fn_out_naive, fn_out))
+    if rcdiff != 0:
+        ew('Inconsistent')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    large = False
+    fn_in = '%s-auto.in' % progname
+    ai = 1
+    T = int(sys.argv[ai]); ai += 1
+    n = int(sys.argv[ai]); ai += 1
+    s = int(sys.argv[ai]); ai += 1 # must be >= 5
+    for t in range(T):
+        ew('Tested %d/%d' % (t, T))
+        f = open(fn_in, 'w')
+        f.write('1\n%d %d\n' % (n, s))
+        for e in range(n):
+            c = randint(1, 5)
+            f.write('%d' % c)
+            skills = set()
+            for si in range(c):
+                skill = randint(1, s)
+                while skill in skills:
+                    skill = randint(1, s)
+                skills.add(skill)
+                f.write(' %d' % skill)
+            f.write('\n')
+        f.close()
+        large = (n > 0x20)
+        if large:
+            fn_out = '%s-auto.out' % progname
+            rc = syscmd('./bin/%s %s %s' % (progname, fn_in, fn_out))
+            check_rc(rc)
+        else:
+            rundiff(fn_in)
+    sys.exit(0)
