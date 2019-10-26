c5ffafa60df21336a579f3e9d274e23fca37267b
Yotam Medini <yotam.medini@gmail.com>
2019-10-26 11:46:00 +0300

    shifts passed Test set 2

--- 2019/ks-round-G/shifts.cc
+++ 2019/ks-round-G/shifts.cc
@@ -1,8 +1,11 @@
 // CodeJam
 // Author:  Yotam Medini  yotam.medini@gmail.com --
 
+#include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <utility>
 #include <vector>
@@ -16,8 +19,19 @@ typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<u_t> vu_t;
 typedef vector<ull_t> vull_t;
+typedef array<ull_t, 2> aull2_t;
+typedef vector<aull2_t> vaull2_t;
 
 static unsigned dbg_flags;
+
+class Shift
+{
+ public:
+    Shift(ull_t a=0, ull_t b=0) : ab{a, b}, sort_idx(u_t(-1)) {}
+    aull2_t ab;
+    u_t sort_idx;
+};
+typedef vector<Shift> vshift_t;
 
 class Shifts
 {
@@ -27,11 +41,30 @@ class Shifts
     void solve();
     void print_solution(ostream&) const;
  private:
+    enum { NMAX = 20 };
+    static void set_power3()
+    {
+        if (power3[0] == 0)
+        {
+            power3[0] = 1;
+            for (u_t p = 1; p <= 20; ++p)
+            {
+                power3[p] = 3 * power3[p - 1];
+            }
+        }
+    }
+    void set_shifts();
+    ull_t tail_hcount(u_t bi, const aull2_t& hgot);
+    static ull_t power3[NMAX + 1];
     u_t n;
     ull_t h;
     vull_t ab[2];
+    vshift_t shifts_dec;
+    vaull2_t shift_tail;
     ull_t solution;
 };
+
+ull_t Shifts::power3[Shifts::NMAX + 1];
 
 Shifts::Shifts(istream& fi) : solution(0)
 {
@@ -95,38 +128,89 @@ void Shifts::solve_naive()
     }
 }
 
-#if 0
-void Shifts::solve_naive()
-{
-    ull_t happy2[2];
-    for (u_t iab = 0; iab != 2; ++iab)
-    {
-        ull_t happy = 0;
-        const vull_t& c = ab[iab];
-        for (ull_t bits = 0, bitse = (1ull << n); bits < bitse; ++bits)
-        {
-            ull_t points = 0;
-            for (u_t si = 0; si < n; ++si)
-            {
-                if (bits & (1ull << si))
-                {
-                    points += c[si];
-                }
-            }
-            if (points >= h)
-            {
-                ++happy;
-            }
-        }
-        happy2[iab] = happy;
-    }
-    solution = happy2[0] * happy2[1];
-}
-#endif
-
 void Shifts::solve()
 {
-    solve_naive();
+    set_power3();
+    set_shifts();
+    solution = tail_hcount(0, aull2_t({0, 0}));
+}
+
+void Shifts::set_shifts()
+{
+    aull2_t ab_sum({0, 0});
+    for (u_t i = 0; i < n; ++i)
+    {
+        shifts_dec.push_back(Shift(ab[0][i], ab[1][i]));
+        ab_sum[0] += ab[0][i];
+        ab_sum[1] += ab[1][i];
+    }
+    vu_t idx2[2];
+    for (u_t i = 0; i != 2; ++i)
+    {
+        vu_t& idx = idx2[i];
+        idx = vu_t(vu_t::size_type(n), 0);
+        iota(idx.begin(), idx.end(), 0);
+        sort(idx.begin(), idx.end(),
+            [this, i](const u_t i0, const u_t i1)
+            {
+                return ((shifts_dec[i0].ab[i] > shifts_dec[i1].ab[i]) ||
+                     ((shifts_dec[i0].ab[i] == shifts_dec[i1].ab[i]) &&
+                      (shifts_dec[i0].ab[1-i] > shifts_dec[i1].ab[1-i])));
+            });
+    }
+    u_t iab = (ab_sum[0] < ab_sum[1] ? 1 : 0);
+    u_t ab_sidx2[2] = {0, 0};
+    for (u_t dec_idx = 0; dec_idx != n; ++dec_idx, iab = 1 - iab)
+    {
+        const vu_t& idx = idx2[iab];
+        u_t& ab_sidx = ab_sidx2[iab];
+        while (shifts_dec[idx[ab_sidx]].sort_idx != u_t(-1))
+        {
+            ++ab_sidx;
+        }
+        shifts_dec[idx[ab_sidx]].sort_idx = dec_idx;
+    }
+    sort(shifts_dec.begin(), shifts_dec.end(),
+        [](const Shift& s0, const Shift& s1)
+        {
+            return s0.sort_idx < s1.sort_idx;
+        });
+    shift_tail = vaull2_t(vaull2_t::size_type(n + 1), aull2_t({0, 0}));
+    for (int si = n; si > 0; )
+    {
+        --si;
+        for (u_t i = 0; i != 2; ++i)
+        {
+            shift_tail[si][i] = shifts_dec[si].ab[i] + shift_tail[si + 1][i];
+        }
+    }
+}
+
+ull_t Shifts::tail_hcount(u_t bi, const aull2_t& hgot)
+{
+    ull_t ret = 0;
+    if ((h <= shift_tail[bi][0] + hgot[0]) &&
+        (h <= shift_tail[bi][1] + hgot[1]))
+    {
+        if ((hgot[0] >= h) && (hgot[1] >= h))
+        {
+            ret = power3[n - bi];
+        }
+        else
+        {
+            const Shift& shift = shifts_dec[bi];
+            for (u_t mask = 1; mask <= 3; ++mask)
+            {
+                aull2_t sub_hgot(hgot);
+                for (u_t iab = 0; iab != 2; ++iab)
+                {
+                    sub_hgot[iab] += (mask & (1u << iab) ? shift.ab[iab] : 0);
+                }
+                ret += tail_hcount(bi + 1, sub_hgot);
+            }
+        }
+    }
+    return ret;
 }
 
 void Shifts::print_solution(ostream &fo) const
