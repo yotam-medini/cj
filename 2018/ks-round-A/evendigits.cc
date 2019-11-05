4444a2b312b8cbdcc759fd42f0b5f7062e31ea79
Yotam Medini <yotam.medini@gmail.com>
2019-11-05 17:08:55 +0200

    evendigits passed tiny maive case

---
+++ 2018/ks-round-A/evendigits.cc
@@ -0,0 +1,145 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <iostream>
+#include <fstream>
+#include <string>
+// #include <set>
+// #include <map>
+// #include <vector>
+#include <utility>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+// typedef vector<ul_t> vul_t;
+
+static unsigned dbg_flags;
+
+class EvenDigits
+{
+ public:
+    EvenDigits(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    bool has_odd(ull_t x) const; 
+    ul_t n;
+    ull_t solution;
+};
+
+EvenDigits::EvenDigits(istream& fi) : solution(0)
+{
+    fi >> n;
+}
+
+void EvenDigits::solve_naive()
+{
+    ull_t n_down = 0, n_up = 0;
+    for (ull_t nn = n; has_odd(nn); --nn, ++n_down) {}
+    for (ull_t nn = n; has_odd(nn); ++nn, ++n_up) {}
+    solution = min(n_down, n_up);
+}
+
+void EvenDigits::solve()
+{
+     solve_naive();
+}
+
+bool EvenDigits::has_odd(ull_t x) const
+{
+     while ((x > 0) && (((x % 10) % 2) == 0))
+     {
+         x /= 10;
+     }
+     return (x != 0);
+}
+void EvenDigits::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (EvenDigits::*psolve)() =
+        (naive ? &EvenDigits::solve_naive : &EvenDigits::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        EvenDigits evenDigits(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (evenDigits.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        evenDigits.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
