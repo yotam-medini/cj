ee026c1c2473550a0220cf7d11c0d4bb0dd0af50
Yotam Medini <yotam.medini@gmail.com>
2020-03-02 23:43:42 +0200

    cave: non-naive works on tiny case

--- 2018/ks-round-G/cave.cc
+++ 2018/ks-round-G/cave.cc
@@ -11,6 +11,7 @@
 #include <utility>
 
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -50,7 +51,7 @@ template <class T>
 class Matrix : public BaseMatrix
 {
   public:
-    Matrix(u_t _m, u_t _n) : BaseMatrix(_m, _n), _a(new T[m *n]) {}
+    Matrix(u_t _m, u_t _n) : BaseMatrix(_m, _n), _a(new T[m * n]) {}
     virtual ~Matrix() { delete [] _a; }
     const T& get(u_t r, u_t c) const { return _a[rc2i(r, c)]; }
     T& get(u_t r, u_t c) { return _a[rc2i(r, c)]; }
@@ -83,7 +84,32 @@ class State
     ll_t energy;
     u_t traps_mask;
     setu_t components;
+    setu_t traps_tried;
 };
+
+class GState
+{
+ public:
+    GState(ll_t e=0) : energy(e), traps_mask(0) {}
+    ll_t energy;
+    u_t traps_mask;
+    setu_t components;
+};
+
+class Node
+{
+ public:
+    typedef pair<u_t, setu_t> mask_comps_t;
+    Node(ll_t e=0, int ci=(-1), int ti=(-1)) :
+        energy(e), component(ci), trap(ti) {}
+    ll_t energy;   // or negative trap strength
+    int component; // -1 if trap
+    int trap; // -1 if component
+    vu_t adjs;
+    setu_t adjs_temp;
+    set<mask_comps_t> tried;
+};
+typedef vector<Node> vnode_t;
 
 class Cave
 {
@@ -98,14 +124,25 @@ class Cave
     void get_traps();
     void set_components();
     ull_t dfs_component(const ai2_t& rc, u_t comp_id, ull_t energy);
-    u_t N, M;
+    void cell_solve(const ai2_t& rc, const State& state, u_t depth);
+    void build_graph();
+    void node_solve_check(u_t node_index, GState& state, u_t depth);
+    void node_solve(u_t node_index, GState& state, u_t depth);
+    void show_graph() const;
+    bool rc_inside(const ai2_t& rc) const
+    {
+        bool in = (0 <= rc[0]) && (rc[0] < N) &&(0 <= rc[1]) && (rc[1] < M);
+        return in;
+    }
+    int N, M;
     ll_t E;
     int Sr, Sc, Tr, Tc;
-    void cell_solve(const ai2_t& rc, const State& state, u_t depth);
     ai2_t source, target;
     mtxcell_t *pmtxcell;
     vai2_t traps;
     vu_t component_value;
+    vnode_t nodes;
+    u_t ni_source_target[2];
     ll_t solution;
 };
 
@@ -115,9 +152,9 @@ Cave::Cave(istream& fi) : pmtxcell(0), solution(0)
     source = ai2_t{Sr - 1, Sc - 1};
     target = ai2_t{Tr - 1, Tc - 1};
     pmtxcell = new mtxcell_t(N, M);
-    for (ull_t r = 0; r < N; ++r)
+    for (int r = 0; r < N; ++r)
     {
-        for (ull_t c = 0; c < M; ++c)
+        for (int c = 0; c < M; ++c)
         {
             int v;
             fi >> v;
@@ -137,7 +174,10 @@ void Cave::solve_naive()
 
 void Cave::solve()
 {
-    solve_naive();
+    solution = -1;
+    build_graph();
+    GState state(E);
+    node_solve_check(ni_source_target[0], state, 0);
 }
 
 void Cave::get_traps()
@@ -158,9 +198,9 @@ void Cave::get_traps()
 void Cave::set_components()
 {
     // init
-    for (u_t r = 0; r < N; ++r)
+    for (int r = 0; r < N; ++r)
     {
-        for (u_t c = 0; c < M; ++c)
+        for (int c = 0; c < M; ++c)
         {
             Cell& cell = pmtxcell->get(r, c);
             if (cell.v >= 0)
@@ -169,9 +209,9 @@ void Cave::set_components()
             }
         }
     }
-    for (u_t r = 0; r < N; ++r)
+    for (int r = 0; r < N; ++r)
     {
-        for (u_t c = 0; c < M; ++c)
+        for (int c = 0; c < M; ++c)
         {
             const Cell& cell = pmtxcell->get(r, c);
             if ((cell.v >= 0) && (cell.color == 0))
@@ -190,12 +230,10 @@ ull_t Cave::dfs_component(const ai2_t& rc, u_t comp_id, ull_t energy)
     cell.color = 1; // gray
     energy += cell.v;
     cell.component = comp_id;
-    for (u_t si = 0; si != 4; ++si)
+    for (const ai2_t& step: steps)
     {
-        const ai2_t& step = steps[si];
         ai2_t arc{rc[0] + step[0], rc[1] + step[1]};
-        if ((0 <= arc[0]) && (arc[0] < int(N)) &&
-            (0 <= arc[1]) && (arc[1] < int(M)))
+        if (rc_inside(arc))
         {
             const Cell& adj = pmtxcell->get(arc[0], arc[1]);
             if ((adj.color == 0) && (adj.v >= 0))
@@ -248,13 +286,164 @@ void Cave::cell_solve(const ai2_t& rc, const State& state, u_t depth)
             {
                 const ai2_t& step = steps[si];
                 ai2_t arc{int(rc[0]) + step[0], int(rc[1]) + step[1]};
-                if ((0 <= arc[0]) && (arc[0] < int(N)) &&
-                    (0 <= arc[1]) && (arc[1] < int(M)))
+                if (rc_inside(arc))
                 {
                     cell_solve(arc, sub_state, depth + 1);
                 }
             }
         }
+    }
+}
+
+void Cave::build_graph()
+{
+    get_traps();
+    set_components();
+    const u_t ncomps = component_value.size();
+    for (u_t ci = 0; ci != ncomps; ++ci)
+    {
+        nodes.push_back(Node(component_value[ci], ci));
+    }
+    const vai2_t::const_iterator tb = traps.begin(), te = traps.end();
+    for (u_t ti = 0, tn = traps.size(), ni = ncomps; ti != tn; ++ti, ++ni)
+    {
+        const ai2_t& trap = traps[ti];
+        nodes.push_back(Node(pmtxcell->get(trap).v, -1, ti));
+        Node& node = nodes.back();
+        for (const ai2_t& step: steps)
+        {
+            ai2_t arc{trap[0] + step[0], trap[1] + step[1]};
+            if (rc_inside(arc))
+            {
+                const Cell& cell = pmtxcell->get(arc);
+                if (cell.v >= 0)
+                {
+                    auto iter_new = node.adjs_temp.insert(cell.component);
+                    if (iter_new.second)
+                    {
+                        nodes[cell.component].adjs_temp.insert(ni);
+                    }
+                }
+                else if (cell.v != OBSTACLE)
+                {
+                    u_t aci = lower_bound(tb, te, arc) - tb;
+                    node.adjs_temp.insert(aci); 
+                }
+            }            
+        }   
+    }
+    for (Node& node: nodes)
+    {
+        setu_t& at = node.adjs_temp;
+        node.adjs.insert(node.adjs.end(), at.begin(), at.end());
+        at.clear();
+    }
+    for (u_t ni = 0;  ni != 2; ++ni)
+    {
+        const ai2_t& rc = ((ni == 0) ? source : target);
+        const Cell& cell = pmtxcell->get(rc);
+        ni_source_target[ni] = ((cell.component >= 0)
+            ? cell.component
+            : ncomps + lower_bound(tb, te, rc) - tb);
+    }
+    if (dbg_flags & 0x1) { show_graph(); }
+}
+
+void Cave::show_graph() const
+{
+    const vai2_t::const_iterator tb = traps.begin(), te = traps.end();
+    for (int r = 0; r < N; ++r)
+    {
+        for (int c = 0; c < M; ++c)
+        {
+            const Cell& cell = pmtxcell->get(r, c);
+            char b[0x10];
+            if (cell.v == OBSTACLE)
+            {
+                strcpy(b, "   ");
+            }
+            else if (cell.component >= 0)
+            {
+                sprintf(b, "C%-2d", cell.component);
+            }
+            else
+            {
+                u_t ti = lower_bound(tb, te, ai2_t{r, c}) - tb;
+                sprintf(b, "T%-2d", ti);
+            }
+            cerr << " " << b;
+        }
+        cerr << '\n';
+    }
+}
+
+void Cave::node_solve_check(u_t node_index, GState& state, u_t depth)
+{
+    Node& node = nodes[node_index];
+    Node::mask_comps_t key{state.traps_mask, state.components};
+    auto iter_added = node.tried.insert(key);
+    if (iter_added.second)
+    {
+        node_solve(node_index, state, depth);
+    }
+}
+
+void Cave::node_solve(u_t node_index, GState& state, u_t depth)
+{
+    Node& node = nodes[node_index];
+    ll_t old_energy = state.energy, new_energy = old_energy;
+    u_t old_traps_mask = state.traps_mask;
+    pair<setu_t::iterator, bool>
+        comp_iter_added = {state.components.end(), false};
+    if (node.component != -1)
+    {
+        comp_iter_added = state.components.insert(node.component);
+        if (comp_iter_added.second)
+        {
+            new_energy += node.energy;
+        }
+    }
+    else // trap
+    {
+        if ((state.traps_mask & (1u << node.trap)) == 0)
+        {
+            new_energy += node.energy; // decrease
+            if (new_energy >= 0)
+            {
+                state.traps_mask |= (1u << node.trap);
+            }
+        }
+    }
+
+    if (dbg_flags & 0x2) { cerr << string(depth, ' ');
+      if (node.component >= 0) { cerr << "C"<<node.component; } else 
+        { cerr << "T"<<node.trap; }
+      cerr << ", Cs=";
+      for (u_t ci: state.components) { cerr << ci << ','; }
+      cerr << " Ts=";
+      for (u_t ti = 0, tn = traps.size(); ti != tn; ti++) { 
+        if (state.traps_mask & (1u << ti)) { cerr << ti << ','; }
+      }
+      cerr << " e="<<state.energy << " -> " << new_energy <<
+        ", depth="<<depth<<'\n';}
+    
+    if (new_energy >= 0)
+    {
+        state.energy = new_energy;
+        if ((node_index == ni_source_target[1]) && (solution < new_energy))
+        {
+            solution = new_energy;
+        }
+        for (u_t ani: node.adjs)
+        {
+            node_solve_check(ani, state, depth + 1);
+        }
+        state.energy = old_energy;
+        state.traps_mask = old_traps_mask;
+    }
+    if (comp_iter_added.second)
+    {
+        state.components.erase(comp_iter_added.first);
     }
 }
 
