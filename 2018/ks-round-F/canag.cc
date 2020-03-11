daae802c542d9b99684fdb32de3aae354e9c66cf
Yotam Medini <yotam.medini@gmail.com>
2020-03-12 01:46:54 +0200

    canag passes naive tiny

---
+++ 2018/ks-round-F/canag.cc
@@ -0,0 +1,164 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+// typedef vector<ul_t> vul_t;
+typedef vector<char> vc_t;
+
+static unsigned dbg_flags;
+
+class CommonAnagram
+{
+ public:
+    CommonAnagram(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    u_t L;
+    string A, B;
+    u_t solution;
+};
+
+CommonAnagram::CommonAnagram(istream& fi) : solution(0)
+{
+    fi >> L >> A >> B;
+}
+
+void CommonAnagram::solve_naive()
+{
+    for (u_t b = 0; b < L; ++b)
+    {
+        for (u_t e = b + 1; e <= L; ++e)
+        {
+            vc_t sa;
+            for (u_t ci = b; ci < e; ++ci)
+            {
+                sa.push_back(A[ci]);
+            }
+            sort(sa.begin(), sa.end());
+
+            bool match = false;
+            for (u_t bb = 0; (bb < L) && !match; ++bb)
+            {
+                for (u_t be = bb + 1; (be <= L) && !match; ++be)
+                {
+                    vc_t sb;
+                    for (u_t ci = bb; ci < be; ++ci)
+                    {
+                        sb.push_back(B[ci]);
+                    }
+                    sort(sb.begin(), sb.end());
+                    match = (sa == sb);
+                    if (match)
+                    {
+                        ++solution;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void CommonAnagram::solve()
+{
+    solve_naive();
+}
+
+void CommonAnagram::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (CommonAnagram::*psolve)() =
+        (naive ? &CommonAnagram::solve_naive : &CommonAnagram::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        CommonAnagram canag(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (canag.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        canag.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
