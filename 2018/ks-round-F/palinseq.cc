bd5545367737cfa619ced063cd849a198f29a1aa
Yotam Medini <yotam.medini@gmail.com>
2020-03-21 17:29:45 +0200

    palinseq: fixes for small cases

--- 2018/ks-round-F/palinseq.cc
+++ 2018/ks-round-F/palinseq.cc
@@ -14,10 +14,40 @@ using namespace std;
 
 typedef unsigned u_t;
 typedef unsigned long ul_t;
+typedef long long ll_t;
 typedef unsigned long long ull_t;
 typedef vector<string> vs_t;
+typedef vector<ull_t> vull_t;
 
 static unsigned dbg_flags;
+
+// short completions to palindromes
+void palicomp(vs_t& palis, const string& s)
+{
+    u_t sz = s.size();
+    palis.clear();
+    for (u_t psz = sz, psze = 2*sz; psz <= psze; ++psz)
+    {
+        u_t half = psz/2;
+        u_t down = half - 1;
+        u_t up = psz - half;
+        bool can = true;
+        for (; can && (up < sz); --down, ++up)
+        {
+            can = (s[down] == s[up]);
+        }
+        if (can)
+        {
+            string pali{s};
+            while (pali.size() < psz)
+            {
+                pali.push_back(s[psz - pali.size() - 1]);
+            }
+            palis.push_back(pali);
+        }
+    }
+    sort(palis.begin(), palis.end());
+}
 
 class PalindromeSequence
 {
@@ -28,7 +58,8 @@ class PalindromeSequence
     void print_solution(ostream&) const;
  private:
     static const char* az;
-    u_t L, N, K;
+    u_t L;
+    ull_t N, K;
     u_t solution;
 };
 const char* PalindromeSequence::az = "abcdefghijklmnopqrstuvwxyz";
@@ -66,9 +97,12 @@ void PalindromeSequence::solve_naive()
         }
     }
     solution = 0;
-    if (K < lang.size())
+    if (K - 1 < lang.size())
     {
         sort(lang.begin(), lang.end());
+        if (dbg_flags & 0x1) {
+          for (const string& s: lang) { cerr << s << '\n'; }
+        }
         const string& target = lang[K - 1];
         solution = target.size();
     }
@@ -76,6 +110,130 @@ void PalindromeSequence::solve_naive()
 
 void PalindromeSequence::solve()
 {
+    vull_t b_sizes, b_sum_sizes;
+    b_sizes.reserve(N + 1);
+    b_sum_sizes.reserve(N + 1);
+    b_sizes.push_back(0);
+    b_sum_sizes.push_back(0);
+    if (N >= 1)
+    {
+        b_sizes.push_back(L);
+        b_sum_sizes.push_back(L);
+    }
+    if (N >= 2) {
+        b_sizes.push_back(L);
+        b_sum_sizes.push_back(L + L);
+    }
+    for (u_t n = 3; n <= N; ++n)
+    {
+        b_sizes.push_back(L * b_sizes[n - 2]);
+        b_sum_sizes.push_back(b_sum_sizes.back() + b_sizes.back());
+    }
+
+    // We need to have an prefix string o the target and the index
+    // of the first palindrom having that prefix.
+    solution = 0;
+    ull_t k = K - 1; // pending to advance
+    ull_t n_all =  b_sum_sizes.back();
+    if (k < n_all)
+    {
+        string s;
+        ull_t cblock = (n_all / L);
+        u_t nb0 = k / cblock;
+        char c = az[nb0];
+        s.push_back(c);
+        k %= cblock;
+        while (k > 0)
+        {
+            --k;
+            u_t sz = s.size();
+            u_t ishort_begin = 0;
+            vs_t short_palis;
+            palicomp(short_palis, s);
+            if ((!short_palis.empty()) && (short_palis[0].size() == sz))
+            {
+                ishort_begin = 1;
+            }
+            if (2*sz < N)
+            {
+                char cnext = 'a';
+                u_t mid = N - 2*sz;
+                ull_t pre_char_size = b_sum_sizes[mid] / L;
+                u_t ishort;
+                for (ishort = ishort_begin; 
+                    (ishort < short_palis.size()) && (s.size() == sz);
+                    ++ishort)
+                {
+                    char cbound = short_palis[ishort][sz];
+                    u_t nb = k / pre_char_size;
+                    u_t nb_low = u_t(cbound - cnext);
+                    if (nb < nb_low)
+                    {
+                        k -= nb * pre_char_size;
+                        cnext += nb;
+                        s.push_back(cnext);
+                    }
+                    else
+                    {
+                        cnext = cbound;
+                        ull_t kbound = (nb_low + 1) * pre_char_size + 1;
+                        if (k < kbound)
+                        {
+                            s.push_back(cbound);
+                            k -= nb_low * pre_char_size;
+                        }
+                        else
+                        {
+                            cnext = cbound + 1;
+                            k -= kbound;
+                        }
+                    }
+                }
+                if (s.size() == sz)
+                {
+                    ull_t nb = k / pre_char_size;
+                    cnext += nb;
+                    if (u_t(cnext - 'a') < L)
+                    {
+                        s.push_back(cnext);
+                    }
+                    else
+                    {
+                        cerr << "Error:"<<__LINE__<< " k="<<k <<
+                            ", s="<<s << '\n';
+                        exit(1);
+                    }
+                    k -= nb * pre_char_size;
+                }
+            } 
+            else
+            {
+                k += ishort_begin;
+                if (k < short_palis.size())
+                {
+                    s = short_palis[k];
+                    k = 0;
+                }
+                else
+                {
+                    cerr << "Error:"<<__LINE__<<", k="<<k <<", s="<<s<<'\n';
+                    exit(1);
+                }
+            }
+        }
+        vs_t palis;
+        palicomp(palis, s);
+        s.clear();
+        for (u_t si = 0; (si < palis.size()) && s.empty(); ++si)
+        {
+            const string& p = palis[si];
+            if (p.size() <= N)
+            {
+                s = p;
+            }
+        }
+        solution = s.size();
+    }
 }
 
 void PalindromeSequence::print_solution(ostream &fo) const
