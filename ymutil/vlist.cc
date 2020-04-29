728525ea66a826e4a75e9cb02cdf11294bf4261a
Yotam Medini <yotam.medini@gmail.com>
2020-04-29 12:41:50 +0300

    vlist: += copt-constructor, operator=

--- ymutil/vlist.cc
+++ ymutil/vlist.cc
@@ -8,6 +8,25 @@ class VList
      typedef std::list<u_t> lu_t;
      typedef lu_t::const_iterator citer_t;
      VList() : sz(0) {}
+     VList(const VList& x) : sz(0)
+     {
+         for (citer_t i = x.begin(), e = x.end(); i != e; ++i)
+         {
+             add(*i);
+         }
+     }
+     VList& operator=(const VList& rhs)
+     {
+         if (this != &rhs)
+         {
+             clear();
+             for (citer_t i = rhs.begin(), e = rhs.end(); i != e; ++i)
+             {
+                 add(*i);
+             }
+         }
+         return *this;
+     }
      u_t size() const { return sz; }
      bool empty() const { return sz == 0; }
      u_t capacity() const { return v.size(); }
@@ -40,6 +59,12 @@ class VList
      }
      citer_t begin() const { return l.begin(); }
      citer_t end() const { return l.end(); }
+     void clear()
+     {
+         v.clear();
+         l.clear();
+         sz = 0;
+     }
  private:
      typedef lu_t::iterator iter_t;
      typedef std::vector<iter_t> viter_t;
@@ -52,8 +77,9 @@ class VList
 using namespace std;
 typedef unsigned u_t;
 
-static void show(const VList& vl)
+static void show(const char* msg, const VList& vl)
 {
+    cout << '\n' << msg << '\n';
     cout << "V:";
     u_t vsz = 0, lsz = 0;
     for (u_t n = 0; n < vl.capacity(); ++n)
@@ -85,7 +111,6 @@ int main(int, char **)
         u_t n = (i*p1) % N;
         vl.add(n);
     }
-    // show(vl);
     for (u_t i = 0; i < N; ++i)
     {
         u_t n = (i*p2) % N;
@@ -94,6 +119,21 @@ int main(int, char **)
             vl.del(n);
         }
     }
-    show(vl);
+    show("vl 8", vl);
+
+    VList vlc(vl);
+    for (u_t i = 0; i < N; ++i)
+    {
+        u_t n = (i*p2) % N;
+        if ((n % 8 == 0) && (n % 16 != 0))
+        {
+            vlc.del(n);
+        }
+    }
+    show("vlc 16", vlc);
+    show("vl 8 again", vl);
+    vl = vlc;
+    show("vl 16", vl);
+
     return 0;
 }
