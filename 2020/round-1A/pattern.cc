d809eec37b1c23d25db983d09264655f4d974745
Yotam Medini <yotam.medini@gmail.com>
2020-05-20 23:26:03 +0300

    pattern: clean obsolete code

--- 2020/round-1A/pattern.cc
+++ 2020/round-1A/pattern.cc
@@ -19,14 +19,11 @@ using namespace std;
 typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
-typedef vector<string> vs_t;;
-typedef vector<u_t> vu_t;;
-typedef set<char> setc_t;;
-typedef set<u_t> setu_t;;
-typedef set<vu_t> setvu_t;;
-typedef queue<setu_t> qsetu_t;;
-typedef queue<vu_t> qvu_t;;
-typedef map<vu_t, char> vutoc_t;
+typedef vector<string> vs_t;
+typedef vector<u_t> vu_t;
+typedef set<char> setc_t;
+typedef set<vu_t> setvu_t;
+typedef queue<vu_t> qvu_t;
 typedef pair<vu_t, char> vuc_t;
 typedef map<vu_t, vuc_t> vu_to_vuc_t;
 
@@ -67,10 +64,6 @@ class Pattern
     bool q_visits(const vu_t& node, char c);
     bool visit(const vu_t& parent, char c, const vu_t& child);
     void build_solution();
-    // void build_pautomata();
-    // void build_automata();
-    void next_bump(vu_t& next_node, u_t pi) const;
-    bool alt_get(char& cnext, u_t pi, u_t ci) const;
     u_t n;
     vs_t p;
     vs_t pat_reduced;
@@ -78,14 +71,6 @@ class Pattern
     vu_t initial_node;
     vu_t final_node;
     qvu_t q;
-    // setu_t initial_node;
-    // setu_t final_node;
-    // vvnode_t pautomata;
-    // vnondetnode_t nd_automata; // non-deterministic
-    // vu_t initial_nodes;
-    // vu_t final_nodes;
-    // setu_t initial_node;
-    // setu_t final_node;
     setvu_t visited;
     vu_to_vuc_t parent;
 };
@@ -95,46 +80,6 @@ Pattern::Pattern(istream& fi)
     fi >> n;
     copy_n(istream_iterator<string>(fi), n, back_inserter(p));
 }
-
-#if 0
-void Pattern::solve_naive()
-{
-    bool possible = true, end = false;
-    vu_t idx{n, 0};
-    while (possible && !end)
-    {
-        char next = ' ';
-        bool progress = false;
-        for (u_t pi = 0; pi < n; ++pi)
-        {
-            char c = p[pi][idx[pi]];
-            if (c != '*')
-            {
-                if (next == ' ')
-                {
-                    solution.push_back(c);
-                    progress = true;
-                }
-                else
-                {
-                    possible = possible && (c == solution.back());
-                }
-            }
-        }
-        if (!progress)
-        {
-            // skip asterisks
-            for (u_t pi = 0; pi < n; ++pi)
-            {
-                u_t ci = idx[pi];
-                const string& pi = p[pi];
-                for (; ci < 
-            }
-            
-        }
-    }
-}
-#endif
 
 void Pattern::solve_naive()
 {
@@ -262,27 +207,6 @@ bool Pattern::q_visits(const vu_t& node, char c)
     return finished;
 }
 
-void Pattern::next_bump(vu_t& next_node, u_t pi) const
-{
-    if (next_node[pi] < pat_reduced[pi].size())
-    {
-        ++next_node[pi];
-    }
-}
-
-#if 0
-bool Pattern::alt_get(char& cnext, u_t pi, u_t ci) const
-{
-    const string& pat = pat_reduced[pi];
-    bool alt = (pat[ci] == '*') && ((ci + 1) < pat.size());
-    if (alt)
-    {
-        cnext = pat[ci + 1];
-    }
-    return alt;
-}
-#endif
-
 bool Pattern::visit(const vu_t& pnode, char c, const vu_t& child)
 {
     auto er = visited.equal_range(child);
@@ -314,92 +238,6 @@ void Pattern::build_solution()
         reverse(solution.begin(), solution.end());
     }
 }
-
-#if 0
-void Pattern::solve()
-{
-    solve_naive();
-    build_pautomata();
-    build_automata();
-}
-#endif
-
-#if 0
-void Pattern::build_pautomata()
-{
-    initial_nodes.reserve(n);
-    final_nodes.reserve(n);
-    pautomata.reserve(n);
-    u_t nn = 0;
-    for (const string& pat: p)
-    {
-        initial_nodes.push_back(nn);
-        vnode_t automata;
-        for (u_t ci = 0, ce = pat.size(); ci < ce; )
-        {
-            char c = pat[ci];
-            bool self = (c == '*');
-            if (self)
-            {
-                for (; (ci + 1 < ce) && (pat[ci] == '*'); ++ci) {}
-            }
-            if (ci + 1 < ce)
-            {
-                ++ci;
-                ++n;
-                c = pat[ci];
-            }
-            automata.push_back(Node(c, nn, self));
-        }
-        final_nodes.push_back(automata.back().next);
-        pautomata.push_back(automata);
-    }
-}
-
-void Pattern::build_automata()
-{
-    // initial_node = setu_t{initial_nodes.begin(), initial_nodes.end()};
-    qvu_t q;
-    q.push(initial_nodes);
-    
-    setvu_t visited;
-    possible = true;
-    while (!q.empty() && possible)
-    {
-        vu_t node = q.front();
-        q.pop();
-        visited.insert(visited.end() node);
-        setc_t cs;
-        u_t nc = 0;
-        vu_t alts;
-        for (u_t pi = 0; pi != n; ++pi)
-        {
-            
-            if (pnode.c != '*')
-            {
-                cs.insert(cs.end(), c)
-                if (pnode.self)
-                {
-                    ++n_alt;
-                }
-                else
-                {
-                    ++nc;
-                }
-            }
-        }
-        possible = possible && (nc <= 1);
-        for (char c: cs)
-        {
-            for (u_t pi = 0; pi < n; ++pi)
-            {
-                
-            }
-        }
-    }
-    
-}
-#endif
 
 void Pattern::solve()
 {
