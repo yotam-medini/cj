947679aba4bf824678a46f98bc2687aef119a62d
Yotam Medini <yotam.medini@gmail.com>
2020-06-27 19:50:56 +0300

    yogurt: passed naive tiny case

---
+++ 2018/ks-round-E/yogurt.cc
@@ -0,0 +1,149 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+typedef vector<u_t> vu_t;
+
+static unsigned dbg_flags;
+
+class Yogurt
+{
+ public:
+    Yogurt(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    u_t N, K;
+    vu_t a;
+    u_t solution;
+};
+
+Yogurt::Yogurt(istream& fi) : solution(0)
+{
+    fi >> N >> K;
+    a.reserve(N);
+    copy_n(istream_iterator<u_t>(fi), N, back_inserter(a));
+}
+
+void Yogurt::solve_naive()
+{
+   vu_t sa(a);
+   sort(sa.begin(), sa.end());
+   for (u_t day = 0, i = 0; i < N; ++day)
+   {
+       u_t ate = 0;
+       for (; (i < N) && (ate < K); ++i)
+       {
+           if (day < sa[i])
+           {
+               ++ate;
+           }
+       }
+       solution += ate;
+   }
+}
+
+void Yogurt::solve()
+{
+    solve_naive();
+}
+
+void Yogurt::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Yogurt::*psolve)() =
+        (naive ? &Yogurt::solve_naive : &Yogurt::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Yogurt yogurt(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (yogurt.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        yogurt.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
