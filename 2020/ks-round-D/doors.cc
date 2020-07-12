e0081f3a7102ebf9b65b0f12ee18bde511314305
Yotam Medini <yotam.medini@gmail.com>
2020-07-12 23:02:15 +0300

    2020/ks-round-D as submitted pre practice

---
+++ 2020/ks-round-D/doors.cc
@@ -0,0 +1,197 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+typedef vector<u_t> vu_t;
+typedef vector<bool> vb_t;
+
+class Query
+{
+ public:
+    Query(u_t _s=0, u_t _k=0): s(_s), k(_k) {}
+    u_t s, k;
+};
+typedef vector<Query> vq_t;
+
+static unsigned dbg_flags;
+
+class Doors
+{
+ public:
+    Doors(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    u_t run_query(const Query& q);
+    u_t N, Q;
+    vu_t d;
+    vq_t qs;
+    vu_t solution;
+};
+
+Doors::Doors(istream& fi)
+{
+    fi >> N >> Q;
+    d.reserve(N - 1);
+    qs.reserve(Q);
+    copy_n(istream_iterator<u_t>(fi), N - 1, back_inserter(d));
+    for (u_t i = 0; i < Q; ++i)
+    {
+        u_t s, k;
+        fi >> s >> k;
+        Query q(s- 1, k - 1);
+        qs.push_back(q);
+    }
+}
+
+void Doors::solve_naive()
+{
+    for (const Query& query: qs)
+    {
+        u_t s = run_query(query);
+        solution.push_back(s);
+    }
+}
+
+u_t Doors::run_query(const Query& q)
+{
+    u_t ret = q.s;
+    // vu_t locked(N - 1, true);
+    int lock_left = q.s - 1;
+    u_t lock_right = q.s;
+    for (u_t i = 0; i < q.k; ++i)
+    {
+        if (lock_left < 0)
+        {
+            ++lock_right;
+            ret = lock_right;
+        }
+        else if (lock_right + 1 == N)
+        {
+            ret = lock_left;
+            --lock_left;
+        }
+        else
+        {
+            if (d[lock_left] < d[lock_right])
+            {
+                ret = lock_left;
+                --lock_left;
+            }
+            else
+            {
+                ++lock_right;
+                ret = lock_right;
+            }
+        }
+    }
+    return ret;
+}
+
+void Doors::solve()
+{
+    solve_naive();
+}
+
+void Doors::print_solution(ostream &fo) const
+{
+    for (u_t s: solution)
+    {
+        fo << ' ' << s + 1;
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Doors::*psolve)() =
+        (naive ? &Doors::solve_naive : &Doors::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Doors doors(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (doors.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        doors.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
