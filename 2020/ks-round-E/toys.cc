fccdcb821a3985e8cd9474cf2684bb5eee1ac985
Yotam Medini <yotam.medini@gmail.com>
2020-11-04 20:53:38 +0200

    toys: non-naive passed tiny case

--- 2020/ks-round-E/toys.cc
+++ 2020/ks-round-E/toys.cc
@@ -1,11 +1,13 @@
 // CodeJam
 // Author:  Yotam Medini  yotam.medini@gmail.com --
 
-// #include <algorithm>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <array>
 #include <string>
 #include <utility>
+#include <set>
 #include <vector>
 
 #include <cstdlib>
@@ -14,8 +16,15 @@ using namespace std;
 
 typedef unsigned u_t;
 typedef unsigned long ul_t;
+typedef long long ll_t;
 typedef unsigned long long ull_t;
 typedef vector<u_t> vu_t;
+typedef vector<ull_t> vull_t;
+
+// typedef array<u_t, 2> au2_t;
+// typedef vector<au2_t, 2> vau2_t;
+typedef pair<ll_t, u_t> extra_idx_t;
+typedef vector<extra_idx_t> vextra_idx_t;
 
 static unsigned dbg_flags;
 
@@ -23,11 +32,11 @@ class Toy
 {
  public:
     Toy(u_t _e=0, u_t _r=0) : e(_e), r(_r) {}
-    u_t e, r;
+    ull_t e, r;
 };
 typedef vector<Toy> vtoy_t;
 
-static const u_t infi(-1);
+static const ull_t infi(-1);
 
 class Toys
 {
@@ -37,10 +46,13 @@ class Toys
     void solve();
     void print_solution(ostream&) const;
  private:
-    u_t time(const vtoy_t& subtoys) const;
+    ull_t time(const vtoy_t& subtoys) const;
+    bool gready_infi();
+    void gready_max();
     u_t n;
     vtoy_t toys;
-    u_t y, z;
+    u_t y;
+    ull_t z;
 };
 
 Toys::Toys(istream& fi) : y(0), z(0)
@@ -50,8 +62,8 @@ Toys::Toys(istream& fi) : y(0), z(0)
     for (u_t i = 0; i < n; ++i)
     {
         u_t e, r;
-	fi >> e >> r;
-	toys.push_back(Toy(e, r));
+        fi >> e >> r;
+        toys.push_back(Toy(e, r));
     }
 }
 
@@ -62,37 +74,37 @@ void Toys::solve_naive()
     for (u_t r = 1; (r < n) && (z != infi); ++r)
     {
         for (u_t mask = 0; (mask < (1u << n)) && (z != infi); ++mask)
-	{
+        {
             vtoy_t subtoys;
-	    u_t n_bits = 0;
-	    for (u_t bi = 0; bi < n; ++bi)
-	    {
-	        if (mask & (1u << bi))
-		{
-		    ++n_bits;
-		}
-		else
-		{
-		    subtoys.push_back(toys[bi]);
-		}
-	    }
-	    if (n_bits == r)
-	    {
-	        u_t t = time(subtoys);
-		if ((t == infi) || (z < t))
-		{
-		    y = n_bits;
-		    z = t;		    
-		}
-	    }
-	}
+            u_t n_bits = 0;
+            for (u_t bi = 0; bi < n; ++bi)
+            {
+                if (mask & (1u << bi))
+                {
+                    ++n_bits;
+                }
+                else
+                {
+                    subtoys.push_back(toys[bi]);
+                }
+            }
+            if (n_bits == r)
+            {
+                const ull_t t = time(subtoys);
+                if ((t == infi) || (z < t))
+                {
+                    y = n_bits;
+                    z = t;
+                }
+            }
+        }
     }
 }
 
-u_t Toys::time(const vtoy_t& subtoys) const
+ull_t Toys::time(const vtoy_t& subtoys) const
 {
-    u_t t = infi;
-    u_t etotal = 0;
+    ull_t t = infi;
+    ull_t etotal = 0;
     for (const Toy& toy: subtoys)
     {
         etotal += toy.e;
@@ -104,29 +116,134 @@ u_t Toys::time(const vtoy_t& subtoys) const
     }
     if (will_cry)
     {
-        // simulater
-	t = 0;
-	bool cry = false;
-	vu_t tleft(subtoys.size(), 0);
-	u_t i = 0;
-	while (!cry)
-	{
-	    const Toy& toy = subtoys[i];
+        // simulate
+        t = 0;
+        bool cry = false;
+        vull_t tleft(subtoys.size(), 0);
+        u_t i = 0;
+        while (!cry)
+        {
+            const Toy& toy = subtoys[i];
             cry = (tleft[i] > 0) && (t - tleft[i] < toy.r);
-	    if (!cry)
-	    {
-	        t += toy.e;
-		tleft[i] = t;
-	    }
-	    i = (i + 1) % (subtoys.size());
-	}
+            if (!cry)
+            {
+                t += toy.e;
+                tleft[i] = t;
+            }
+            i = (i + 1) % (subtoys.size());
+        }
     }
     return t;
 }
 
 void Toys::solve()
 {
-    solve_naive();
+    y = 0;
+    if (!gready_infi())
+    {
+        gready_max();
+    }
+}
+
+bool Toys::gready_infi()
+{
+    ull_t etotal = 0;
+    for (const Toy& toy: toys)
+    {
+        etotal += toy.e;
+    }
+    bool will_cry = false;
+    for (const Toy& toy: toys)
+    {
+        will_cry = will_cry || (etotal - toy.e < toy.r);
+    }
+    bool can = !will_cry;
+    if (!can)
+    {
+        vextra_idx_t extra_idx; extra_idx.reserve(n);
+        for (u_t i = 0; i < n; ++i)
+        {
+            const Toy& toy = toys[i];
+            ll_t e = etotal - toy.e;
+            ll_t extra = e - ll_t(toy.r);
+            extra_idx.push_back(extra_idx_t(extra, i));
+        }
+        sort(extra_idx.begin(), extra_idx.end());
+        u_t ndel = 0;
+        ll_t extra_del = 0;
+        for (ndel = 0; (ndel + 2 < n) && !can; ++ndel)
+        {
+            const u_t idel = extra_idx[ndel].second;
+            const Toy& toy_del = toys[idel];
+            extra_del += toy_del.e;
+            const extra_idx_t& ei_check = extra_idx[ndel + 1];
+            can = ei_check.first >= extra_del;
+        }
+        if (can) { y = ndel; }
+    }
+    if (can)
+    {
+        z = infi;
+    }
+
+    return can;
+}
+
+void Toys::gready_max()
+{
+    ull_t e_total = 0;
+    for (const Toy& toy: toys)
+    {
+        e_total += toy.e;
+    }
+    z = e_total;
+    ll_t e_deleted = 0;
+    ull_t tcandid = z;
+    set<extra_idx_t> considered;
+    for (u_t i = 0; i < n; ++i)
+    {
+        const Toy& toy = toys[i];
+        ll_t extra = (e_total - toy.e) - toy.r;
+        if (extra >= 0)
+        {
+            tcandid += toy.e;
+            if (z < tcandid)
+            {
+                z = tcandid;
+            }
+            considered.insert(considered.end(), extra_idx_t(extra, i));
+        }
+        else
+        {
+            // If we drop toy. we should decrease tcandid for pre-failing
+            tcandid -= toy.e;
+            e_deleted += toy.e;
+            bool erasing = true;
+            while (erasing)
+            {
+                erasing = false;
+                for (set<extra_idx_t>::iterator iter = considered.begin(),
+                    iter_next = iter;
+                    (iter != considered.end()); iter = iter_next)
+                {
+                     iter_next = iter; ++iter_next;
+                     const extra_idx_t& ei = *iter;
+                     if (ei.first < e_deleted)
+                     {
+                         const Toy& toy_del = toys[ei.second];
+                         tcandid -= 2*toy_del.e;
+                         e_deleted += toy_del.e;
+                         considered.erase(iter);
+                         erasing = true;
+                     }
+                     else
+                     {
+                         iter_next = considered.end(); // exit-loop
+                     }
+                }
+            }
+        }
+    }
 }
 
 void Toys::print_solution(ostream &fo) const
