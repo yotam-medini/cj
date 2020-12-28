f83656d644f971aca6e5b3d62d453e7b93c08cf8
Yotam Medini <yotam.medini@gmail.com>
2020-12-28 18:11:27 +0200

    firecrack under construction

---
+++ codeforces/1468/firecrack.cc
@@ -0,0 +1,143 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+typedef vector<u_t> vu_t;
+
+static unsigned dbg_flags;
+
+class Firecrack
+{
+ public:
+    Firecrack(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+ private:
+    u_t n, m, a, b;
+    vu_t s;
+    u_t solution;
+};
+
+Firecrack::Firecrack(istream& fi) : solution(0)
+{
+    fi >> n >> m >> a >> b;
+    copy_n(istream_iterator<u_t>(fi), m, back_inserter(s));
+}
+
+void Firecrack::solve_naive()
+{
+    u_t lit_time = (a < b ? b - a : a - b);
+    u_t chase_time = (a < b ? b - 1 : n - b);
+    sort(s.begin(), s.end());
+    int nc = (lit_time < m ? lit_time : m);
+    for (int ci = nc; ci > 0; --chase_time)
+    {
+        if (s[--ci] <= chase_time)
+	{
+	    ++solution;
+	}
+    }
+}
+
+void Firecrack::solve()
+{
+    solve_naive();
+}
+
+void Firecrack::print_solution(ostream &fo) const
+{
+    fo << solution << '\n';
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Firecrack::*psolve)() =
+        (naive ? &Firecrack::solve_naive : &Firecrack::solve);
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Firecrack firecrack(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (firecrack.*psolve)();
+        firecrack.print_solution(fout);
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
