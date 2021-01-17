e8e5082470b2dc7d539c2d0b0f144be4ff9d019f
Yotam Medini <yotam.medini@gmail.com>
2021-01-17 20:53:42 +0200

    sherbit: non-naive based on published analysis solves tiny case

--- 2018/ks-round-B/sherbit.cc
+++ 2018/ks-round-B/sherbit.cc
@@ -2,10 +2,12 @@
 // Author:  Yotam Medini  yotam.medini@gmail.com --
 
 #include <algorithm>
+#include <functional>
 #include <array>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <numeric>
 #include <set>
 #include <string>
@@ -19,11 +21,8 @@ using namespace std;
 typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
-typedef set<u_t> setu_t;
 typedef vector<u_t> vu_t;
 typedef array<u_t, 2> au2_t;
-typedef vector<au2_t> vau2_t;
-typedef map<u_t, au2_t> u_to_au2_t;
 
 static unsigned dbg_flags;
 
@@ -40,94 +39,16 @@ string u2str(ull_t x)
     return rs;
 }
 
-u_t gcd(u_t m, u_t n)
-{
-   while (n)
-   {
-      u_t t = n;
-      n = m % n;
-      m = t;
-   }
-   return m;
-}
-
-u_t choose(u_t n, u_t k)
-{
-    if (k > n/2)
-    {
-        k = n - k;
-    }
-    vu_t high, low;
-    high = vu_t(vu_t::size_type(k), 0);
-    iota(high.begin(), high.end(), n - k + 1);
-    u_t d = 2;
-    while (d <= k)
-    {
-        u_t dd = d;
-        for (vu_t::iterator i = high.begin(), e = high.end();
-            (dd > 1) && (i != e); ++i)
-        {
-            u_t g = gcd(dd, *i);
-            if (g > 1)
-            {
-                *i /= g;
-                dd /= g;
-            }
-        }
-        ++d;
-    }
-    u_t r = accumulate(high.begin(), high.end(), 1, multiplies<u_t>());
-    return r;
-}
-
-u_t choose_cache(u_t n, u_t k)
-{
-    typedef pair<u_t, u_t> uu_t;
-    typedef map<uu_t, u_t> uu2u_t;
-    static uu2u_t nk2c;
-
-    u_t ret = 0;
-    uu2u_t::key_type nk(n, k);
-    pair<uu2u_t::iterator, uu2u_t::iterator> w = nk2c.equal_range(nk);
-    if (w.first == w.second)
-    {
-        ret = choose(n, k);
-        uu2u_t::value_type v(nk, ret);
-        nk2c.insert(w.first, v);
-    }
-    else
-    {
-        ret = (*w.first).second;
-    }
-    return ret;
-}
-
-static void minby(u_t& v, const u_t x)
-{
-    if (v > x)
-    {
-        v = x;
-    }
-}
-
-static void maxby(u_t& v, const u_t x)
-{
-    if (v < x)
-    {
-        v = x;
-    }
-}
-
 class ABC
 {
  public:
     ABC(u_t _a=0, u_t _b=0, u_t _c=0) : a(_a), b(_b), c(_c) {}
-    bool legal(const ull_t n) const
+    bool Nlegal(u_t N, const ull_t n) const
     {
         u_t n1 = 0;
-	for (u_t bi = a - 1; bi <= b - 1; ++bi)
+	for (u_t bi = a; bi <= b; ++bi)
 	{
-	    if ((n & (ull_t(1) << bi)) != 0)
+	    if ((n & (ull_t(1) << (N - bi))) != 0)
 	    {
 	        ++n1;
 	    }
@@ -148,50 +69,40 @@ class Constraint
 {
  public:
     Constraint(const au2_t& _be={0, 0}, u_t _nb=0) : be(_be), nbits(_nb) {}
-    Constraint(const ABC& abc) : be{abc.a, abc.b + 1}, nbits(abc.c) {}
+    Constraint(const ABC& abc) : be{abc.a - 1, abc.b}, nbits(abc.c) {}
     u_t size() const { return be[1] - be[0]; }
     au2_t be;
     u_t nbits;
-    vu_t subs;
 };
 bool operator<(const Constraint& c0, const Constraint& c1)
 {
-    bool lt = (c0.be < c1.be);
+    bool lt = (c0.be[1] < c1.be[1]) ||
+         ((c0.be[1] || c1.be[1]) && (c0.be[0] < c1.be[0]));
     return lt;
 }
 typedef vector<Constraint> vconstraint_t;
 
-class SubConstraint
+class XL
 {
  public:
-    SubConstraint(const au2_t& _be={0, 0}, u_t _cmin=0, u_t _cmax=u_t(-1)) :
-        be(_be), cmin(_cmin), cmax(_cmax), sub_solution(0) {}
-    au2_t be;
-    u_t size() const { return be[1] - be[0]; }
-    u_t cmin, cmax;
-    vu_t parents;
-    vu_t end_parents;
-    ull_t sub_solution;
+    XL(u_t _x=0, u_t _last=0) : x(_x), last(_last) {}
+    u_t x;
+    u_t last;
 };
-bool operator<(const SubConstraint& c0, const SubConstraint& c1)
+bool operator==(const XL& xl0, const XL& xl1)
 {
-    bool lt = (c0.be < c1.be);
-    return lt;
+    bool eq = (xl0.x == xl1.x) && (xl0.last == xl1.last);
+    return eq;
 }
-typedef vector<SubConstraint> vsubconstraint_t;
 
-#if 0
-class SubBits : public au2_t
-{
+class HashXL {
  public:
-    SubBits(u_t i=0, u_t nb=0) : au2_t({i, nb}) {}
-    u_t subc_idx() const { return (*this)[0]; }
-    void subc_idx(u_t i) { (*this)[0] = i; }
-    u_t pre_bits() const { return (*this)[1]; }
-    void pre_bits(u_t nb) { (*this)[1] = nb; }
+  size_t operator()(const XL& xl) const noexcept {
+    u_t u = (xl.x << 16) | xl.last;
+    return hash<int>()(u);
+  }
 };
-typedef map<SubBits, ull_t> sb2ull_t;
-#endif
+typedef unordered_map<XL, ull_t, HashXL> xl2ull_t;
 
 class SherBit
 {
@@ -201,19 +112,19 @@ class SherBit
     void solve();
     void print_solution(ostream&) const;
  private:
+    static const ull_t MAXP = 1000000000000000000 + 1; // 10^18 + 1
     bool legal(const ull_t n) const;
     void set_sconstraints();
-    ull_t sub_solve(const vu_t& pre_bits);
-    ull_t parent_pre_bits(u_t pi, const vu_t& pre_bits) const;
+    ull_t comp_pfx_nlegals(u_t x, u_t last);
+    bool legal_segment(u_t x, u_t last) const;
+    void build_solution();
     u_t N;
     u_t K;
     ull_t P;
     vabc_t constraints;
     vconstraint_t sconstraints;
-    vsubconstraint_t sub_cstr;
     string solution;
-    typedef map<vu_t, ull_t> memo_t;
-    memo_t memo;
+    xl2ull_t pfx2_nlegals;
 };
 
 SherBit::SherBit(istream& fi)
@@ -254,7 +165,7 @@ bool SherBit::legal(const ull_t n) const
         ret && (i != e); ++i)
     {
         const ABC& constraint = *i;
-	ret = constraint.legal(n);
+	ret = constraint.Nlegal(N, n);
     }
     return ret;
 }
@@ -262,143 +173,106 @@ bool SherBit::legal(const ull_t n) const
 void SherBit::solve()
 {
     set_sconstraints();
-    sub_solve(vu_t());
+    if (comp_pfx_nlegals(0, 0) > P)
+    {
+        comp_pfx_nlegals(0, 1);
+    }
+    build_solution();
 }
 
 void SherBit::set_sconstraints()
 {
     sconstraints = vconstraint_t{constraints.begin(), constraints.end()};
     sort(sconstraints.begin(), sconstraints.end());
-    setu_t bes;
-    for (const Constraint& c: sconstraints)
-    {
-        bes.insert(bes.end(), c.be[0]);
-        bes.insert(bes.end(), c.be[1]);
-    }
-    setu_t::const_iterator iter = bes.begin();
-    for (u_t b = *iter, e = *(++iter); iter != bes.end(); b = e, e = *(++iter))
-    {
-        sub_cstr.push_back(SubConstraint(au2_t{b, e}));
-    }
-
-    // cmin pass
-    for (u_t ci = 0; ci != K; ++ci)
-    {
-        Constraint& c = sconstraints[ci];
-	SubConstraint point(au2_t{c.be[0], c.be[0]});
-        u_t scb = upper_bound(sub_cstr.begin(), sub_cstr.end(), point) -
-	    sub_cstr.begin();
-	u_t sce = scb;
-	for (; (sce < sub_cstr.size()) && (sub_cstr[sce].be[1] <= c.be[1]);
-	     ++sce)
-	{
-	    c.subs.push_back(sce);
-	    if (sub_cstr[sce].be[1] == c.be[1])
-	    {
-	        sub_cstr[sce].end_parents.push_back(ci);
-	    }
-	    else
-	    {
-                sub_cstr[sce].parents.push_back(ci);
-	    }
-	}
-	for (u_t sci = scb; sci != sce; ++sci)
-	{
-	    SubConstraint& subc = sub_cstr[sci];
-	    u_t other_size = c.size() - subc.size();
-	    u_t cmin = c.nbits - min(other_size, c.nbits);
-	    maxby(subc.cmin, cmin);
-	}
-    }
-    // cmax pass
-    for (u_t ci = 0; ci != K; ++ci)
-    {
-        const Constraint& c = sconstraints[ci];
-	u_t sum_cmin = 0;
-	SubConstraint point(au2_t{c.be[0], c.be[0]});
-        u_t scb = upper_bound(sub_cstr.begin(), sub_cstr.end(), point) -
-	    sub_cstr.begin();
-	u_t sce = scb;
-	for (; (sce < sub_cstr.size()) && (sub_cstr[sce].be[1] <= c.be[1]);
-	     ++sce)
-	{
-	    const SubConstraint& subc = sub_cstr[sce];
-	    sum_cmin += subc.cmin;
-	}
-	for (u_t sci = scb; sci != sce; ++sci)
-	{
-	    SubConstraint& subc = sub_cstr[sci];
-	    u_t other_cmin = sum_cmin - subc.cmin;
-	    u_t cmax = c.nbits - min(other_cmin, c.nbits);
-	    minby(subc.cmax, cmax);
-	}
-    }
 }
 
-ull_t SherBit::sub_solve(const vu_t& pre_bits)
+// How many starting upto NOT including x, and last is bitwise preceding
+ull_t SherBit::comp_pfx_nlegals(u_t x, u_t last)
 {
-    // SubBits key{subi, pre_bits};
-    u_t subi = pre_bits.size();
-    auto er = memo.equal_range(pre_bits);
-    memo_t::iterator iter = er.first;
-    if (er.first == er.second)
+    ull_t ret = 0;
+    XL key(x, last);
+    xl2ull_t::iterator iter = pfx2_nlegals.find(key);
+    if (iter == pfx2_nlegals.end())
     {
-         ull_t nnn = 0;
-	 SubConstraint& sc = sub_cstr[subi];
-	 vu_t sub_pre_bits(pre_bits);
-	 for (u_t my_bits = sc.cmin; my_bits <= sc.cmax; ++my_bits)
-	 {
-	     bool can = true;
-	     for (u_t ci = 0, ce = sc.parents.size(); can && (ci != ce); ++ci)
-	     {
-	         u_t pi = sc.parents[ci];
-		 u_t nb = parent_pre_bits(pi, pre_bits) + my_bits;
-		 can = nb <= sconstraints[pi].nbits;
-	     }
-	     for (u_t ci = 0, ce = sc.end_parents.size(); can && (ci != ce);
-	          ++ci)
-	     {
-	         u_t pi = sc.end_parents[ci];
-		 u_t nb = parent_pre_bits(pi, pre_bits) + my_bits;
-		 can = nb == sconstraints[pi].nbits;
-	     }
-	     if (can)
-	     {
-	         ull_t tail = 1;
-		 if (subi + 1 < sub_cstr.size())
-		 {
-		     sub_pre_bits.push_back(my_bits);
-		     tail = sub_solve(sub_pre_bits);
-		     sub_pre_bits.pop_back();
-		 }
-		 // chosse ...
-		 ull_t choose_my = choose(sc.size(), my_bits);
-		 ull_t ct = choose_my * tail;
-		 if (ct >= P)
-		 {
-		 
-		 }
-	     }
-	 }
-	 memo_t::value_type v{pre_bits, nnn};
-	 iter = memo.insert(iter, v);
+        if (legal_segment(x, last))
+        {
+            if (x == N - 1)
+            {
+                ret = 1;
+            }
+            else
+            {
+                const u_t sub_last0 = (x < 16 ? last : last >> 1);
+                for (u_t bit = 0; (bit < 2) && (ret < P); ++bit)
+                {
+                    u_t sub_last = sub_last0 | (bit << min<u_t>(x + 1, 15));
+                    ret += comp_pfx_nlegals(x + 1, sub_last);
+                }
+            }
+        }
+        xl2ull_t::value_type v(key, ret);
+        pfx2_nlegals.insert(iter, v);
     }
-    ull_t ret = iter->second;
+    else
+    {
+        ret = iter->second;
+    }
     return ret;
 }
 
-ull_t SherBit::parent_pre_bits(u_t pi, const vu_t& pre_bits) const
+// upto including x, last covers including upto it 
+bool SherBit::legal_segment(u_t x, u_t last) const
 {
-    u_t nb = 0;
-    const Constraint& parent = sconstraints[pi];
-    for (u_t subi: parent.subs)
+    bool legal = true;
+    const u_t xmin = (x > 15 ? x - 15 : 0);
+    au2_t be{xmin, x + 1};
+    size_t kib = lower_bound(sconstraints.begin(), sconstraints.end(), be,
+        [](const Constraint& c0, const au2_t& value) -> bool
+        {
+            const bool lt = c0.be < value;
+            return lt;
+        }) - sconstraints.begin();
+    be[0] = x;
+    size_t kie = upper_bound(sconstraints.begin(), sconstraints.end(), be,
+        [](const au2_t& value, const Constraint& c0) -> bool
+        {
+            const bool lt = value < c0.be;
+            return lt;
+        }) - sconstraints.begin();
+    for (u_t ki = kib; legal && (ki < kie); ++ki)
     {
-        if (subi < pre_bits.size())
-	{
-	    nb += pre_bits[subi];
-	}
+        const Constraint& sc = sconstraints[ki];
+        u_t nbits = 0;
+        for (u_t bi = sc.be[0] - xmin, bie = sc.be[1] - xmin; bi < bie; ++bi)
+        {
+            nbits += (last & (1u << bi)) ? 1 : 0;
+        }
+        legal = (nbits == sc.nbits);
     }
-    return nb;
+    return legal;
+}
+
+void SherBit::build_solution()
+{
+    u_t last = 0;
+    ull_t pending_legal = P;
+    for (u_t bi = 0; bi < N; ++bi)
+    {
+        u_t bit = 0;
+        xl2ull_t::const_iterator iter = pfx2_nlegals.find(XL(bi, last));
+        ull_t n_legals = ((iter == pfx2_nlegals.end()) ? 0 : iter->second);
+        if (n_legals < pending_legal)
+        {
+            bit = 1;
+            pending_legal -= n_legals;
+        }
+        solution.push_back("01"[bit]);
+        if (bi > 15)
+        {
+            last >>= 1;
+        }
+        last |= (bit << min<u_t>(bi, 15));
+    }
 }
 
 void SherBit::print_solution(ostream &fo) const
