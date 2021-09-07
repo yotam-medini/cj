7746ec8a880a191c0b08ef556e235197f1e062a6
Yotam Medini <yotam.medini@gmail.com>
2021-09-07 17:58:11 +0300

    palicross: passed Test Set 2

--- 2021/ks-round-E/palicross.cc
+++ 2021/ks-round-E/palicross.cc
@@ -26,10 +26,17 @@ class Palicross
 {
  public:
     Palicross(istream& fi);
+    Palicross(size_t _N, size_t _M, const vs_t& rows) :
+        N(_N), M(_M), R(rows), n_filled(0) {}
     void solve_naive();
     void solve();
     void solve_smart();
     void print_solution(ostream&) const;
+    const vs_t& get_solution(u_t& nf) const
+    {
+        nf = n_filled;
+        return R;
+    }
  private:
     size_t N, M;
     vs_t R;
@@ -105,7 +112,7 @@ void Palicross::solve_naive()
     }
 }
 
-static bool dot_fill_who(u_t who, char& c0, char& c1)
+static bool dot_fill_who(u_t& who, char& c0, char& c1)
 {
     bool fill = false;
     if ((c0 != '.') && (c1 == '.'))
@@ -125,14 +132,7 @@ static bool dot_fill_who(u_t who, char& c0, char& c1)
 
 void Palicross::solve()
 {
-    if ((N <= 50) && (M <= 50) && ((dbg_flags & 0x2) == 0))
-    {
-        solve_naive();
-    }
-    else
-    {
-        solve_smart();
-    }
+    solve_smart();
 }
 
 void Palicross::solve_smart()
@@ -150,9 +150,13 @@ void Palicross::solve_smart()
     u_t n_loops = 0;
     while (filling)
     {
+        setu_t rows_next, cols_next;
+        if (n_loops == 0)
+        {
+            cols_next = cols;
+        }
         ++n_loops;
         filling = false;
-        setu_t rows_next, cols_next;
         u_t who = 2;
         for (u_t r: rows)
         {
@@ -173,6 +177,7 @@ void Palicross::solve_smart()
                 }
             }
         }
+        swap(cols, cols_next);
         for (size_t c: cols)
         {
             for (size_t r = 0; r < N; )
@@ -193,7 +198,6 @@ void Palicross::solve_smart()
             }
         }
         swap(rows, rows_next);
-        swap(cols, cols_next);
     }
     if (dbg_flags & 0x1) { cerr << "#loops: " << n_loops << '\n'; }
 }
@@ -205,6 +209,142 @@ void Palicross::print_solution(ostream &fo) const
     {
         fo << '\n' << row;
     }
+}
+
+void pali_complete(vs_t& rows, u_t i, u_t j)
+{
+    const char c = rows[i][j];
+    const int N = rows.size(), M = rows[0].size();
+
+    // horizontal
+    int l, r;;
+    for (l = j; (l >= 0) && (rows[i][l] != '#'); --l) {}
+    ++l;
+    for (r = j; (r < M) && (rows[i][r] != '#'); ++r) {}
+    --r;
+    u_t jnew = r - (j - l);
+    if (rows[i][jnew] == '.')
+    {
+        rows[i][jnew] = c;
+        pali_complete(rows, i, jnew);
+    }
+
+    // vertocal
+    int t, b;
+    for (t = i; (t >= 0) && (rows[t][j] != '#'); --t) {}
+    ++t;
+    for (b = i; (b < N) && (rows[b][j] != '#'); ++b) {}
+    --b;
+    u_t inew = b - (i - t);
+    if (rows[inew][j] == '.')
+    {
+        rows[inew][j] = c;
+        pali_complete(rows, inew, j);
+    }
+}
+
+void gen_palicross(vs_t& rows, u_t N, u_t M, u_t nc, u_t sharps)
+{
+    rows.clear();
+    string dots(M, '.');
+    rows = vs_t(N, dots);
+    u_t max_del = N * M - sharps;
+    u_t ndel = (max_del == 0 ? 0 : rand() % max_del);
+    while (sharps > 0)
+    {
+        u_t i = rand() % N;
+        u_t j = rand() % M;
+        if (rows[i][j] == '.')
+        {
+            rows[i][j] = '#';
+            --sharps;
+        }
+    }
+    for (u_t i = 0; i < N ; ++i)
+    {
+        for (u_t j = 0; j < M; ++j)
+        {
+            if (rows[i][j] == '.')
+            {
+                rows[i][j] = 'A' + (rand() % nc);
+                pali_complete(rows, i, j);
+            }
+        }
+    }
+    if (dbg_flags & 0x1) {
+        cerr << "complete, pre deletions:\n";
+        for (const string& row: rows)
+        {
+            cerr << row << '\n';
+        }
+    }
+    while (ndel > 0)
+    {
+        u_t i = rand() % N;
+        u_t j = rand() % M;
+        if ((rows[i][j] != '.') && (rows[i][j] != '#'))
+        {
+            rows[i][j] = '.';
+            --ndel;
+        }
+    }
+    if (dbg_flags & 0x2) {
+        cerr << "After deletions:\n";
+        for (const string& row: rows)
+        {
+            cerr << row << '\n';
+        }
+    }
+}
+
+int test(int argc, char ** argv)
+{
+    int rc = 0; 
+    int ai = 0; 
+    u_t n_tests = strtoul(argv[ai++], 0, 0);
+    u_t n_max = strtoul(argv[ai++], 0, 0);
+    u_t m_max = strtoul(argv[ai++], 0, 0);
+    u_t c_max = strtoul(argv[ai++], 0, 0);
+    u_t sharp_max = strtoul(argv[ai++], 0, 0);
+    for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
+    {
+        cerr << "Tested: " << ti << '/' << n_tests << '\n';
+        u_t N = max<u_t>(1, rand() % (n_max + 1));
+        u_t M = max<u_t>(1, rand() % (m_max + 1));
+        u_t nc = max<u_t>(1, rand() % (c_max + 1));
+        u_t sharps = min<u_t>(N * M, rand() % (sharp_max + 1));
+        vs_t rows;
+        gen_palicross(rows, N, M, nc, sharps);
+        Palicross real(N, M, rows);
+        real.solve();
+        u_t n_filled;
+        const vs_t& solution = real.get_solution(n_filled);
+        Palicross naive(N, M, rows);
+        real.solve_smart();
+        u_t n_filled_naive;
+        const vs_t& solution_naive = real.get_solution(n_filled_naive);
+        if ((n_filled != n_filled_naive) || (solution != solution_naive))
+        {
+            rc = 1;
+            cerr << "Inconsistent: original:\n1\n" << N << ' ' << M << '\n';
+            for (const string& row: rows)
+            {
+                cerr << row << '\n';
+            }
+            cerr << "naive:";
+            naive.print_solution(cerr);
+            cerr << "\nreal:";
+            real.print_solution(cerr);
+            cerr << '\n';
+        }
+        else if (dbg_flags & 0x4)
+        {
+            real.print_solution(cerr); 
+            cerr << '\n';
+        }
+    }
+    
+    return rc;
 }
 
 int main(int argc, char ** argv)
@@ -231,6 +371,11 @@ int main(int argc, char ** argv)
         else if (opt == string("-debug"))
         {
             dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-test"))
+        {
+            rc = test(argc - (ai + 1), argv + (ai + 1));
+            exit(rc);
         }
         else if (opt == string("-tellg"))
         {
