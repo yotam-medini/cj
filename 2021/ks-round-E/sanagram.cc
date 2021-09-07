362cb67733e068544ca5f4e5dd1a957ed7a2ee3f
Yotam Medini <yotam.medini@gmail.com>
2021-09-07 12:52:12 +0300

    sanagram: non-naive passes tiny case

--- 2021/ks-round-E/sanagram.cc
+++ 2021/ks-round-E/sanagram.cc
@@ -7,6 +7,8 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <array>
+#include <set>
 #include <numeric>
 
 #include <cstdlib>
@@ -17,6 +19,11 @@ typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<u_t> vu_t;
+
+typedef array<u_t, 2> au2_t; // N, char
+typedef set<au2_t> setau2_t;
+typedef setau2_t::iterator iter_t;
+typedef setau2_t::reverse_iterator riter_t;
 
 static unsigned dbg_flags;
 
@@ -56,8 +63,13 @@ class Sanagran
     void solve();
     void print_solution(ostream&) const;
  private:
+    void dec_update(setau2_t& setau2, riter_t riter);
+    bool even(const setau2_t& setau2) const;
+    void rotate_complete(string& candid);
     string S;
     string solution;
+    set<au2_t> pending;
+    vu_t available_places[26];    
 };
 
 Sanagran::Sanagran(istream& fi)
@@ -91,10 +103,91 @@ void Sanagran::solve_naive()
 
 void Sanagran::solve()
 {
-     if (S.size() <= 8)
-     {
-         solve_naive();
-     }
+    const u_t N = S.size();
+    for (u_t i = 0; i < N; ++i)
+    {
+        u_t ci = S[i] - 'a';
+        available_places[ci].push_back(i);
+    }
+    for (u_t ci = 0; ci < 26; ++ci)
+    {
+        const u_t nc = available_places[ci].size();
+        if (nc > 0)
+        {
+            pending.insert(pending.end(), au2_t{nc, ci});
+        }
+    }
+    bool possible = true;
+    string candid(S.size(), ' ');
+    u_t located = 0;
+    while ((pending.size() > 1) && !even(pending))
+    {
+        riter_t iter = pending.rbegin();
+        au2_t pc = *iter;
+        riter_t iter_next = iter; ++iter_next;
+        au2_t pc_next = *iter_next;
+        char c = 'a' + pc[1];
+        char c_next = 'a' + pc_next[1];
+        u_t pca = (*iter)[1];
+        u_t pca_next = (*iter_next)[1];
+        u_t ai = available_places[pca].back();
+        u_t ai_next = available_places[pca_next].back();
+        candid[ai_next] = c;
+        candid[ai] = c_next;
+        available_places[pca].pop_back();
+        available_places[pca_next].pop_back();
+        dec_update(pending, iter);
+        dec_update(pending, iter_next);
+        located += 2;
+    }
+    possible = located == N;
+    if (!possible && even(pending) && (pending.size() > 1))
+    {
+        rotate_complete(candid);           
+        possible = true;
+    }
+    if (possible)
+    {
+        solution = candid;
+    }
+}
+
+bool Sanagran::even(const setau2_t& setau2) const
+{
+    const au2_t& low = *(setau2.begin());
+    const au2_t& high = *(setau2.rbegin());
+    bool ret = (low[0] == high[0]);
+    return ret;
+}
+
+void Sanagran::rotate_complete(string& candid)
+{
+    vu_t cis;
+    for (const au2_t& nc: pending)
+    {
+        cis.push_back(nc[1]);
+    }
+    for (u_t cisi = 0, sz = cis.size(); cisi < sz; ++cisi)
+    {
+        u_t cisi1 = (cisi + 1) % sz;
+        char c = 'a' + cis[cisi];
+        for (u_t ai: available_places[cis[cisi1]])
+        {
+            candid[ai] = c;
+        }
+    }
+}
+
+void Sanagran::dec_update(setau2_t& setau2, riter_t riter)
+{
+    au2_t pc = *riter;
+    ++riter;
+    iter_t iter(riter.base());
+    setau2.erase(iter);
+    if (--pc[0] > 0)
+    {
+        setau2.insert(setau2.end(), pc);
+    }
 }
 
 void Sanagran::print_solution(ostream &fo) const
