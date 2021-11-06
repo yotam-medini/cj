8181d9579d69747a862ac66d36d9df795f422501
Yotam Medini <yotam.medini@gmail.com>
2021-11-06 23:50:36 +0200

    banana: non-naive solves tiny

--- 2021/ks-round-G/banana.cc
+++ 2021/ks-round-G/banana.cc
@@ -10,6 +10,8 @@
 #include <utility>
 #include <vector>
 #include <array>
+#include <map>
+#include <set>
 
 #include <cstdlib>
 
@@ -22,6 +24,14 @@ typedef vector<u_t> vu_t;
 typedef array<u_t, 2> au2_t;
 
 static unsigned dbg_flags;
+
+// typedef set<u_t> setu_t;
+typedef array<u_t, 2> au2_t;
+// typedef map<u_t, setu_t> u_2setu_t;
+typedef map<u_t, au2_t> u_2au2_t;
+// typedef map<u_t, u_2setu_t> u_2u_2setu_t;
+typedef map<u_t, u_2au2_t> u_2u_2au2_t
+;
 
 class Banana
 {
@@ -39,9 +49,11 @@ class Banana
             solution = sol;
         }
     }
+    void compute_bunch_to_len_to_pos();
     u_t N, K;
     vu_t B;
     int solution;
+    u_2u_2au2_t bunch_to_len2pos;
 };
 
 Banana::Banana(istream& fi) : solution(-1)
@@ -136,7 +148,80 @@ int Banana::knip(u_t n, u_t target, const au2_t& last2)
 
 void Banana::solve()
 {
-     solve_naive();
+    compute_bunch_to_len_to_pos();
+    for (auto const& x: bunch_to_len2pos)
+    {
+        const u_t lbunch = x.first;
+        const u_t rbunch = K - lbunch;
+        const u_2u_2au2_t::const_iterator riter = bunch_to_len2pos.find(rbunch);
+        if (riter != bunch_to_len2pos.end())
+        {
+            const u_2au2_t& llen2pos = x.second;
+            const u_2au2_t& rlen2pos = riter->second;
+            for (auto const& llp: llen2pos)
+            {
+                u_t llen = llp.first;
+                u_t lpos = llp.second[0];
+                for (auto const& rlp: rlen2pos)
+                {
+                    u_t rlen = rlp.first;
+                    u_t rpos = rlp.second[1];
+                    if (lpos + llen <= rpos)
+                    {
+                        candidate(llen + rlen);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void Banana::compute_bunch_to_len_to_pos()
+{
+    // B = [2, 3, 5, 7]
+    // subsums = [0, 2, 5, 10, 17]
+    vu_t subsums; subsums.reserve(N + 1);
+    subsums.push_back(0);
+    for (u_t b: B)
+    {
+        subsums.push_back(subsums.back() + b);
+    }
+    for (u_t b = 0; b < N; ++b)
+    {
+        for (u_t e = b; e <= N; ++e)
+        {
+            const u_t bunch = subsums[e] - subsums[b];
+            if (bunch <= K)
+            {
+                const u_t len = e - b;
+                auto er = bunch_to_len2pos.equal_range(bunch);
+                u_2u_2au2_t::iterator liter = er.first;
+                if (er.first == er.second)
+                {
+                    u_2u_2au2_t::value_type v(bunch, u_2au2_t());
+                    liter = bunch_to_len2pos.insert(liter, v);
+                }
+                u_2au2_t& len_to_pos = liter->second;
+                auto per = len_to_pos.equal_range(len);
+                u_2au2_t::iterator piter = per.first;
+                if (per.first == per.second)
+                {
+                    u_2au2_t::value_type v(len, au2_t{b, b});
+                    piter = len_to_pos.insert(piter, v);
+                }
+                else
+                {
+                    au2_t& pos_min_max = piter->second;
+                    // pos_min_max[0] = is good
+                    if (pos_min_max[1] >- b)
+                    {
+                        cerr << __FILE__ << ':'<<  __LINE__ << "Failure\n";
+                    }
+                    pos_min_max[1] = b;
+                }
+            }
+        }
+    }
 }
 
 void Banana::print_solution(ostream &fo) const
