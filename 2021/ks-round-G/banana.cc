b0f1ba185a65373243df99ec573ab6f722fce295
Yotam Medini <yotam.medini@gmail.com>
2021-11-09 09:16:15 +0200

    banana.cc add testing functionality

--- 2021/ks-round-G/banana.cc
+++ 2021/ks-round-G/banana.cc
@@ -32,9 +32,11 @@ class Banana
 {
  public:
     Banana(istream& fi);
+    Banana(const vu_t _B, u_t _K) : N(_B.size()), K(_K), B(_B), solution(-1) {}
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
+    int get_solution() const { return solution; }
  private:
     void candidate(int sol)
     {
@@ -132,6 +134,9 @@ void Banana::compute_bunch_to_len_to_pos()
     }
     for (u_t b = 0; b < N; ++b)
     {
+        if ((dbg_flags & 0x1) && ((b & (b - 1)) == 0)) {
+            cerr << __func__ << ": b/N = " << b << " / " << N << '\n';
+        }
         for (u_t e = b; e <= N; ++e)
         {
             const u_t bunch = subsums[e] - subsums[b];
@@ -259,10 +264,48 @@ static int real_main(int argc, char ** argv)
     return 0;
 }
 
-static int test_specific(int argc, char ** argv)
+int test_case(const vu_t B, u_t K)
 {
     int rc = 0;
+    Banana banana(B, K);
+    banana.solve();
+    int solution = banana.get_solution();
+    if (B.size() < 0x10)
+    {
+        Banana banana_naive(B, K);
+        banana_naive.solve_naive();
+        int solution_naive = banana_naive.get_solution();
+        if (solution != solution_naive)
+        {
+            rc = 1;
+            cerr << "Inconsistent solution: "<<solution <<
+                ", naive: " << solution_naive << '\n';
+            cerr << "specific " << K;
+            for (u_t b: B) { cerr << ' ' << b; }
+            cerr << '\n';
+        }
+    }
     return rc;
+}
+
+static int test_specific(int argc, char ** argv)
+{
+    int ai = 0;
+    u_t K = strtoul(argv[ai++], 0, 0);
+    vu_t B;
+    for ( ; ai < argc; ++ai)
+    {
+        u_t b = strtoul(argv[ai++], 0, 0);
+        B.push_back(b);
+    }
+    return test_case(B, K);
+}
+
+static u_t minmax_rand(u_t vmin, u_t vmax)
+{
+    u_t d = vmax - vmin;
+    int ret = vmin + (rand() % (d + 1));
+    return ret;
 }
 
 static int test_random(int argc, char ** argv)
@@ -270,15 +313,30 @@ static int test_random(int argc, char ** argv)
     int rc = 0;
     int ai = 0;
     u_t n_tests = strtoul(argv[ai++], 0, 0);
+    u_t n_min = strtoul(argv[ai++], 0, 0);
+    u_t n_max = strtoul(argv[ai++], 0, 0);
+    u_t b_min = strtoul(argv[ai++], 0, 0);
+    u_t b_max = strtoul(argv[ai++], 0, 0);
+    u_t k_min = strtoul(argv[ai++], 0, 0);
+    u_t k_max = strtoul(argv[ai++], 0, 0);
     for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
     {
         cerr << "Tested: " << ti << '/' << n_tests << '\n';
+        u_t N = minmax_rand(n_min, n_max);
+        u_t K = minmax_rand(k_min, k_max);
+        vu_t B;
+        while (B.size() < N)
+        {
+            B.push_back(minmax_rand(b_min, b_max));
+        }
+        rc = test_case(B, K);
     }
     return rc;
 }
 
 static int test(int argc, char ** argv)
 {
+    dbg_flags = 0x1;
     int rc = ((argc > 1) && (string(argv[1]) == string("specific"))
         ? test_specific(argc - 1, argv + 1)
         : test_random(argc, argv));
@@ -288,7 +346,7 @@ static int test(int argc, char ** argv)
 int main(int argc, char **argv)
 {
     int rc = ((argc > 1) && (string(argv[1]) == string("test"))
-        ? test(argc - 1, argv + 1)
+        ? test(argc - 2, argv + 2)
         : real_main(argc, argv));
     return rc;
 }
