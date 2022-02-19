d0df24d9769f39cedc2e1c4bf2b98700e8f41e8e
Yotam Medini <yotam.medini@gmail.com>
2022-02-19 21:43:00 +0200

    milk: non-naive solves tiny

--- 2022/ks-practice-1/milk.cc
+++ 2022/ks-practice-1/milk.cc
@@ -5,8 +5,9 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
-// #include <map>
-// #include <set>
+#include <array>
+#include <set>
+#include <tuple>
 #include <string>
 #include <utility>
 #include <vector>
@@ -15,13 +16,20 @@
 
 using namespace std;
 
+typedef unsigned char uc_t;
 typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<string> vs_t;
 typedef unsigned char uc_t;
+typedef vector<u_t> vu_t;
 typedef vector<uc_t> vuc_t;
 typedef vector<vuc_t> vvuc_t;
+typedef array<u_t, 2> au2_t;
+typedef vector<au2_t> vau2_t;
+typedef set<uc_t> setuc_t;
+typedef tuple<u_t, setuc_t> usetuc_t;
+typedef set<usetuc_t> set_usetuc_t;
 
 static unsigned dbg_flags;
 
@@ -42,12 +50,19 @@ class Milk
         }
         return v;
     }
+    void compute_option_on_count();
+    ul_t price(const vuc_t& offer) const;
+    const vuc_t& change_offer(vuc_t& offer, const setuc_t& by) const;
+    // ul_t price(const setuc_t& offer) const;
     u_t N, M, P;
     vs_t s_prefs;
     vs_t s_forbiddens;
     vvuc_t prefs;
     vvuc_t forbiddens;
-    ull_t solution;
+    ul_t solution;
+    vu_t option_on_count;
+    vu_t penalties;
+    vuc_t base_offer;
 };
 
 Milk::Milk(istream& fi) : solution(0)
@@ -109,7 +124,73 @@ void Milk::solve_naive()
 
 void Milk::solve()
 {
-    solve_naive();
+    const u_t half = N/2;
+    const set<vuc_t> set_forbiddens(forbiddens.begin(), forbiddens.end());
+    compute_option_on_count();
+    base_offer.reserve(P);
+    for (u_t i = 0; i < P; ++i)
+    {
+        base_offer.push_back(option_on_count[i] <= half ? 0 : 1);
+    }
+    set_usetuc_t q;
+    q.insert(q.end(), usetuc_t{price(base_offer), setuc_t()});
+    vuc_t offer(size_t(P), 0);
+    while (set_forbiddens.find(change_offer(offer, get<1>(*q.begin()))) !=
+        set_forbiddens.end())
+    {
+        const setuc_t curr_change = get<1>(*q.begin());
+        for (uc_t i = 0; i < P; ++i)
+        {
+            setuc_t candidate_change(curr_change);
+            candidate_change.insert(candidate_change.end(), i);
+            u_t penalty = price(change_offer(offer, candidate_change));
+            q.insert(q.end(), usetuc_t{penalty, candidate_change});
+        }
+        q.erase(q.begin());
+    }
+    solution = get<0>(*(q.begin()));
+}
+
+void Milk::compute_option_on_count()
+{
+    const u_t half = N/2;
+    option_on_count.reserve(P);
+    penalties.reserve(P);
+    for (u_t p = 0; p < P; ++p)
+    {
+        u_t count = 0;
+        for (u_t i = 0; i < N; ++i)
+        {
+            count += prefs[i][p];
+        }
+        option_on_count.push_back(count);
+        u_t majority = (half < count ? count : N - count);
+        u_t minority = N - majority;
+        u_t penalty = majority - minority;
+        penalties.push_back(penalty);
+    }
+}
+
+ul_t Milk::price(const vuc_t& offer) const
+{
+    ul_t total = 0;
+    for (u_t i = 0; i < P; ++i)
+    {
+        u_t want = option_on_count[i];
+        u_t complaints = (offer[i] == 0 ? want : N - want);
+        total += complaints;
+    }
+    return total;
+}
+
+const vuc_t& Milk::change_offer(vuc_t& offer, const setuc_t& by) const
+{
+    for (u_t i = 0; i < P; ++i)
+    {
+        const uc_t boi = base_offer[i];
+        offer[i] = (by.find(i) == by.end() ? boi : 1 - boi);
+    }
+    return offer;
 }
 
 void Milk::print_solution(ostream &fo) const
