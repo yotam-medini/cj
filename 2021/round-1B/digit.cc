d6c3c64e7b8fb148d6c86831647e7ee51ba45c8b
Yotam Medini <yotam.medini@gmail.com>
2022-03-24 08:44:31 +0200

    digit: add do_learn, but not good enough

--- 2021/round-1B/digit.cc
+++ 2021/round-1B/digit.cc
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
 #include <utility>
 
 #include <cstdlib>
@@ -20,6 +21,8 @@ typedef unsigned long long ull_t;
 typedef vector<int> vi_t;
 typedef vector<u_t> vu_t;
 typedef vector<vu_t> vvu_t;
+typedef array<u_t, 2> au2_t;
+typedef vector<au2_t> vau2_t;
 
 static unsigned dbg_flags;
 
@@ -61,12 +64,189 @@ class ErrLog
     ofstream *_f;
 };
 
+class Rule
+{
+ public:
+    Rule(u_t unf=0, u_t alm=0, u_t pend=0, u_t thres=0) : 
+        unfinished(unf),
+        almost(alm),
+        pending_blocks(pend),
+        threshold(thres)
+        {}
+    string str() const
+    {
+        ostringstream os;
+        os << "Rule(" << unfinished << ", " << almost << ", " <<
+            pending_blocks << ", " << threshold << ")";
+        return os.str();
+    }
+    u_t unfinished; // #(towers) < B
+    u_t almost;     // #(towers) == B-1
+    u_t pending_blocks; 
+    u_t threshold;
+};
+typedef vector<Rule> vrule_t;
+
+class Config
+{
+ public:
+    Config(): low_to_low(0) {}
+    vrule_t rules;
+    u_t low_to_low;
+    void print(ostream& os) const
+    {
+        os << "Config cfg;\n";
+        for (const Rule& rule: rules)
+        {
+            os << "cfg.rules.push_back(" << rule.str() << ");\n";
+        }
+        os << "cfg.low_to_low = " << low_to_low << "\n";
+    }
+};
+
+class Towers
+{
+ public:
+    Towers(u_t _N, u_t _B, ErrLog& el, const Config& _cfg=Config()) :
+         N(_N), B(_B), errlog(el), cfg(_cfg), tblocks(N, vu_t()),
+         n_unfinished(N), n_almost(0), n_pending(N*B)
+         {}
+    u_t put_get(u_t digit);
+    void show() const;
+    ull_t compute_score() const;
+ private:
+    u_t get_index_of_max_tower_lt(u_t ltval) const;
+    u_t get_threshold() const;
+    const u_t N, B;
+    ErrLog& errlog;
+    Config cfg;
+    vvu_t tblocks;
+    u_t n_unfinished;
+    u_t n_almost;
+    u_t n_pending;
+};
+
+u_t Towers::put_get(u_t digit)
+{
+    u_t threshold = get_threshold();
+    u_t tower = N;
+    if (digit >= threshold)
+    {
+        u_t ti;
+        for (ti = 0; (ti < N) && (tblocks[ti].size() != B - 1); ++ti)
+        {
+            ;
+        }
+        if (ti < N)
+        {
+            tower = ti;
+        }
+        else
+        {
+            tower = get_index_of_max_tower_lt(B);
+        }
+    }
+    else
+    {
+        tower = get_index_of_max_tower_lt(B - 1);
+        if (tower == N)
+        {
+            tower = get_index_of_max_tower_lt(B);
+        }
+        else if (digit < cfg.low_to_low)
+        {
+            u_t h = tblocks[tower].size();
+            int alt_tower = get_index_of_max_tower_lt(h);
+            if (alt_tower < int(N))
+            {
+                tower = alt_tower;
+            }
+        }
+    }
+    tblocks[tower].push_back(digit);
+    if (tblocks[tower].size() == B)
+    {
+        --n_unfinished;
+        --n_almost;
+    }
+    else if (tblocks[tower].size() == B - 1)
+    {
+        ++n_almost;
+    }
+    --n_pending;
+    return tower;    
+}
+
+void Towers::show() const
+{
+    for (const vu_t& tblock: tblocks)
+    {
+        errlog << '[' << tblock.size() << ']';
+        for (int i = tblock.size() - 1; i >= 0; --i)
+        {
+            errlog << tblock[i];
+        }
+        errlog << ' ';
+    }
+    errlog << '\n';
+    errlog.flush();
+}
+
+ull_t Towers::compute_score() const
+{
+    ull_t score = 0;
+    for (const vu_t& tblock: tblocks)
+    {
+        ull_t tenp = 1;
+        ull_t tower_number = 0;
+        for (u_t d: tblock)
+        {
+            tower_number += tenp * ull_t(d);
+            tenp *= 10;
+        }
+        score += tower_number;
+    }
+    return score;
+}
+
+u_t Towers::get_index_of_max_tower_lt(u_t ltval) const
+{
+    u_t ret = N;
+    u_t maxh = B + 1;
+    for (u_t ti = 0; ti < N; ++ti)
+    {
+        u_t h = tblocks[ti].size();
+        if ((h < ltval) && ((maxh > B) || (maxh < h)))
+        {
+            maxh = h;
+            ret = ti; 
+        }
+    }
+    return ret;
+}
+
+u_t Towers::get_threshold() const
+{
+    u_t threshold = 10;
+    for (u_t i = 0; (threshold == 10) && (i < cfg.rules.size()); ++i)
+    {
+        const Rule& rule = cfg.rules[i];
+        if ((n_unfinished <= rule.unfinished) &&
+            (n_almost <= rule.almost) &&
+            (n_pending <= rule.pending_blocks))
+        {
+            threshold = rule.threshold;
+        }
+    }
+    threshold = min<u_t>(threshold, 9);
+    return threshold;
+}
+
 class Digit
 {
  public:
     Digit(istream& cfi, ostream& cfo, ErrLog &el) :
         fi(cfi), fo(cfo), errlog(el) {}
-    void solve_naive();
     void solve();
  private:
     bool readline_ints(vi_t &v);
@@ -98,82 +278,49 @@ bool Digit::readline_ints(vi_t &v)
     return fi.eof();
 }
 
-void Digit::solve_naive()
+void Digit::solve()
 {
     fi >> T >> N >> B >> P;
-    errlog << "T="<<T << ", N="<<N << ", B="<<B << ", P="<<P << '\n';
+    if (dbg_flags & 0x1) {
+        errlog << "T="<<T << ", N="<<N << ", B="<<B << ", P="<<P << '\n'; }
     errlog.flush();
     ull_t total_score = 0;
-    u_t digit, ti;
+    u_t digit;
     int judge;
+
+    Config cfg_best; // see do_learn below
+    cfg_best.rules.push_back(Rule(2, 1, 6, 6));
+    cfg_best.rules.push_back(Rule(3, 1, 15, 7));
+    cfg_best.rules.push_back(Rule(4, 1, 17, 8));
+    cfg_best.low_to_low = 3;
+
     for (u_t itest = 0; itest < T; ++itest)
     {
-        errlog << "itest="<<itest << '\n'; errlog.flush();
-        vu_t towers(N, 0);
-        vvu_t tblocks(N, vu_t());
-        u_t end_tower_threshold = 9;
+        if (dbg_flags & 0x2) { errlog << "itest="<<itest << '\n'; }
+        Towers towers(N, B, errlog, cfg_best);
         for (u_t di = 0; di < N*B; ++di)
         {
             fi >> digit;
             if (dbg_flags & 0x8) { errlog << "itest="<<itest << 
                 ", di="<<di << ", digit="<<digit <<'\n';  errlog.flush(); }
-            int tower = -1;
-            if (towers[N - 2] == B - 1)
-            {
-                end_tower_threshold = 8;
-            }
-            if (digit >= end_tower_threshold)
-            {
-                for (ti = 0; ((ti < N) && (towers[ti]) != B - 1); ++ti)
-                {
-                    ;
-                }
-                if (ti < N)
-                {
-                    tower = ti;
-                }
-                else
-                {
-                    tower = get_index_of_max_tower_lt(towers, B);
-                }
-            }
-            else
-            {
-                tower = get_index_of_max_tower_lt(towers, B - 1);
-                if (tower == int(N))
-                {
-                    tower = get_index_of_max_tower_lt(towers, B);
-                }
-                else if (digit < 1)
-                {
-                    u_t h = towers[tower];
-                    int alt_tower = get_index_of_max_tower_lt(towers, h);
-                    if (alt_tower < int(N))
-                    {
-                        tower = alt_tower;
-                    }
-                }
-            }
-            ++towers[tower];
-            tblocks[tower].push_back(digit);
+            u_t itower = towers.put_get(digit);
             if (dbg_flags & 0x8) { errlog << "di="<<di <<
-                ", tower=" << tower << '\n';  errlog.flush(); }
-            fo << (tower + 1) << '\n'; fo.flush();
-            if (dbg_flags & 0x4) { show_blocks(tblocks); }
+                ", itower=" << itower << '\n';  errlog.flush(); }
+            fo << (itower + 1) << '\n'; fo.flush();
+            if (dbg_flags & 0x4) { towers.show(); }
         }
-        show_blocks(tblocks);
-        ull_t blocks_score = compute_block_score(tblocks);
-        errlog << "End-of-test: " << itest << ", score="<<blocks_score << '\n';
+        ull_t blocks_score = towers.compute_score();
+        if (dbg_flags & 0x2) { 
+            towers.show(); 
+            errlog << "End-of-test: " << itest << ", score="<<blocks_score<<'\n';
+        }
         total_score += blocks_score;
     }
     fi >> judge;
-    errlog<<"judge="<<judge << ", P="<<P << ", total_score="<<total_score<< '\n';
-}
-
-void Digit::solve()
-{
-     solve_naive();
-     
+    if (dbg_flags & 0x1) {  errlog<<"judge="<<judge << 
+        ",\n  P="<<P << ", total_score="<<total_score<< 
+        ", /P = " << double(total_score)/double(P) << '\n'; 
+    }
 }
 
 u_t Digit::get_index_of_max_tower_lt(const vu_t& towers, u_t ltval) const
@@ -224,20 +371,119 @@ ull_t Digit::compute_block_score(const vvu_t& tblocks) const
     return score;
 }
 
+class Learn
+{
+ public:
+    enum {T = 50, N= 20, B=15};
+    Learn(ErrLog& el) : errlog(el), best_score(0) {}
+    void run();
+ private:
+    void test_current_config();
+    ErrLog& errlog;
+    Config cfg_current;
+    Config cfg_best;
+    ull_t best_score;
+    static const ull_t P1, P2;
+    void iterate();
+};
+const ull_t Learn::P1 = 860939810732536850;
+const ull_t Learn::P2 = 937467793908762347;
+
+void Learn::run()
+{
+    test_current_config();
+    for (u_t threshold = 8; threshold >= 5; --threshold)
+    {
+        for (u_t low_to_low: {0, 1, 2, 3})
+        {
+            cfg_current.low_to_low = low_to_low;
+            cfg_current.rules.clear();
+            cfg_current.rules.push_back(Rule(2, 1, 3, threshold));
+            iterate();
+        }
+    }
+    cout << "best_score = " << best_score << ", /P2 = " <<
+        double(best_score)/double(P2) << " with:\n";
+    cfg_best.print(cout);
+    
+}
+
+void Learn::test_current_config()
+{
+    cerr << __func__ << '\n';
+    ull_t min_total_score = P2;
+    for (u_t sample = 0; sample < 3; ++sample)
+    {
+        ull_t total_score = 0;
+        for (u_t t = 0; t < T; ++t)
+        {
+            Towers towers(N, B, errlog, cfg_current);
+            for (u_t b = 0; b < N*B; ++b)
+            {
+                u_t digit = rand() % 10;
+                towers.put_get(digit); // ignore returned tower index;
+            }
+            ull_t tscore = towers.compute_score();
+            total_score += tscore;
+        }
+        min_total_score = min<ull_t>(min_total_score, total_score);
+    }
+    ull_t total_score = min_total_score;
+    cout << "total_score = " << total_score << 
+        ", /P1 = " << double(total_score)/double(P1) <<
+        ", /P2 = " << double(total_score)/double(P2) << '\n';
+    if (best_score < total_score)
+    {
+        best_score = total_score;
+        cfg_best = cfg_current;
+    }
+}
+
+void Learn::iterate()
+{
+    vrule_t& rules = cfg_current.rules;
+    u_t i = rules.size() - 1;
+    for ( ; rules[i].almost <= rules[i].unfinished; ++rules[i].almost)
+    {
+        for ( ; rules[i].pending_blocks <= 2*B; ++rules[i].pending_blocks)
+        {
+            if (rules[i].threshold == 8)
+            {
+                test_current_config();
+            }
+            else
+            {
+                Rule next_rule(rules[i]);
+                ++next_rule.unfinished;
+                ++next_rule.threshold;
+                rules.push_back(next_rule);
+                iterate();
+                rules.pop_back();
+            }
+        }
+    }
+}
+
+static void do_learn(ErrLog& el)
+{
+    Learn learn(el);
+    learn.run();
+}
+
 int main(int argc, char ** argv)
 {
     const string dash("-");
 
-    bool naive = false;
+    bool learn = false;
     int rc = 0, ai;
 
     for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
         argv[ai][1] != '\0'; ++ai)
     {
         const string opt(argv[ai]);
-        if (opt == string("-naive"))
+        if (opt == string("-learn"))
         {
-            naive = true;
+            learn = true;
         }
         else if (opt == string("-debug"))
         {
@@ -277,15 +523,14 @@ int main(int argc, char ** argv)
         }
     }
 
-    string ignore;
-    Digit digit(*pfi, *pfo, errlog);
-    if (naive)
+    if (learn)
     {
-         digit.solve_naive();
+        do_learn(errlog);
     }
     else
     {
-         digit.solve_naive();
+        Digit digit(*pfi, *pfo, errlog);
+        digit.solve();
     }
 
     if (pfi != &cin) { delete pfi; }
