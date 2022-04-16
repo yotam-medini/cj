4d72c70c160e4c91cb3f46f83e40192a7416d8c7
Yotam Medini <yotam.medini@gmail.com>
2022-04-16 23:19:16 +0300

    segtree tested except for updae

--- ymutil/segtree.cc
+++ ymutil/segtree.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <cstdlib>
 
@@ -15,8 +16,11 @@ class SegmentTree
     SegmentTree(const ve_t& _a);
     void update(u_t pos, e_t val);
     e_t queru_sum(u_t b, u_t e) const;
+    void print(ostream& os) const;
  private:
     typedef vector<ve_t> vve_t;
+    e_t level_sum(u_t level, u_t b, u_t e) const;
+    void level_update(u_t level, u_t pos, e_t old_val, e_t val);
     u_t height() const { return a.size(); }
     u_t size() const { return a.empty() ? 0 : a[0].size(); }
     vve_t a;
@@ -24,16 +28,74 @@ class SegmentTree
 
 SegmentTree::SegmentTree(const ve_t& _a)
 {
+    a.push_back(_a);
+    while (a.back().size() > 1)
+    {
+        const ve_t& prev = a.back();
+        const u_t sz = prev.size() / 2;
+        const u_t sz2 = 2*sz;
+        ve_t b; b.reserve(sz);
+        for (u_t i = 0; i < sz2; i += 2)
+        {
+            b.push_back(prev[i] + prev[i + 1]);
+        }
+        a.push_back(ve_t());
+        swap(a.back(), b);
+    }
+    print(cerr);
 }
 
 void SegmentTree::update(u_t pos, e_t val)
 {
+    level_update(0, pos, a[0][pos], val);
 }
 
 SegmentTree::e_t SegmentTree::queru_sum(u_t b, u_t e) const
 {
+    return level_sum(0, b, e);
+}
+
+SegmentTree::e_t SegmentTree::level_sum(u_t level, u_t b, u_t e) const
+{
     e_t total = 0;
+    if (b < e)
+    {
+        if (b % 2 != 0)
+        {
+            total += a[level][b++];
+        }
+        if (e % 2 != 0)
+        {
+            total += a[level][--e];
+        }
+        total += level_sum(level + 1, b/2, e/2);
+    }
     return total;
+}
+
+void SegmentTree::level_update(u_t level, u_t pos, e_t old_val, e_t val)
+{
+    ve_t& alevel = a[level];
+    alevel[pos] -= old_val;
+    alevel[pos] -= val;
+    if ((pos % 2 != 0) || (pos + 1 < alevel.size()))
+    {
+        level_update(level + 1, pos / 2, old_val, val);
+    }
+}
+
+void SegmentTree::print(ostream& os) const
+{
+    for (u_t level = 0; level < a.size(); ++level)
+    {
+        const ve_t& alevel = a[level];
+        os << "[" << level << "]";
+        for (e_t x: alevel)
+        {
+            os << ' ' << x;
+        }
+        os << '\n';
+    }
 }
 
 // ================================ Test ================================
@@ -49,7 +111,7 @@ static int verdict(const vull_t& a, u_t b, u_t e, ull_t st_sum, ull_t naive_sum)
     if (st_sum != naive_sum)
     {
         rc = 1;
-        cerr << "Failure: test " << b << ' ' << e;
+        cerr << "Failure: specific " << b << ' ' << e;
         for (ull_t x: a) { cerr << ' ' << x; }
         cerr << '\n';
     }
@@ -115,13 +177,13 @@ int main(int argc, char **argv)
     int rc = 0;
     if (string(argv[1]) == string("specific"))
     {
-        int ai = 1;
-        u_t b = strtoul(argv[++ai], 0, 0);
-        u_t e = strtoul(argv[++ai], 0, 0);
+        int ai = 2;
+        u_t b = strtoul(argv[ai++], 0, 0);
+        u_t e = strtoul(argv[ai++], 0, 0);
         vull_t a;
         for (; ai < argc; ++ai)
         {
-            a.push_back(strtoul(argv[++ai], 0, 0));
+            a.push_back(strtoul(argv[ai++], 0, 0));
         }
         rc = test_case(a, b, e);
     }
