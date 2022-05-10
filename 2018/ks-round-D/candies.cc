ae88eaf3f7d429a8f86ac2a6223068822c09b1c1
Yotam Medini <yotam.medini@gmail.com>
2022-05-10 15:12:29 +0300

    candies: +=internal-test;  non-naive under construction

--- 2018/ks-round-D/candies.cc
+++ 2018/ks-round-D/candies.cc
@@ -19,6 +19,7 @@ typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef long long ll_t;
+typedef vector<u_t> vu_t;
 typedef vector<ll_t> vll_t;
 
 static unsigned dbg_flags;
@@ -27,14 +28,28 @@ class Candies
 {
  public:
     Candies(istream& fi);
+    Candies(
+        u_t _N, u_t _O, ll_t _D,
+        ll_t _X1, ll_t _X2, ll_t _A, ll_t _B, ll_t _C, ull_t _M, ll_t _L);
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
+    ll_t get_solution(bool& poss) const { return solution; }
  private:
+    void generate_sweet();
+    void candidate_check(ll_t candidate)
+    {
+        if ((candidate <= D) && ((!possible) || (solution < candidate)))
+        {
+            possible = true;
+            solution = candidate;
+        }
+    }
     u_t N, O;
     ll_t D;
-    ll_t A, B, C, L;
+    ll_t X1, X2, A, B, C;
     ull_t M;
+    ll_t L;
     vll_t S;
     bool possible;
     ll_t solution;
@@ -44,8 +59,22 @@ Candies::Candies(istream& fi) : possible(false), solution(0)
 {
     // copy_n(istream_iterator<u_t>(fi), N, back_inserter(a));
     fi >> N >> O >> D;
-    ll_t X1, X2;
     fi >> X1 >> X2 >> A >> B >> C >> M >> L;
+    generate_sweet();
+}
+
+Candies::Candies(
+    u_t _N, u_t _O, ll_t _D,
+    ll_t _X1, ll_t _X2, ll_t _A, ll_t _B, ll_t _C, ull_t _M, ll_t _L
+) : N(_N), O(_O), D(_D),
+    X1(_X1), X2(_X2), A(_A), B(_B), C(_C), M(_M), L(_L),
+    possible(false), solution(0)
+{
+    generate_sweet();
+}
+    
+void Candies::generate_sweet()
+{
     S.reserve(N);
     vll_t X;
     X.reserve(N);
@@ -78,22 +107,96 @@ void Candies::solve_naive()
                 n_odd += (x % 2 == 0 ? 0 : 1);
                 sum_be += x;
             }
-            if ((n_odd <= O) && (sum_be <= D))
+            if (n_odd <= O)
             {
-                if ((!possible) || (solution < sum_be))
-                {
-                    possible = true;
-                    solution = sum_be;
-                }
+                candidate_check(sum_be);
             }
         }
     }
 }
 
+ll_t v_at_minus1(const vll_t& v, u_t i)
+{
+    ll_t ret = ((0 < i) && (i <= v.size()) ? v[i - 1] : 0);
+    return ret;
+}
+
 void Candies::solve()
 {
-    solve_naive();
+    // solve_naive();
+    // static const u_t undef(-1);
+    vll_t sub_sums; sub_sums.reserve(N + 1);
+    vu_t odd_count; odd_count.reserve(N + 1);
+    sub_sums.push_back(0);
+    odd_count.push_back(0);
+    for (u_t i = 0; i < N; ++i)
+    {
+        sub_sums.push_back(sub_sums.back() + S[i]);
+        u_t zo = (S[i] % 2 == 0 ? 0 : 1);
+        odd_count.push_back(odd_count.back() + zo);
+    }
+    for (u_t b = 0; b < N; ++b)
+    {
+        for (u_t e = b + 1; e <= N; ++e)
+        {
+            u_t n_odd = odd_count[e] - odd_count[b];
+            if (n_odd <= O)
+            {
+                ll_t be_sum = sub_sums[e] - sub_sums[b];
+                candidate_check(be_sum);
+            }
+        }
+    }
 }
+
+#if 0
+void Candies::solve()
+{
+    // solve_naive();
+    // static const u_t undef(-1);
+    vll_t sub_sums;
+    vu_t odd_idx;
+    vu_t pos_idx; // after odd
+    ll_t vsum_last = 0;
+    u_t curr_pos_start = 0;
+    for (int i = 0; i < int(N); ++i)
+    {
+        const ll_t v = S[i];
+        if ((O > 0) || ((v % 2) == 0))
+        {
+            candidate_check(v); // for single negative
+        }
+        vsum_last += v;
+        sub_sums.push_back(vsum_last);
+        ll_t pre_sum = v_at_minus1(sub_sums, curr_pos_start);
+        ll_t curr_sum = vsum_last - pre_sum;
+        candidate_check(curr_sum);
+        if (curr_sum < 0)
+        {
+            curr_pos_start = i + 1;
+        }
+        if (v % 2 != 0)
+        {
+            odd_idx.push_back(i);
+            pos_idx.push_back(i);
+            u_t n_odds = odd_idx.size();
+            if (n_odds > O)
+            {
+                curr_pos_start = max(curr_pos_start, pos_idx[n_odds - O]);
+            }
+        }
+        if ((!pos_idx.empty()) && (S[pos_idx.back()] < 0) && (v >= 0))
+        {
+            pos_idx.back() = i;
+        }
+        {
+            pre_sum = v_at_minus1(sub_sums, curr_pos_start);
+            ll_t candid = sub_sums.back() - pre_sum;
+            candidate_check(candid);
+        }
+    }
+}
+#endif
 
 void Candies::print_solution(ostream &fo) const
 {
@@ -199,9 +302,41 @@ static u_t rand_range(u_t nmin, u_t nmax)
     return r;
 }
 
-static int test_case(int argc, char ** argv)
+static int test_case(
+    u_t N, u_t O, ll_t D,
+    ll_t X1, ll_t X2, ll_t A, ll_t B, ll_t C, ull_t M, ll_t L
+)
 {
-    int rc = rand_range(0, 1);
+    int rc = 0;
+    bool possible = false, possible_naive = false;
+    ll_t solution = 0, solution_naive = 0;
+    bool small = (N < 0x20);
+    if (small)
+    {
+        Candies p(
+            N, O, D,
+            X1, X2, A, B, C, M, L);
+        p.solve_naive();
+        solution_naive = p.get_solution(possible_naive);
+    }
+    {
+        Candies p(
+            N, O, D,
+            X1, X2, A, B, C, M, L);
+        p.solve();
+        solution = p.get_solution(possible);
+    }
+    if (small && ((solution != solution_naive) || (possible != possible_naive)))
+    {
+        rc = 1;
+        cerr << "Fail: inconsistent\n";
+        ofstream f("candies-fail.in");
+        f << "1\n" << N << ' ' << O << ' ' << D << '\n' <<
+            X1 << ' ' << X2 << ' ' <<
+            A << ' ' << B << ' ' << C << ' ' << M << ' ' << L << '\n';
+            
+        f.close();
+    }
     return rc;
 }
 
@@ -210,10 +345,45 @@ static int test_random(int argc, char ** argv)
     int rc = 0;
     int ai = 0;
     u_t n_tests = strtoul(argv[ai++], 0, 0);
+    ull_t Nmin = strtoul(argv[ai++], 0, 0);
+    ull_t Nmax = strtoul(argv[ai++], 0, 0);
+    ull_t Omin = strtoul(argv[ai++], 0, 0);
+    ull_t Omax = strtoul(argv[ai++], 0, 0);
+    ll_t Dmin = strtoul(argv[ai++], 0, 0);
+    ll_t Dmax = strtoul(argv[ai++], 0, 0);
+    ull_t X12max = strtoul(argv[ai++], 0, 0);
+    ull_t Amax = strtoul(argv[ai++], 0, 0);
+    ull_t Bmax = strtoul(argv[ai++], 0, 0);
+    ull_t Cmax = strtoul(argv[ai++], 0, 0);
+    ull_t Mmax = strtoul(argv[ai++], 0, 0);
+    ll_t Lmax = strtoul(argv[ai++], 0, 0);
+    cerr << "n_tests="<<n_tests <<
+        ", Nmin="<<Nmin << ", Nmax="<<Nmax <<
+        ", Omin="<<Omin << ", Omax="<<Omax <<
+        ", Dmin="<<Dmin << ", Dmax="<<Dmax <<
+        ", X12Max="<<X12max <<
+        ", Amax="<<Amax <<
+        ", Bmax="<<Bmax <<
+        ", Cmax="<<Cmax <<
+        ", Mmax="<<Mmax <<
+        ", Lmax="<<Lmax <<
+        '\n';
     for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
     {
         cerr << "Tested: " << ti << '/' << n_tests << '\n';
-        rc = test_case(argc, argv);
+        ull_t N = rand_range(Nmin, Nmax);
+        ull_t O = rand_range(Omin, Omax);
+        ll_t D = Dmin + rand_range(0, Dmax - Dmin);
+        ll_t X1 = rand_range(0, X12max);
+        ll_t X2 = rand_range(0, X12max);
+        ll_t A = rand_range(0, Amax);
+        ll_t B = rand_range(0, Bmax);
+        ll_t C = rand_range(0, Cmax);
+        ull_t M = rand_range(1, Mmax);
+        ll_t L = -(ll_t(rand_range(0, Lmax)));
+        rc = test_case(
+            N, O, D,
+            X1, X2, A, B, C, M, L);
     }
     return rc;
 }
