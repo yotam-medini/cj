3845ae118546f3bf003026d806e9b9b232b09afe
Yotam Medini <yotam.medini@gmail.com>
2022-09-24 08:41:05 +0300

    pizza delivery: initial construction

---
+++ 2022/ks-round-E/pizza.cc
@@ -0,0 +1,231 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+// #include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+// #include <iterator>
+// #include <map>
+// #include <set>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+typedef vector<u_t> vu_t;
+
+static unsigned dbg_flags;
+
+class Op
+{
+ public:
+    Op(char _c='+', u_t _k=0) : c(_c), k(_k) {}
+    char c;
+    u_t k;
+};
+
+class Customer
+{
+ public:
+    Customer(int _x=0, int _y=0, u_t _c=0) : x(_x), y(_y), c(_c) {}
+    int x, y;
+    u_t c;
+};
+typedef vector<Customer> vcust_t;
+
+class Pizza
+{
+ public:
+    Pizza(istream& fi);
+    Pizza(const vu_t&) {}; // TBD for test_case
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+    ull_t get_solution() const { return 0; }
+ private:
+    u_t N, P, M;
+    int Ar, Ac;
+    array<Op, 4> ops;
+    vcust_t customers;
+    ull_t solution;
+};
+
+Pizza::Pizza(istream& fi) : solution(0)
+{
+    fi >> N >> P >> M >> Ar >> Ac;
+    for (Op& op: ops)
+    {
+        string s;
+        fi >> s >> op.k;
+        op.c = s[0];
+    }
+    customers.reserve(P);
+    while (customers.size() < P)
+    {
+        Customer customer;
+        fi >> customer.x >> customer.y >> customer.c;
+        customers.push_back(customer);
+    }
+}
+
+void Pizza::solve_naive()
+{
+}
+
+void Pizza::solve()
+{
+    solve_naive();
+}
+
+void Pizza::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+static int real_main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    u_t solve_ver = 0;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-solve1"))
+        {
+            solve_ver = 1;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], 0, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Pizza::*psolve)() =
+        (naive ? &Pizza::solve_naive : &Pizza::solve);
+    if (solve_ver == 1) { psolve = &Pizza::solve; } // solve1
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Pizza pizza(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (pizza.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        pizza.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
+
+static u_t rand_range(u_t nmin, u_t nmax)
+{
+    u_t r = nmin + rand() % (nmax + 1 - nmin);
+    return r;
+}
+
+static int test_case(int argc, char ** argv)
+{
+    int rc = rand_range(0, 1);
+    ull_t solution(-1), solution_naive(-1);
+    bool small = rc == 0;
+    if (small)
+    {
+        Pizza p{vu_t()};
+        p.solve_naive();
+        solution_naive = p.get_solution();
+    }
+    {
+        Pizza p{vu_t()};
+        p.solve();
+        solution = p.get_solution();
+    }
+    if (small && (solution != solution_naive))
+    {
+        rc = 1;
+        cerr << "Failed: solution="<<solution << " != " <<
+            solution_naive << " = solution_naive\n";
+        ofstream f("pizza-fail.in");
+        f << "1\n";
+        f.close();
+    }
+    return rc;
+}
+
+static int test_random(int argc, char ** argv)
+{
+    int rc = 0;
+    int ai = 0;
+    u_t n_tests = strtoul(argv[ai++], 0, 0);
+    for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
+    {
+        cerr << "Tested: " << ti << '/' << n_tests << '\n';
+        rc = test_case(argc, argv);
+    }
+    return rc;
+}
+
+int main(int argc, char **argv)
+{
+    int rc = ((argc > 1) && (string(argv[1]) == string("test"))
+        ? test_random(argc - 2, argv + 2)
+        : real_main(argc, argv));
+    return rc;
+}
