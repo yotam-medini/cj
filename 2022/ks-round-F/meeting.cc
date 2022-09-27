76a9ba48fe3925f5d7f656eebcb54506a1a8dbc4
Yotam Medini <yotam.medini@gmail.com>
2022-09-27 18:44:30 +0300

    scheduling a meeting: non-naive under cosntruction

--- 2022/ks-round-F/meeting.cc
+++ 2022/ks-round-F/meeting.cc
@@ -2,10 +2,12 @@
 // Author:  Yotam Medini  yotam.medini@gmail.com --
 
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -18,6 +20,8 @@ typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
 typedef vector<u_t> vu_t;
 typedef vector<vu_t> vvu_t;
+typedef array<u_t, 2> time_evi_t;
+typedef vector<time_evi_t> vtime_evi_t;
 
 static unsigned dbg_flags;
 
@@ -58,6 +62,44 @@ class PreMeet
 };
 typedef vector<PreMeet> vpremeet_t;
 
+class Event
+{
+ public:
+    Event(u_t _t=0, bool b=false, u_t l=0) : t(_t), begin(b), lead(l) {}
+    u_t t;
+    bool begin; // end=0 < begin=1
+    u_t lead;
+};
+bool operator<(const Event& e0, const Event& e1)
+{
+#if 1
+    typedef tuple<u_t, bool, u_t> ubu_t;
+    bool lt = ubu_t{e0.t, e0.begin, e0.lead} < ubu_t{e1.t, e1.begin, e1.lead};
+#else
+    bool lt = false;
+    if (e0.t < e1.t)
+    {
+        lt = true;
+    }
+    else if (e0.t == e1.t)
+    {
+        if (e0.begin < e1.begin)
+        {
+            lt = true;
+        }
+        else if (e0.begin == e1.begin)
+        {
+            if (e0.lead < e1.lead)
+            {
+                lt = true;
+            }
+        }
+    }
+#endif
+    return lt;
+}
+typedef vector<Event> vevent_t;
+
 class Meeting
 {
  public:
@@ -66,17 +108,30 @@ class Meeting
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
-    ull_t get_solution() const { return 0; }
+    u_t get_solution() const { return solution; }
  private:
+    void set_events();
+    void initial_window();
+    void initial_state();
     u_t N, K, X, D;
     u_t M;
     vpremeet_t pre_meetings;
+    // non-naive
+    u_t lead_free;
+    u_t cancel;
+    vevent_t events;
+    vtime_evi_t window_event;
+    vu_t lead_meetings;
+    vu_t multi_meet;
+    vu_t multi_cancel;
+
     u_t solution;
 };
 
-Meeting::Meeting(istream& fi) : solution(0)
+Meeting::Meeting(istream& fi) : 
+     lead_free(0), cancel(0),
+     solution(0)
 {
-    // copy_n(istream_iterator<u_t>(fi), N, back_inserter(a));
     fi >> N >> K >> X >> D;
     fi >> M;
     pre_meetings.reserve(M);
@@ -127,7 +182,112 @@ void Meeting::solve_naive()
 
 void Meeting::solve()
 {
-    solve_naive();
+    solution = M;
+    set_events();
+    initial_window();
+    initial_state();
+    for (const time_evi_t& time_evi: window_event)
+    {
+        const u_t ei = time_evi[1];
+        const Event& event = events[ei];
+        if (event.begin)
+        {
+            if (lead_meetings[event.lead] == 0)
+            {
+                --lead_free;
+                if (lead_free < K)
+                {
+                    
+                }
+            }
+        }
+        else // end
+        {
+            ;
+        }
+    }
+}
+
+void Meeting::set_events()
+{
+    events.reserve(2*M);
+    for (const PreMeet& pm: pre_meetings)
+    {
+        events.push_back(Event(pm.L, true, pm.P - 1));
+        events.push_back(Event(pm.R, false, pm.P - 1));
+    }
+    sort(events.begin(), events.end());
+    for (u_t ei = 0; ei < events.size(); ++ei)
+    {
+        const Event& event = events[ei];
+        if (event.begin)
+        {
+            if (X <= event.t)
+            {
+                window_event.push_back(time_evi_t{event.t - X, ei});
+            }
+        }
+        else // end
+        {
+            if (event.t < D + X)
+            {
+                window_event.push_back(time_evi_t{event.t, ei});
+            }
+        }
+    }
+    sort(window_event.begin(), window_event.end());
+    lead_meetings = vu_t(N, 0);
+    multi_meet = vu_t(M + 1, 0);
+    multi_cancel = vu_t(M + 1, 0);
+}
+
+void Meeting::initial_window()
+{
+    lead_free = N;
+    u_t ei;
+    for (ei = 0; (ei < events.size()) && (events[ei].t < X); ++ei)
+    {
+        const Event& event = events[ei];
+        if (event.begin)
+        {
+            u_t nm = lead_meetings[event.lead];
+            if (nm == 0)
+            {
+                --lead_free;
+            }
+            else
+            {
+                --multi_meet[nm];
+            }
+            ++nm;
+            lead_meetings[event.lead] = nm;
+            ++multi_meet[nm];
+        }
+    }
+}
+
+void Meeting::initial_state()
+{
+    cancel = 0;
+    if (lead_free >= K)
+    {
+        min_by(solution, 0);
+    }
+    else
+    {
+        u_t missing = K - lead_free;
+        for (u_t mi = 1; missing > 0; ++mi)
+        {
+            if (multi_meet[mi] > 0)
+            {
+                u_t d = min(missing, multi_meet[mi]);
+                missing -= d;
+                cancel += d * mi;
+                multi_cancel[mi] += d;
+            }
+        }
+    }
+    min_by(solution, cancel);
 }
 
 void Meeting::print_solution(ostream &fo) const
