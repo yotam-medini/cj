26f7b5f32178186599ef4495546b79fa14102c33
Yotam Medini <yotam.medini@gmail.com>
2022-10-08 10:51:33 +0300

    Maximum Gain: passed Test Set 2

--- 2022/ks-round-D/gain.cc
+++ 2022/ks-round-D/gain.cc
@@ -1,15 +1,13 @@
 // CodeJam
 // Author:  Yotam Medini  yotam.medini@gmail.com --
 
-// #include <algorithm>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <utility>
 #include <vector>
-// #include <iterator>
-// #include <map>
-// #include <set>
+#include <iterator>
 
 #include <cstdlib>
 
@@ -18,7 +16,7 @@ using namespace std;
 typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
-typedef vector<u_t> vu_t;
+typedef vector<ull_t> vull_t;
 
 static unsigned dbg_flags;
 
@@ -26,21 +24,84 @@ class Gain
 {
  public:
     Gain(istream& fi);
-    Gain(const vu_t&) {}; // TBD for test_case
+    Gain(const vull_t& _A, const vull_t& _B, u_t _K) : 
+        N(A.size()), M(B.size()), K(_K), A(_A), B(_B) {};
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
-    ull_t get_solution() const { return 0; }
+    ull_t get_solution() const { return solution; }
  private:
+    u_t N, M, K;
+    vull_t A, B;
+    ull_t solution;
 };
 
-Gain::Gain(istream& fi)
+Gain::Gain(istream& fi) : solution(0)
 {
-    // copy_n(istream_iterator<u_t>(fi), N, back_inserter(a));
+    fi >> N;
+    copy_n(istream_iterator<ull_t>(fi), N, back_inserter(A));
+    fi >> M;
+    copy_n(istream_iterator<ull_t>(fi), M, back_inserter(B));
+    fi >> K;
 }
+
+#if 0
+static void max_by(ull_t& v, ull_t x)
+{
+    if (v < x)
+    {
+        v = x;
+    }
+}
+#endif
 
 void Gain::solve_naive()
 {
+    const u_t ignore = (N + M) - K;
+    const u_t na_max = min(N, ignore);
+    const u_t nb_max = min(M, ignore);
+    vull_t Asums, Bsums; 
+    Asums.reserve(N + 1); Bsums.reserve(M + 1);
+    Asums.push_back(0);
+    Bsums.push_back(0);
+    for (const ull_t a: A) { Asums.push_back(Asums.back() + a); }
+    for (const ull_t b: B) { Bsums.push_back(Bsums.back() + b); }
+    u_t best_na = 0, best_ia = 0, best_ib;
+    for (u_t na = ignore - nb_max; na <= na_max; ++na)
+    {
+        ull_t Amin = Asums.back(), Bmin = Bsums.back();
+        u_t tbest_ia = 0, tbest_ib;
+        for (u_t ib = 0, ie = na; ie <= N; ++ib, ++ie)
+        {
+            const ull_t deltaA = Asums[ie] - Asums[ib];
+            if (Amin > deltaA)
+            {
+                Amin = deltaA;
+                tbest_ia = ib;
+            }
+        }
+        const u_t nb = ignore - na;
+        for (u_t ib = 0, ie = nb; ie <= M; ++ib, ++ie)
+        {
+            const ull_t deltaB = Bsums[ie] - Bsums[ib];
+            if (Bmin > deltaB)
+            {
+                Bmin = deltaB;
+                tbest_ib = ib;
+            }
+        }
+        ull_t candidate = (Asums.back() - Amin) + (Bsums.back() - Bmin);
+        if (solution < candidate)
+        {
+            solution = candidate;
+            best_na = na;
+            best_ia = tbest_ia;
+            best_ib = tbest_ib;
+        }
+    }
+    if (dbg_flags & 0x1) {
+        cerr <<"best: #(A)="<<best_na << ", #(B)=" << K - best_na <<
+            ", ia=" << best_ia << ", ib=" << best_ib << '\n'; }
 }
 
 void Gain::solve()
@@ -50,6 +111,7 @@ void Gain::solve()
 
 void Gain::print_solution(ostream &fo) const
 {
+    fo << ' ' << solution;
 }
 
 static int real_main(int argc, char ** argv)
@@ -144,19 +206,19 @@ static u_t rand_range(u_t nmin, u_t nmax)
     return r;
 }
 
-static int test_case(int argc, char ** argv)
+static int test_case(const vull_t& A, const vull_t& B, u_t K)
 {
     int rc = rand_range(0, 1);
     ull_t solution(-1), solution_naive(-1);
     bool small = rc == 0;
     if (small)
     {
-        Gain p{vu_t()};
+        Gain p{A, B, K};
         p.solve_naive();
         solution_naive = p.get_solution();
     }
     {
-        Gain p{vu_t()};
+        Gain p{A, B, K};
         p.solve();
         solution = p.get_solution();
     }
@@ -180,7 +242,8 @@ static int test_random(int argc, char ** argv)
     for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
     {
         cerr << "Tested: " << ti << '/' << n_tests << '\n';
-        rc = test_case(argc, argv);
+        vull_t A, B; u_t K = 0;
+        rc = test_case(A, B, K);
     }
     return rc;
 }
