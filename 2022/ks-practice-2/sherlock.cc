72cfe566d3a42cd3e4bee80a6ddf2113f6122063
Yotam Medini <yotam.medini@gmail.com>
2022-12-15 23:04:03 +0200

    Sherlock and Parentheses: passed Test Set 2

---
+++ 2022/ks-practice-2/sherlock.cc
@@ -0,0 +1,138 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+// #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+
+static unsigned dbg_flags;
+
+class Sherlock
+{
+ public:
+    Sherlock(istream& fi);
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+    ull_t get_solution() const { return 0; }
+ private:
+    u_t L, R;
+    ull_t solution;
+};
+
+Sherlock::Sherlock(istream& fi) : solution(0)
+{
+    fi >> L >> R;
+}
+
+void Sherlock::solve_naive()
+{
+    ull_t m = L < R ? L : R;
+    solution = (m * (m - 1))/2 + m;
+}
+
+void Sherlock::solve()
+{
+    solve_naive();
+}
+
+void Sherlock::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+int main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    u_t solve_ver = 0;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-solve1"))
+        {
+            solve_ver = 1;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], nullptr, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Sherlock::*psolve)() =
+        (naive ? &Sherlock::solve_naive : &Sherlock::solve);
+    if (solve_ver == 1) { psolve = &Sherlock::solve; } // solve1
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Sherlock sherlock(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (sherlock.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        sherlock.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
