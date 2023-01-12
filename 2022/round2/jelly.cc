8f89147a31aa1c4eedb0717eb5d37fab3afa96f4
Yotam Medini <yotam.medini@gmail.com>
2023-01-13 00:10:02 +0200

    Saving the Jelly: non-naive solves sample case

--- 2022/round2/jelly.cc
+++ 2022/round2/jelly.cc
@@ -3,12 +3,14 @@
 
 #include <algorithm>
 #include <array>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <limits>
 #include <numeric>
 #include <set>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -25,6 +27,297 @@ typedef array<ll_t, 2> all2_t;
 typedef array<u_t, 2> au2_t;
 typedef vector<all2_t> vall2_t;
 typedef vector<au2_t> vau2_t;
+typedef deque<u_t> dqu_t;
+typedef vector<dqu_t> vdqu_t;
+
+// ========================================================================
+#include <map>
+#include <queue>
+#include <vector>
+
+typedef vector<u_t> vu_t;
+typedef map<u_t, vu_t> u2vu_t;
+typedef map<u_t, u_t> u2u_t;
+typedef array<u_t, 2> au2_t;
+typedef map<au2_t, u_t> au2_2u_t;
+
+class EdgeFlow
+{
+ public:
+    EdgeFlow(u_t c=0, u_t r=0, u_t f=0, bool o=true): 
+        capacity(c), residual_capcity(r), flow(f), original(o) {}
+    u_t capacity;
+    u_t residual_capcity;
+    u_t flow;
+    bool original;
+};
+typedef map<au2_t, EdgeFlow> au2_2ef_t;
+
+class GFK
+{
+ public:
+    GFK(const au2_2u_t& ucapacity, u_t _src, u_t _sink) :
+        source(_src), sink(_sink)
+    {
+        build_graph(ucapacity);
+        init_edges(ucapacity);
+    }
+    bool ford_fulkerson(u_t &total_flow, au2_2u_t& result_flow);
+ private:
+    void build_graph(const au2_2u_t& ucapacit);
+    void init_edges(const au2_2u_t& ucapacit);
+    bool get_augment_path(u_t& flow, vu_t& path) const;
+    void augment_flow(u_t flow, const vu_t& path);
+    void add_edge(const au2_t& edge);
+    // const au2_2u_t& capacity;
+    u_t source;
+    u_t sink;
+    u2vu_t graph;
+    au2_2ef_t eflows;
+};
+
+bool GFK::ford_fulkerson(u_t &total_flow, au2_2u_t& result_flow)
+{
+    bool ok = true;
+    u_t flow;
+    vu_t path;
+    while (get_augment_path(flow, path))
+    {
+        augment_flow(flow, path);
+    }
+    total_flow = 0;
+    result_flow.clear();
+    for (const au2_2ef_t::value_type& kv: eflows)
+    {
+        const EdgeFlow& ef = kv.second;
+        if (ef.flow > 0)
+        {
+            const au2_t& edge = kv.first;
+            if (edge[0] == source)
+            {
+                total_flow += ef.flow;
+            }
+            au2_2u_t::value_type v(edge, ef.flow);
+            result_flow.insert(result_flow.end(), v);
+        }
+    }
+    return ok;
+}
+
+void GFK::build_graph(const au2_2u_t& ucapacity)
+{
+    for (const au2_2u_t::value_type& kv: ucapacity)
+    {
+        const au2_t& edge = kv.first;
+        add_edge(edge);
+    }
+}
+
+void GFK::add_edge(const au2_t& edge)
+{
+    u_t u = edge[0];
+    auto er = graph.equal_range(u);
+    u2vu_t::iterator iter = er.first;
+    if (er.first == er.second)
+    {
+        vu_t adjs;
+        u2vu_t::value_type gkv(u, adjs);
+        iter = graph.insert(iter, gkv);
+    }
+    vu_t& adjs = iter->second;
+    adjs.push_back(edge[1]);
+}
+
+void GFK::init_edges(const au2_2u_t& ucapacity)
+{
+    for (const au2_2u_t::value_type& kv: ucapacity)
+    {
+        const au2_t& edge = kv.first;
+        u_t capacity = kv.second;
+        EdgeFlow ef(capacity, capacity, 0);
+        eflows.insert(eflows.end(), au2_2ef_t::value_type(edge, ef));
+    }
+}
+
+bool GFK::get_augment_path(u_t& flow, vu_t& path) const
+{
+    // BFS
+    bool found = false;
+    u2u_t parent;
+    queue<u_t> q;
+    q.push(source);
+    while (!(q.empty() || found))
+    {
+        u_t node = q.front();
+        q.pop();
+        u2vu_t::const_iterator i_adjs = graph.find(node);
+        static const vu_t empty_adjs;
+        const vu_t& adjs = i_adjs == graph.end() ? empty_adjs : i_adjs->second;
+        for (vu_t::const_iterator ai = adjs.begin(), ae = adjs.end();
+            (ai != ae) && !found; ++ai)
+        {
+            const u_t a = *ai;
+            if (parent.find(a) == parent.end())
+            {
+                const au2_t edge{node, a};
+                const EdgeFlow& ef = eflows.find(edge)->second;
+                if (ef.residual_capcity > 0)
+                {
+                    found = (a == sink);
+                    parent.insert(parent.end(), u2u_t::value_type(a, node));
+                    q.push(a);
+                }
+            }
+        }
+    }
+    path.clear();
+    if (found)
+    {
+        flow = u_t(-1); // infinite
+        vu_t revpath;
+        u_t v = sink;
+        revpath.push_back(v);
+        while (revpath.back() != source)
+        {
+            u_t p = parent.find(v)->second;
+            au2_t edge{p, v};
+            u_t pu_residual = eflows.find(edge)->second.residual_capcity;
+            if (flow > pu_residual)
+            {
+                flow = pu_residual;
+            }
+            v = p;
+            revpath.push_back(v);
+        }
+        path.insert(path.end(), revpath.rbegin(), revpath.rend());
+    }
+    return found;
+}
+
+void GFK::augment_flow(u_t flow, const vu_t& path)
+{
+    for (u_t i = 0, i1 = 1, e = path.size(); i1 != e; i = i1++)
+    {
+        const au2_t edge{path[i], path[i1]};
+        EdgeFlow& ef = eflows.find(edge)->second;
+        ef.residual_capcity -= flow;
+
+        const au2_t redge{path[i1], path[i]};
+        auto er = eflows.equal_range(redge);
+        au2_2ef_t::iterator iter = er.first;
+        if (er.first == er.second)
+        {
+            EdgeFlow ef_res(0, 0, 0, false);
+            iter = eflows.insert(iter, au2_2ef_t::value_type(redge, ef_res));
+            add_edge(redge);
+        }
+        EdgeFlow& rev_ef = iter->second;
+        rev_ef.residual_capcity += flow;
+
+        if (ef.original)
+        {
+            ef.flow += flow;
+        }
+        else
+        {
+            rev_ef.flow -= flow;
+        }
+    }
+}
+
+bool max_flow(
+    u_t& total_flow,
+    au2_2u_t& result_flow,
+    const au2_2u_t& flow,
+    u_t source,
+    u_t sink)
+{
+    GFK gfk(flow, source, sink);
+    bool ret = gfk.ford_fulkerson(total_flow, result_flow);
+    return ret;
+}
+
+typedef unsigned u_t;
+typedef set<u_t> setu_t;
+typedef map<u_t, unsigned> u2u_t;
+
+unsigned maps_set(u2u_t &c2g, u2u_t &g2c, const setu_t &s, unsigned gi)
+{
+    for (unsigned i: s)
+    {
+        c2g.insert(c2g.end(), u2u_t::value_type(i, gi));
+        g2c.insert(g2c.end(), u2u_t::value_type(gi, i));
+        ++gi;
+    }
+    return gi;
+}
+
+int bipartitee_max_match(vau2_t &match, const vau2_t &edges)
+{
+    setu_t lset, rset;
+    for (const auto &e: edges)
+    {
+       lset.insert(e[0]);
+       rset.insert(e[1]);
+    }
+    u2u_t l2g, r2g, g2l, g2r;
+    u_t eol = maps_set(l2g, g2l, lset, 0);
+    u_t eor = maps_set(r2g, g2r, rset, eol);
+    u_t source = eor;
+    u_t sink = source + 1;
+    au2_2u_t flow, result_flow;
+    for (u_t i: lset)
+    {
+        au2_t e{source, l2g[i]};
+        flow.insert(au2_2u_t::value_type(e, 1));
+    }
+    for (const auto &e: edges)
+    {
+        au2_t e1{l2g[e[0]], r2g[e[1]]};
+        flow.insert(au2_2u_t::value_type(e1, 1));
+    }
+    for (u_t i: rset)
+    {
+        au2_t e{r2g[i], sink};
+        flow.insert(au2_2u_t::value_type(e, 1));
+    }
+    u_t total_flow = 0;
+    bool ok = flow.empty() ||
+        max_flow(total_flow, result_flow, flow, source, sink);
+    if (ok)
+    {
+        for (const auto v: result_flow)
+        {
+            const au2_t &ge = v.first;
+            u_t capacity = v.second;
+            if ((ge[0] < eol) && (eol <= ge[1]) && (ge[1] < eor) &&
+                (capacity == 1))
+            {
+                au2_t e{g2l[ge[0]], g2r[ge[1]]};
+                match.push_back(e);
+            }
+        }
+    }
+    int ret = ok ? total_flow : -1;
+    return ret;
+}
+// ========================================================================
+
+
+class DistIdx
+{
+ public:
+    DistIdx(ull_t _d2=0, u_t _i=0) : d2(_d2), i(_i) {}
+    ull_t d2;
+    u_t i;
+};
+bool operator<(const DistIdx& di0, const DistIdx& di1)
+{
+    bool lt = make_tuple(di0.d2, di0.i) < make_tuple(di1.d2, di1.i);
+    return lt;
+}
+typedef deque<DistIdx> dqdistidx_t;
+typedef vector<dqdistidx_t> vdqdistidx_t;
 
 static unsigned dbg_flags;
 
@@ -33,7 +326,8 @@ class Jelly
  public:
     Jelly(istream& fi);
     Jelly(const vall2_t& _children, const vall2_t& _sweets) :
-        N(_children.size()), children(_children), sweets(_sweets) {}; 
+        N(_children.size()), children(_children), sweets(_sweets),
+        possible(true) {}; 
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
@@ -46,13 +340,16 @@ class Jelly
         const ll_t d2 = dx*dx + dy*dy;
         return d2;
     }
+    void set_d2_child_sweet();
     u_t N;
     vall2_t children;
     vall2_t sweets;
     vau2_t solution;
+    bool possible;
+    vdqdistidx_t d2_child_sweet;
 };
 
-Jelly::Jelly(istream& fi)
+Jelly::Jelly(istream& fi) : possible(true)
 {
     fi >> N;
     children.reserve(N);
@@ -79,7 +376,7 @@ void Jelly::solve_naive()
         more = next_permutation(perm.begin(), perm.end()))
     {
         vau2_t candidate;
-        bool possible = true;
+        possible = true;
         vu_t available(N + 1, 0);
         iota(available.begin(), available.end(), 0);
         for (u_t pi = 0; possible && (pi < N); ++pi)
@@ -113,6 +410,85 @@ void Jelly::solve_naive()
 void Jelly::solve()
 {
     // solve_naive();
+    set_d2_child_sweet();
+    solution.clear();
+    setu_t c_pending, s_pending;
+    for (u_t i = 0; i < N; ++i)
+    {
+        c_pending.insert(c_pending.end(), i);
+        s_pending.insert(s_pending.end(), i + 1);
+    }
+    while (possible && (solution.size() < N))
+    {
+        vau2_t match;
+        vau2_t edges;
+        for (u_t ci: c_pending)
+        {
+            dqdistidx_t d2_sweet = d2_child_sweet[ci];
+            while ((!d2_sweet.empty()) &&
+                (s_pending.find(d2_sweet.front().i) == s_pending.end()))
+            {
+                d2_sweet.pop_front();
+            }
+            if (d2_sweet.empty())
+            {
+                possible = false;
+            }
+            else
+            {
+                const ull_t d2 = d2_sweet.front().d2;
+                while ((!d2_sweet.empty()) && (d2_sweet.front().d2 == d2))
+                {
+                    const u_t si = d2_sweet.front().i;
+                    d2_sweet.pop_front();
+                    if (s_pending.find(si) != s_pending.end())
+                    {
+                        const au2_t edge{ci, si};
+                        edges.push_back(edge);
+                    }
+                }
+            }
+        }
+        int n_match = bipartitee_max_match(match, edges);
+        if (n_match > 0)
+        {
+            for (const au2_t& m: match)
+            {
+                solution.push_back(m);
+                c_pending.erase(m[0]);
+                s_pending.erase(m[1]);
+            }
+        }
+        else
+        {
+            possible = false;
+        }
+    }
+    if (!possible) { solution.clear(); }
+}
+
+void Jelly::set_d2_child_sweet()
+{
+    possible = true;
+    d2_child_sweet.reserve(N);
+    for (u_t ci = 0; possible && (ci < N); ++ci)
+    {
+        const all2_t& child = children[ci];
+        const ll_t d2_to_jelly = dist2(child, sweets[0]);
+        dqdistidx_t d2_sweets;
+        for (u_t si = 1; si < N + 1; ++si)
+        {
+            const all2_t& sweet = sweets[si];
+            u_t d2 = dist2(child, sweet);
+            if (d2 <= d2_to_jelly)
+            {
+                d2_sweets.push_back(DistIdx(d2, si));
+            }
+        }
+        sort(d2_sweets.begin(), d2_sweets.end());
+        possible = !d2_sweets.empty();
+        d2_child_sweet.push_back(d2_sweets);
+    }
 }
 
 void Jelly::print_solution(ostream &fo) const
