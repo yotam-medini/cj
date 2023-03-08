cab44a784d30d5c82b3d4c78c770b43e5048d3c8
Yotam Medini <yotam.medini@gmail.com>
2023-03-08 15:40:02 +0200

    Hidden Pancakes: naive solves tiny case

---
+++ 2021/round2/hidden.cc
@@ -0,0 +1,261 @@
+// CodeJam
+// Author:  Yotam Medini  yotam.medini@gmail.com --
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <cstdlib>
+
+using namespace std;
+
+typedef unsigned u_t;
+typedef unsigned long ul_t;
+typedef unsigned long long ull_t;
+typedef vector<u_t> vu_t;
+
+static unsigned dbg_flags;
+
+static const ull_t MOD_BIG = 1000000000 + 7; // 1000000007
+
+void permute_to_shown(vu_t& shown, const vu_t& p)
+{
+    shown.clear();
+    shown.reserve(p.size());
+    for (u_t i = 0; i < p.size(); ++i)
+    {
+        u_t n_shown = 0;
+        for (u_t j = 0; j <= i; ++j)
+        {
+            bool j_shown = true;
+            for (u_t k = j + 1; j_shown && (k <= i); ++k)
+            {
+                j_shown = p[k] < p[j];
+            }
+            if (j_shown)
+            {
+                ++n_shown;
+            }
+        }
+        shown.push_back(n_shown);
+    }
+}
+
+class Hidden
+{
+ public:
+    Hidden(istream& fi);
+    Hidden(const vu_t& v) : N(v.size()), V(v), solution(0) {}; 
+    void solve_naive();
+    void solve();
+    void print_solution(ostream&) const;
+    ull_t get_solution() const { return 0; }
+ private:
+    u_t N;
+    vu_t V;
+    ull_t solution;
+};
+
+Hidden::Hidden(istream& fi) : solution(0)
+{
+    fi >> N;    
+    copy_n(istream_iterator<u_t>(fi), N, back_inserter(V));
+}
+
+void Hidden::solve_naive()
+{
+    vu_t p; p.reserve(N);
+    while (p.size() < N)
+    {
+        p.push_back(p.size());
+    }
+    u_t n = 0;
+    for (bool more = true; more; more = next_permutation(p.begin(), p.end()))
+    {
+        vu_t shown;
+        permute_to_shown(shown, p);
+        if (shown == V)
+        {
+            ++n;
+            if (n == MOD_BIG)
+            {
+                n = 0;
+            }
+        }
+    }
+    solution = n;
+}
+
+void Hidden::solve()
+{
+    solve_naive();
+}
+
+void Hidden::print_solution(ostream &fo) const
+{
+    fo << ' ' << solution;
+}
+
+static int real_main(int argc, char ** argv)
+{
+    const string dash("-");
+
+    bool naive = false;
+    bool tellg = false;
+    u_t solve_ver = 0;
+    int rc = 0, ai;
+
+    for (ai = 1; (rc == 0) && (ai < argc) && (argv[ai][0] == '-') &&
+        argv[ai][1] != '\0'; ++ai)
+    {
+        const string opt(argv[ai]);
+        if (opt == string("-naive"))
+        {
+            naive = true;
+        }
+        else if (opt == string("-solve1"))
+        {
+            solve_ver = 1;
+        }
+        else if (opt == string("-debug"))
+        {
+            dbg_flags = strtoul(argv[++ai], nullptr, 0);
+        }
+        else if (opt == string("-tellg"))
+        {
+            tellg = true;
+        }
+        else
+        {
+            cerr << "Bad option: " << opt << "\n";
+            return 1;
+        }
+    }
+
+    int ai_in = ai;
+    int ai_out = ai + 1;
+    istream *pfi = (argc <= ai_in || (string(argv[ai_in]) == dash))
+         ? &cin
+         : new ifstream(argv[ai_in]);
+    ostream *pfo = (argc <= ai_out || (string(argv[ai_out]) == dash))
+         ? &cout
+         : new ofstream(argv[ai_out]);
+
+    if ((!pfi) || (!pfo) || pfi->fail() || pfo->fail())
+    {
+        cerr << "Open file error\n";
+        exit(1);
+    }
+
+    string ignore;
+    unsigned n_cases;
+    *pfi >> n_cases;
+    getline(*pfi, ignore);
+
+    void (Hidden::*psolve)() =
+        (naive ? &Hidden::solve_naive : &Hidden::solve);
+    if (solve_ver == 1) { psolve = &Hidden::solve; } // solve1
+    ostream &fout = *pfo;
+    ul_t fpos_last = pfi->tellg();
+    for (unsigned ci = 0; ci < n_cases; ci++)
+    {
+        Hidden hidden(*pfi);
+        getline(*pfi, ignore);
+        if (tellg)
+        {
+            ul_t fpos = pfi->tellg();
+            cerr << "Case (ci+1)="<<(ci+1) << ", tellg=[" <<
+                fpos_last << " " << fpos << ") size=" <<
+                (fpos - fpos_last) << "\n";
+            fpos_last = fpos;
+        }
+
+        (hidden.*psolve)();
+        fout << "Case #"<< ci+1 << ":";
+        hidden.print_solution(fout);
+        fout << "\n";
+        fout.flush();
+    }
+
+    if (pfi != &cin) { delete pfi; }
+    if (pfo != &cout) { delete pfo; }
+    return 0;
+}
+
+static u_t rand_range(u_t nmin, u_t nmax)
+{
+    u_t r = nmin + rand() % (nmax + 1 - nmin);
+    return r;
+}
+
+static void save_case(const char* fn)
+{
+    ofstream f(fn);
+    f << "1\n";
+    f.close();
+}
+
+static int test_case(int argc, char ** argv)
+{
+    int rc = rand_range(0, 1);
+    ull_t solution(-1), solution_naive(-1);
+    bool small = rc == 0;
+    if (dbg_flags & 0x100) { save_case("hidden-curr.in"); }
+    if (small)
+    {
+        Hidden p{vu_t()};
+        p.solve_naive();
+        solution_naive = p.get_solution();
+    }
+    {
+        Hidden p{vu_t()};
+        p.solve();
+        solution = p.get_solution();
+    }
+    if (small && (solution != solution_naive))
+    {
+        rc = 1;
+        cerr << "Failed: solution = " << solution << " != " <<
+            solution_naive << " = solution_naive\n";
+        save_case("hidden-fail.in");
+    }
+    if (rc == 0) { cerr << "  ..." <<
+        (small ? " (small) " : " (large) ") << " --> " <<
+        solution << '\n'; }
+    return rc;
+}
+
+static int test_random(int argc, char ** argv)
+{
+    int rc = 0;
+    int ai = 0;
+    if (string(argv[ai]) == string("-debug"))
+    {
+        dbg_flags = strtoul(argv[ai + 1], nullptr, 0);
+        ai += 2;
+    }
+    const u_t n_tests = strtoul(argv[ai++], nullptr, 0);
+    const u_t Nmin = strtoul(argv[ai++], nullptr, 0);
+    const u_t Nmax = strtoul(argv[ai++], nullptr, 0);
+    cerr << "n_tests=" << n_tests <<
+        ", Nmin=" << Nmin << ", Nmax=" << Nmax <<
+        '\n';
+    for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
+    {
+        cerr << "Tested: " << ti << '/' << n_tests << '\n';
+        rc = test_case(argc, argv);
+    }
+    return rc;
+}
+
+int main(int argc, char **argv)
+{
+    int rc = ((argc > 1) && (string(argv[1]) == string("test"))
+        ? test_random(argc - 2, argv + 2)
+        : real_main(argc, argv));
+    return rc;
+}
