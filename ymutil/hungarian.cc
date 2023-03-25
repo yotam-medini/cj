1dc995ab2c6910a9e8aff0f3758aa66a4fb80741
Yotam Medini <yotam.medini@gmail.com>
2023-03-25 10:37:36 +0300

    Hungarian: test under construction

--- ymutil/hungarian.cc
+++ ymutil/hungarian.cc
@@ -10,10 +10,6 @@ typedef vector<int> vi_t;
 typedef vector<u_t> vu_t;
 typedef vector<vu_t> vvu_t;
 
-void minimal_matching(vu_t& matching, const vvu_t& weight_matrix)
-{
-}
-
 u_t permutation_weight(const vu_t& permutation, const vvu_t& weight_matrix)
 {
     u_t weight = 0;
@@ -28,30 +24,48 @@ u_t permutation_weight(const vu_t& permutation, const vvu_t& weight_matrix)
 #include <algorithm>
 #include <numeric>
 
+template <bool _Maximize>
 class Hungarian
 {
  public:
     Hungarian(const vvu_t& weight_matrix) :
         w(weight_matrix),
-        lp(w.size(), -1),
-        rp(w.size(), -1)
+        NL(w.size()),
+        NR(w[0].size()),
+        lp(max(NL, NR), -1),
+        rp(max(NL, NR), -1)
         {}
+    void matching_get(vu_t& matching);
  private:
     const vvu_t& w;
+    const u_t NL, NR;
     vi_t lp, rp;
     vu_t lh, rh;
 };
 
+template<bool _Maximize>
+void Hungarian<_Maximize>::matching_get(vu_t& matching)
+{
+}
+
+void minimal_matching(vu_t& matching, const vvu_t& weight_matrix)
+{
+    Hungarian<false>(weight_matrix).matching_get(matching);
+}
+
+void maximal_matching(vu_t& matching, const vvu_t& weight_matrix)
+{
+    Hungarian<true>(weight_matrix).matching_get(matching);
+}
+
+////////////////////////////////////////////////////////////////////////
+#if defined(TEST_HUNGARIAN)
 typedef bool (*_cmp_t)(u_t, u_t);
 
 template<typename LG_Compare, typename LGEQ_Compare>
 void best_matching_naive(
     vvu_t& matchings,
     const vvu_t& weight_matrix
-    // _cmp_t lgeq,
-    // _cmp_t lg)
-    // const pointer_to_binary_function<u_t, u_t, bool> lgeq,
-    // const pointer_to_binary_function<u_t, u_t, bool> lg)
 )
 {
     matchings.clear();
@@ -94,16 +108,91 @@ void maximal_matching_naive(vvu_t& matchings, const vvu_t& weight_matrix)
 
 void save_weight(const vvu_t& weight_matrix, const string& fn)
 {
-    const u_t n = weight_matrix.size();
+    const u_t nl = weight_matrix.size();
+    const u_t nr = weight_matrix[0].size();
     ofstream f(fn);
-    f << n << '\n';
-    for (u_t r = 0; r < n; ++r)
+    f << nl << ' ' << nr << '\n';
+    for (u_t r = 0; r < nl; ++r)
     {
         const vu_t& row = weight_matrix[r];
-        for (u_t c = 0; c < n; ++c)
+        for (u_t c = 0; c < nr; ++c)
         {
             f << (c == 0 ? "" : " ") << row[c];
         }
         f << '\n';
     }
 }
+
+void read_weight(vvu_t& weight_matrix, const string& fn)
+{
+    ifstream f(fn);
+    u_t nl, nr;
+    f >> nl >> nr;
+    weight_matrix.reserve(nl);
+    weight_matrix.clear();
+    while (weight_matrix.size() < nl)
+    {
+        weight_matrix.push_back(vu_t());
+        vu_t& row = weight_matrix.back();
+        row.reserve(nr);
+        while (row.size() < nr)
+        {
+            u_t x;
+            f >> x;
+            row.push_back(x);
+        }
+        
+    }    
+}
+
+static void usage(const char* p0)
+{
+    cerr << "Usage: " << p0 << " <subcmd> ...\n" << 
+        "  Where <subcmd> can be\n:"
+        "   " << p0 << " naive <filein>\n"
+        "   " << p0 << " test <filein>\n";
+}
+
+static int test_naive(const char* fn)
+{
+    vvu_t weight_matrix;
+    read_weight(weight_matrix, fn);
+    vvu_t matchings;
+    maximal_matching_naive(matchings, weight_matrix);
+    for (const vu_t& matching: matchings)
+    {
+        for (u_t l = 0; l < matching.size(); ++l)
+        {
+            cout << (l > 0 ? ", " : "") << "[" << l << "]: " << matching[l];
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+    int rc = 0;
+    if (argc == 0)
+    {
+        usage(argv[0]);
+        rc = 1;
+    }
+    else
+    {
+        const string subcmd(argv[1]);
+        if (subcmd == string("naive"))
+        {
+            rc = test_naive(argv[2]);
+        }
+        else
+        {
+            cerr << "Unsupported subcmd: " << subcmd << '\n';
+            usage(argv[0]);
+            rc = 1;
+        }
+    }
+    return rc;
+}
+
+#endif /* TEST_HUNGARIAN */
