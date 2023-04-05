a398447bcb029e89430fda8fb2cddc21acbd0e9c
Yotam Medini <yotam.medini@gmail.com>
2023-04-05 09:33:11 +0300

    HungarianBase::solve under construction

--- ymutil/hungarian.cc
+++ ymutil/hungarian.cc
@@ -1,5 +1,9 @@
+#include <iostream>
+#include <array>
+#include <deque>
 #include <functional>
 #include <limits>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -7,9 +11,25 @@ using namespace std;
 typedef unsigned u_t;
 typedef unsigned long ul_t;
 typedef unsigned long long ull_t;
+typedef array<u_t, 2> au2_t;
 typedef vector<int> vi_t;
 typedef vector<u_t> vu_t;
 typedef vector<vu_t> vvu_t;
+typedef vector<au2_t> vau2_t;
+typedef unordered_set<u_t> hsetu_t;
+typedef vector<hsetu_t> vhsetu_t;
+typedef deque<u_t> dqu_t;
+
+class Hash_AU2 {
+ public:
+     size_t operator()(const au2_t& a) const {
+       u_t n = a[0] ^ (a[1] << 7);
+       return hash_uint(n);
+     }
+ private:
+     hash<u_t> hash_uint;
+};
+typedef unordered_set<au2_t, Hash_AU2> hset_au2_t;
 
 u_t permutation_weight(const vu_t& permutation, const vvu_t& weight_matrix)
 {
@@ -28,43 +48,154 @@ u_t permutation_weight(const vu_t& permutation, const vvu_t& weight_matrix)
 class HungarianBase
 {
  public:
-    HungarianBase(const vvu_t& weight_matrix) :
+    HungarianBase(const vvu_t& weight_matrix, int wdef=0) :
+        w_default(wdef),
         w(weight_matrix),
         NL(w.size()),
         NR(w[0].size()),
-        lp(max(NL, NR), -1),
-        rp(max(NL, NR), -1)
+        N(max(NL, NR)),
+        lp(N, -1),
+        rp(N, -1),
+        matched(0)
         {}
     void matching_get(vu_t& matching);
  protected:
+    void solve();
+    bool is_feasible(u_t l, u_t r) const;
+    void find_aug_path();
     int get_w(u_t l, u_t r) const
     {
-        return ((l < NL) && (r < NR) ? w[l][r] : w_default());
+        return ((l < NL) && (r < NR) ? w[l][r] : w_default);
     }
-    virtual int w_default() const = 0;
- private:
+    virtual void init_feasible() = 0;
+    virtual u_t get_delta() const = 0;
+    virtual void relabel(u_t delta) = 0;
+    int w_default; // for _Maximize
     const vvu_t& w;
-    const u_t NL, NR;
+    const u_t NL, NR, N;
     vi_t lp, rp;
     vi_t lh, rh;
+    // equality sub graph
+    vhsetu_t eq_l2r, eqr2l;
+    vi_t match_l2r, match_r2l;
+    u_t matched;
+    vu_t P; // augmented path
+    hsetu_t FL, FR, FRc;
 };
+
+void HungarianBase::matching_get(vu_t& matching)
+{
+    solve();
+}
+
+void HungarianBase::solve()
+{
+    init_feasible();
+    FL.clear();
+    FR.clear();
+    for (u_t r = 0; r < N; ++r) { FRc.insert(r); } // R - FR;
+    while (matched < N)
+    {
+        find_aug_path();
+        if (P.empty()) { cerr << "Error empty augmented path\n"; exit(1); }
+    }
+}
+
+void HungarianBase::find_aug_path()
+{
+    dqu_t Q;
+    for (u_t l = 0; l < N; ++l)
+    {
+        if (match_l2r[l] == -1)
+        {
+            lp[l] = -1;
+            Q.push_back(l);
+            FL.insert(l);
+        }
+    }
+    bool found = false; // path found
+    while (!found)
+    {
+        u_t delta = get_delta();
+        relabel(delta);
+    }
+    P.clear();
+}
 
 template <bool _Maximize>
 class Hungarian : public HungarianBase
 {
  public:
-    Hungarian(const vvu_t& weight_matrix) : HungarianBase(weight_matrix)
-        {}
-    void matching_get(vu_t& matching);
-    int w_default() const
-    {
-        int wdefv = (_Maximize ? 0 : numeric_limits<int>::max());
-        return wdefv;
-    }
+    Hungarian(const vvu_t& weight_matrix) :
+        HungarianBase(
+            weight_matrix,
+            _Maximize ? 0 : numeric_limits<int>::max())
+    {}
+    // void matching_get(vu_t& matching);
+ private:
+    void init_feasible();
+    u_t get_delta() const;
+    void relabel(u_t delta);
 };
 
-template<bool _Maximize>
-void Hungarian<_Maximize>::matching_get(vu_t& matching)
+template <bool _Maximize>
+void Hungarian<_Maximize>::init_feasible()
+{
+    for (u_t i = 0; i < N; ++i)
+    {
+        int h = w[i][0];
+        for (u_t j = 1; j < N; ++j)
+        {
+            if (_Maximize)
+            {
+                 if (h < int(w[i][j]))
+                 {
+                     h = w[i][j];
+                 }
+            }
+            else
+            {
+                 if (h > int(w[i][j]))
+                 {
+                     h = w[i][j];
+                 }
+            }
+        }
+        lh[i] = h;
+        rh[i] = 0;
+    }
+}
+
+template <bool _Maximize>
+u_t Hungarian<_Maximize>::get_delta() const
+{
+    u_t l0 = *FL.begin();
+    u_t r0 = *FRc.begin();
+    u_t delta = (_Maximize
+        ? (lh[l0] + rh[r0]) - w[l0][r0]
+        : w[l0][r0] - (lh[l0] + rh[r0]));
+    for (u_t l: FL)
+    {
+        for (u_t r: FRc)
+        {
+            u_t v = (_Maximize
+                ? (lh[l] + rh[r]) - w[l][r]
+                : w[l][r] - (lh[l] + rh[r]));
+            if (_Maximize)
+            {
+                if (delta > v) { delta = v; }
+            }
+            else
+            {
+                if (delta < v) { delta = v; }
+            }
+        }
+    }
+    return delta;
+}
+
+template <bool _Maximize>
+void Hungarian<_Maximize>::relabel(u_t delta)
 {
 }
 
