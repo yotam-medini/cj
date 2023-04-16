006e6c4bfa4384013e688be1656e08d445894a2a
Yotam Medini <yotam.medini@gmail.com>
2023-04-16 22:56:51 +0300

    Collecting Pancakes: non-naive fixes, self random test - passed Test Set 2

--- 2023/round-B/collecting.cc
+++ 2023/round-B/collecting.cc
@@ -25,11 +25,15 @@ class Collecting
 {
  public:
     Collecting(istream& fi);
-    Collecting(const vu_t&) {}; // TBD for test_case
+    Collecting(
+       const vull_t &_A,
+       u_t _La, u_t _Ra,
+       u_t _Lb, u_t _Rb
+    ) : N(_A.size()), A(_A), La(_La), Ra(_Ra), Lb(_Lb), Rb(_Rb), solution(0) {}
     void solve_naive();
     void solve();
     void print_solution(ostream&) const;
-    ull_t get_solution() const { return 0; }
+    ull_t get_solution() const { return solution; }
  private:
     void set_pre_sum();
     u_t N;
@@ -130,19 +134,21 @@ void Collecting::solve()
     for (u_t x = La; x <= Ra; ++x)
     {
         ull_t Bleft = 0, Bright = 0;
-        if (Rb <= x)
+        if (Lb < x)
         {
-            u_t delta = x - Rb;
-            u_t e = x - delta / 2;
-            Bleft = pre_sum[e] - pre_sum[0];
+            const u_t y = min(Rb, x - 1);
+            const u_t delta = x - y;
+            const u_t e = x - delta / 2;
+            Bleft = pre_sum[e - 1] - pre_sum[0];
         }
-        if (x <= Lb)
+        if (x < Rb)
         {
-            u_t delta = Lb - x;
+            const u_t y = max(Lb, x + 1);
+            u_t delta = y - x;
             u_t b = x + 1 + delta/2;
             Bright = pre_sum[N] - pre_sum[b - 1];
         }
-        if ((Lb <= x) && (x <= Rb))
+        if ((Lb < x) && (x < Rb))
         {
             Bleft = pre_sum[x - 1] - pre_sum[0];
             Bright = pre_sum[N] - pre_sum[x];
@@ -250,27 +256,41 @@ static u_t rand_range(u_t nmin, u_t nmax)
     return r;
 }
 
-static void save_case(const char* fn)
-{
+static void save_case(
+    const char* fn, 
+    const vull_t &A,
+    u_t La, u_t Ra,
+    u_t Lb, u_t Rb)
+{ 
+    const u_t N = A.size();
     ofstream f(fn);
-    f << "1\n";
+    f << "1\n" << N;
+    char sep = '\n';
+    for (u_t x: A) { f << sep << x; sep = ' '; }
+    f << '\n';
+    f << La << ' ' << Ra << ' ' << Lb << ' ' << Rb << '\n';
     f.close();
 }
 
-static int test_case(int argc, char ** argv)
+static int test_case(
+    const vull_t &A,
+    u_t La, u_t Ra,
+    u_t Lb, u_t Rb)
 {
-    int rc = rand_range(0, 1);
+    int rc = 0;
+    const u_t N = A.size();
     ull_t solution(-1), solution_naive(-1);
-    bool small = rc == 0;
-    if (dbg_flags & 0x100) { save_case("collecting-curr.in"); }
+    bool small = (N < 0x20);
+    if (dbg_flags & 0x100) {
+        save_case("collecting-curr.in", A, La, Ra, Lb, Rb); }
     if (small)
     {
-        Collecting p{vu_t()};
+        Collecting p(A, La, Ra, Lb, Rb);
         p.solve_naive();
         solution_naive = p.get_solution();
     }
     {
-        Collecting p{vu_t()};
+        Collecting p(A, La, Ra, Lb, Rb);
         p.solve();
         solution = p.get_solution();
     }
@@ -279,12 +299,22 @@ static int test_case(int argc, char ** argv)
         rc = 1;
         cerr << "Failed: solution = " << solution << " != " <<
             solution_naive << " = solution_naive\n";
-        save_case("collecting-fail.in");
+        save_case("collecting-fail.in", A, La, Ra, Lb, Rb);
     }
     if (rc == 0) { cerr << "  ..." <<
-        (small ? " (small) " : " (large) ") << " --> " <<
+        (small ? " (small) " : " (large) ") << " N=" << N << " --> " <<
         solution << '\n'; }
     return rc;
+}
+
+static void rand_LR(u_t& L, u_t& R, u_t N)
+{
+    L = rand_range(1, N);
+    R = rand_range(1, N);
+    if (L > R)
+    {
+        swap(L, R);
+    }
 }
 
 static int test_random(int argc, char ** argv)
@@ -299,13 +329,29 @@ static int test_random(int argc, char ** argv)
     const u_t n_tests = strtoul(argv[ai++], nullptr, 0);
     const u_t Nmin = strtoul(argv[ai++], nullptr, 0);
     const u_t Nmax = strtoul(argv[ai++], nullptr, 0);
+    const u_t Amax = strtoul(argv[ai++], nullptr, 0);
     cerr << "n_tests=" << n_tests <<
         ", Nmin=" << Nmin << ", Nmax=" << Nmax <<
+        ", Amax=" << Amax <<
         '\n';
     for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti)
     {
         cerr << "Tested: " << ti << '/' << n_tests << '\n';
-        rc = test_case(argc, argv);
+        const u_t N = rand_range(Nmin, Nmax);
+        vull_t A; A.reserve(N);
+        while (A.size() < N)
+        {
+            A.push_back(rand_range(1, Amax));
+        }
+        u_t La, Ra, Lb, Rb;
+        bool good_quad = false;
+        while (!good_quad)
+        {
+            rand_LR(La, Ra, N);
+            rand_LR(Lb, Rb, N);
+            good_quad = !((La <= Lb) && (Lb == Rb) && (Rb <= Ra));
+        }
+        rc = test_case(A, La, Ra, Lb, Rb);
     }
     return rc;
 }
