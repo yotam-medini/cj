679a33c9494276830545194059fb3f7177e980cd
Yotam Medini <yotam.medini@gmail.com>
2025-02-19 14:29:46 +0200

    segtree: use nodes for future max queries, use fmt::format

--- ymutil/segtree.cc
+++ ymutil/segtree.cc
@@ -1,48 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cstdlib>
-
-using namespace std;
+#include <fmt/core.h>
 
 using u_t = unsigned;
 using ul_t = unsigned long;
 using ull_t = unsigned long long;
+using e_t = ull_t;
+
+class Node {
+ public:
+  Node(e_t sum=0, e_t max=0, size_t max_count=0) :
+    sum_{sum}, max_{max}, max_count_{max_count} {
+  }
+  std::string str() const {
+    return fmt::format("{}sum={}, max={}, #={}{}",
+       "{", sum_, max_, max_count_, "}");
+  }
+  std::string StrShort() const {
+    return fmt::format("{}S={}, M={}, #={}{}",
+       "{", sum_, max_, max_count_, "}");
+  }
+  e_t sum_{0};
+  e_t max_{0};
+  size_t max_count_{0};
+};
 
 class SegmentTree {
  public:
-  using e_t = ull_t;
-  using ve_t = vector<e_t>;
+  using ve_t = std::vector<e_t>;
   SegmentTree(const ve_t& _a);
   void update(u_t pos, e_t val);
   e_t query_sum(u_t b, u_t e) const;
-  void print(ostream& os) const;
+  void print(std::ostream& os) const;
  private:
-  using vve_t = vector<ve_t>;
+  using level_nodes_t = std::vector<Node>;
   e_t level_sum(u_t level, u_t b, u_t e) const;
   void level_update(u_t level, u_t pos, e_t old_val, e_t val);
-  u_t height() const { return a.size(); }
-  u_t size() const { return a.empty() ? 0 : a[0].size(); }
-  vve_t a;
+  u_t height() const { return levels_nodes_.size(); }
+  u_t size() const {
+    return levels_nodes_.empty() ? 0 : levels_nodes_[0].size();
+  }
+  std::vector<level_nodes_t> levels_nodes_;
 };
 
-SegmentTree::SegmentTree(const ve_t& _a) {
-  a.push_back(_a);
-  while (a.back().size() > 1) {
-    const ve_t& prev = a.back();
+SegmentTree::SegmentTree(const ve_t& a) {
+  levels_nodes_.push_back(level_nodes_t());
+  auto &nodes = levels_nodes_.back();
+  nodes.reserve(a.size());
+  for (const auto x: a) {
+    nodes.push_back({x, x, 1});
+  }
+  while (levels_nodes_.back().size() > 1) {
+    const level_nodes_t& prev = levels_nodes_.back();
     const u_t sz = prev.size() / 2;
     const u_t sz2 = 2*sz;
-    ve_t b; b.reserve(sz);
+    level_nodes_t b; b.reserve(sz);
     for (u_t i = 0; i < sz2; i += 2) {
-        b.push_back(prev[i] + prev[i + 1]);
+        b.push_back(Node{prev[i].sum_ + prev[i + 1].sum_}); // JUST sum.....
     }
-    a.push_back(ve_t());
-    swap(a.back(), b);
+    levels_nodes_.push_back(level_nodes_t());
+    swap(levels_nodes_.back(), b);
   }
-  print(cerr);
+  print(std::cerr);
 }
 
 void SegmentTree::update(u_t pos, e_t val) {
-  level_update(0, pos, a[0][pos], val);
+  level_update(0, pos, levels_nodes_[0][pos].sum_, val);
 }
 
 auto SegmentTree::query_sum(u_t b, u_t e) const -> e_t {
@@ -53,10 +78,10 @@ auto SegmentTree::level_sum(u_t level, u_t b, u_t e) const -> e_t {
   e_t total = 0;
   if (b < e) {
     if (b % 2 != 0) {
-      total += a[level][b++];
+      total += levels_nodes_[level][b++].sum_;
     }
     if (e % 2 != 0) {
-      total += a[level][--e];
+      total += levels_nodes_[level][--e].sum_;
     }
     total += level_sum(level + 1, b/2, e/2);
   }
@@ -64,20 +89,20 @@ auto SegmentTree::level_sum(u_t level, u_t b, u_t e) const -> e_t {
 }
 
 void SegmentTree::level_update(u_t level, u_t pos, e_t old_val, e_t val) {
-  ve_t& alevel = a[level];
-  alevel[pos] -= old_val;
-  alevel[pos] -= val;
+  level_nodes_t& alevel = levels_nodes_[level];
+  alevel[pos].sum_ -= old_val;
+  alevel[pos].sum_ -= val;
   if ((pos % 2 != 0) || (pos + 1 < alevel.size())) {
     level_update(level + 1, pos / 2, old_val, val);
   }
 }
 
-void SegmentTree::print(ostream& os) const {
-  for (u_t level = 0; level < a.size(); ++level) {
-    const ve_t& alevel = a[level];
-    os << "[" << level << "]";
-    for (e_t x: alevel) {
-      os << ' ' << x;
+void SegmentTree::print(std::ostream& os) const {
+  for (u_t level = 0; level < levels_nodes_.size(); ++level) {
+    const level_nodes_t& alevel = levels_nodes_[level];
+    os << fmt::format("[{}]", level);
+    for (const Node& node: alevel) {
+      os << fmt::format(" {}", node.StrShort());
     }
     os << '\n';
   }
@@ -88,16 +113,16 @@ void SegmentTree::print(ostream& os) const {
 #include <algorithm>
 #include <numeric>
 
-using vull_t = vector<ull_t>;
+using vull_t = std::vector<ull_t>;
 
 static int
 verdict(const vull_t& a, u_t b, u_t e, ull_t st_sum, ull_t naive_sum) {
   int rc = 0;
   if (st_sum != naive_sum) {
     rc = 1;
-    cerr << "Failure: specific " << b << ' ' << e;
-    for (ull_t x: a) { cerr << ' ' << x; }
-    cerr << '\n';
+    std::cerr << "Failure: specific " << b << ' ' << e;
+    for (ull_t x: a) { std::cerr << ' ' << x; }
+    std::cerr << '\n';
   }
   return rc;
 }
@@ -134,10 +159,10 @@ static int random_test(int argc, char **argv) {
   u_t szmin = strtoul(argv[ai++], 0, 0);
   u_t szmax = strtoul(argv[ai++], 0, 0);
   u_t nmax = strtoul(argv[ai++], 0, 0);
-  cerr << "n_tests="<<n_tests <<
+  std::cerr << "n_tests="<<n_tests <<
       ", szmin="<<szmin << ", szmax="<<szmax << ", nmax="<<nmax << '\n';
   for (u_t ti = 0; (rc == 0) && (ti < n_tests); ++ti) {
-    cerr << "Tested: " << ti << '/' << n_tests << '\n';
+    std::cerr << "Tested: " << ti << '/' << n_tests << '\n';
     u_t sz = rand_range(szmin, szmax);
     vull_t a;  a.reserve(sz);
     while (a.size() < sz) {
@@ -150,7 +175,7 @@ static int random_test(int argc, char **argv) {
 
 int main(int argc, char **argv) {
   int rc = 0;
-  if (string(argv[1]) == string("specific")) {
+  if (std::string(argv[1]) == std::string("specific")) {
     int ai = 2;
     u_t b = strtoul(argv[ai++], 0, 0);
     u_t e = strtoul(argv[ai++], 0, 0);
